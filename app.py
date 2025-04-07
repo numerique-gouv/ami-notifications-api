01c5a4cf6fdafec408a1fcd49b68e2e4db7e0377
Mathieu Agopian <mathieu@agopian.info>
2025-04-07 18:31:08 +0200

    Display a local notification

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from litestar import Litestar, get
+from litestar.static_files import create_static_files_router
 
-
-@get("/")
-async def index() -> str:
-    return "Hello, world!"
+HTML_DIR = "public"
 
 
 @get("/notifications/{email:str}")
@@ -11,4 +9,13 @@ async def get_notifications(email: str) -> list[str]:
     return []
 
 
-app = Litestar([index, get_notifications])
+app = Litestar(
+    route_handlers=[
+        create_static_files_router(
+            path="/",
+            directories=[HTML_DIR],
+            html_mode=True,
+        ),
+        get_notifications,
+    ]
+)
