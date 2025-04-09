179a9edf6e1153e71c5baa7c25a0ac1f8128215b
Mathieu Agopian <mathieu@agopian.info>
2025-04-09 18:32:33 +0200

    Download uv ourselves in the start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,9 @@
 PORT="${PORT:-8000}"
 HOSTNAME="${HOSTNAME:-127.0.0.1}"
 
+curl -LsSf "https://astral.sh/uv/0.6.1/install.sh" | sh 2>&1
+
+# Make the uv commands available
+source "$HOME/.local/bin/env"
+
 uv run litestar run -p ${PORT} -H ${HOSTNAME} ${RELOAD}
