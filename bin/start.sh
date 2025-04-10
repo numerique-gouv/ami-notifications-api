ab7b5e566f4f8d7e1b66d21279febec1f07ceb23
Mathieu Agopian <mathieu@agopian.info>
2025-04-10 07:17:47 +0200

    Only install "uv" if it's not already available

--- bin/start.sh
+++ bin/start.sh
@@ -6,10 +6,13 @@ HOSTNAME="${HOSTNAME:-127.0.0.1}"
 VAPID_PRIVATE_KEY_FILE="private_key.pem"  # Only works when developping locally...
 export VAPID_PRIVATE_KEY=${VAPID_PRIVATE_KEY:-$(cat ${VAPID_PRIVATE_KEY_FILE})}  # Otherwise export it in your deployement config
 
-curl -LsSf "https://astral.sh/uv/0.6.13/install.sh" | sh 2>&1
+if ! command -v uv 2>&1 >/dev/null
+then
+  curl -LsSf "https://astral.sh/uv/0.6.13/install.sh" | sh 2>&1
 
-# Make the uv commands available
-source "$HOME/.local/bin/env"
+  # Make the uv commands available
+  source "$HOME/.local/bin/env"
+fi
 
 
 uv run litestar run -p ${PORT} -H ${HOSTNAME} ${RELOAD} ${DEBUG}
