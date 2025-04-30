d8b32ad3ad5c8cf3db7b3c6fdee77e538287487d
Mathieu Agopian <mathieu@agopian.info>
2025-04-30 17:59:36 +0200

    Better pythonic code structure
    Co-Authored-By: David Larlet <david@larlet.fr>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,8 @@
-import sys
 from collections.abc import Iterator
 
 import pytest
 from litestar import Litestar
 from litestar.testing import TestClient
-
-# FIXME: this is a hack, should be resolved by a better code/package structure.
-sys.path.append(".")
 
 from app import app
 
