cb53fb886c15416ba5f1f223237329787323b9ac
David Larlet <david@larlet.fr>
2025-04-30 21:15:21 -0400

    Tests using a database session setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,42 @@
+import os
 from collections.abc import Iterator
 
 import pytest
 from litestar import Litestar
 from litestar.testing import TestClient
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-from app import app
+from app import Notification, app
 
 app.debug = True
 
 
-@pytest.fixture(scope="function")
-def test_client() -> Iterator[TestClient[Litestar]]:
+@pytest.fixture
+def test_client(session: AsyncSession) -> Iterator[TestClient[Litestar]]:
     with TestClient(app=app) as client:
         yield client
+
+
+@pytest.fixture(name="session", scope="function")
+async def session_fixture():
+    engine = create_async_engine(os.getenv("DATABASE_URL", ""))
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+    async with AsyncSession(engine) as session:
+        yield session
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.drop_all)
+
+    await engine.dispose()
+
+
+@pytest.fixture
+async def notification1(session: AsyncSession) -> Notification:
+    notification_1 = Notification(email="foo@example.com", message="Hello notification 1")
+    session.add(notification_1)
+    await session.commit()
+    return notification_1
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,30 @@
 from litestar import Litestar
 from litestar.status_codes import HTTP_200_OK
 from litestar.testing import TestClient
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app import Notification
 
 
 def test_homepage_title(test_client: TestClient[Litestar]) -> None:
     response = test_client.get("/")
     assert response.status_code == HTTP_200_OK
     assert "<title>Notification test</title>" in response.text
+
+
+def test_homepage_notifications_empty(
+    test_client: TestClient[Litestar], session: AsyncSession
+) -> None:
+    response = test_client.get("/notifications/test@example.com")
+    assert response.status_code == HTTP_200_OK
+    assert response.json() == []
+
+
+async def test_homepage_notifications_one(
+    test_client: TestClient[Litestar], session: AsyncSession, notification1: Notification
+) -> None:
+    response = test_client.get("/notifications/foo@example.com")
+    assert response.status_code == HTTP_200_OK
+    assert len(response.json()) == 1
+    assert response.json()[0]["email"] == "foo@example.com"
+    assert response.json()[0]["message"] == "Hello notification 1"
