444836af77c18924131fac0348777b9cdcd1fab5
David Larlet <david@larlet.fr>
2025-05-15 10:40:56 -0400

    Lint with biome: switch to 2 spaces indentation

--- public/admin.js
+++ public/admin.js
@@ -1,22 +1,22 @@
 const notifyMessage = async () => {
-    const payload = {
-        message: notifyMessageInput.value,
-        email: registerEmailInput.value,
-    }
-    console.log('notifying a message')
-    notifyMessageBtn.disabled = true
-    notifyMessageStatus.innerText = 'Notifying...'
-    const response = await fetch('/notification/send', {
-        method: 'POST',
-        body: JSON.stringify(payload),
-    })
-    console.log('response from AMI:', response)
-    notifyMessageBtn.disabled = false
-    if (response.status < 400) {
-        notifyMessageStatus.innerText = 'Done!'
-    } else {
-        notifyMessageStatus.innerText = `error ${response.status}: ${response.statusText}, ${response.body}`
-    }
+  const payload = {
+    message: notifyMessageInput.value,
+    email: registerEmailInput.value,
+  }
+  console.log('notifying a message')
+  notifyMessageBtn.disabled = true
+  notifyMessageStatus.innerText = 'Notifying...'
+  const response = await fetch('/notification/send', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  })
+  console.log('response from AMI:', response)
+  notifyMessageBtn.disabled = false
+  if (response.status < 400) {
+    notifyMessageStatus.innerText = 'Done!'
+  } else {
+    notifyMessageStatus.innerText = `error ${response.status}: ${response.statusText}, ${response.body}`
+  }
 }
 
 const registerEmailInput = document.querySelector('#register-email')
