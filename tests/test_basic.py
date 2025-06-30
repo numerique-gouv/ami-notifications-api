2c732559a410a1228b5eef2d578a97770e252570
Mathieu Agopian <mathieu@agopian.info>
2025-06-30 18:58:45 +0200

    Modify the `register` endpoint to accept an optional label and enabled status

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,13 @@
-from typing import Any
+import datetime
+import uuid
+from typing import Any, cast
 
+import pytest
 from litestar import Litestar
 from litestar.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 from litestar.testing import TestClient
 from pytest_httpx import HTTPXMock
+from sqlalchemy.orm import InstrumentedAttribute, selectinload
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -171,3 +175,65 @@ async def test_do_not_register_again_when_user_and_subscription_already_exist(
     # Still only one registration, no duplicates.
     all_registrations = await db_session.exec(select(Registration))
     assert len(all_registrations.all()) == 1  # The registration from the fixture.
+
+
+async def test_registration_default_fields(
+    test_client: TestClient[Litestar],
+    db_session: AsyncSession,
+    webpushsubscription: dict[str, Any],
+) -> None:
+    all_registrations = await db_session.exec(select(Registration))
+    assert len(all_registrations.all()) == 0
+
+    # First registration, we're expecting a 201 CREATED.
+    register_data = {"email": "foo@bar.baz", "subscription": webpushsubscription}
+    response = test_client.post("/notification/register", json=register_data)
+    assert response.status_code == HTTP_201_CREATED
+
+    # Make sure the registration has all the default fields set properly.
+    all_registrations = await db_session.exec(
+        select(Registration).options(
+            selectinload(cast(InstrumentedAttribute[Any], Registration.user))
+        )
+    )
+    registration = all_registrations.one()
+    assert registration.user.email == "foo@bar.baz"
+    try:
+        uuid.UUID(registration.label, version=4)
+    except ValueError:
+        pytest.fail(
+            f"Label should be initialized with a uuid4, but instead is set to: '{registration.label}'"
+        )
+    assert registration.enabled  # By default the registration is enabled.
+    assert registration.created_at < datetime.datetime.now()
+
+
+async def test_registration_custom_fields(
+    test_client: TestClient[Litestar],
+    db_session: AsyncSession,
+    webpushsubscription: dict[str, Any],
+) -> None:
+    all_registrations = await db_session.exec(select(Registration))
+    assert len(all_registrations.all()) == 0
+
+    # First registration, we're expecting a 201 CREATED.
+    register_data = {
+        "email": "foo@bar.baz",
+        "label": "foobar",
+        "enabled": False,
+        "subscription": webpushsubscription,
+    }
+    response = test_client.post("/notification/register", json=register_data)
+    assert response.status_code == HTTP_201_CREATED
+
+    # Make sure the registration has all the default fields set properly.
+    all_registrations = await db_session.exec(
+        select(Registration).options(
+            selectinload(cast(InstrumentedAttribute[Any], Registration.user))
+        )
+    )
+    registration = all_registrations.one()
+    assert registration.user.email == "foo@bar.baz"
+    assert registration.label == "foobar"
+    assert not registration.enabled
+    assert registration.created_at < datetime.datetime.now()
