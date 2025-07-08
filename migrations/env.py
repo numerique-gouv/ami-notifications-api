8b58fbf66efbe47740aae7cb0fbcf7814f63e337
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2025-07-08 16:26:33 +0200

    Extract methods with database operations in another file

--- migrations/env.py
+++ migrations/env.py
@@ -6,8 +6,8 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from app import SQLModel
 from app.database import DATABASE_URL
+from app.database_operations import SQLModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
