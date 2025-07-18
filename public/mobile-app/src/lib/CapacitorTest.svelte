2847542ce0ef3a41139bb1fece72f013307369a6
Mathieu Agopian <mathieu@agopian.info>
2025-07-18 16:33:47 +0200

    Very basic capacitorjs API calls

---
+++ public/mobile-app/src/lib/CapacitorTest.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { Capacitor } from '@capacitor/core'
+
+const platform = Capacitor.getPlatform()
+const isNativePlatform = Capacitor.isNativePlatform()
+</script>
+
+{#if (platform === 'ios')}
+  This is an iOS application!
+{:else if (platform === 'android')}
+  This is an Android application!
+{:else}
+  This is a Web {#if isNativePlatform}application (PWA)!{:else}page{/if}
+{/if}
