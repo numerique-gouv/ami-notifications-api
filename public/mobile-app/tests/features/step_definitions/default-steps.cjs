045003b33e1878a43c51a15d5828916c7bf94a36
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2025-08-04 18:38:34 +0200

    Tentative de créer les steps pour autocomplete etc.

---
+++ public/mobile-app/tests/features/step_definitions/default-steps.cjs
@@ -0,0 +1,27 @@
+const { Given,
+    Then,
+//    When
+} = require('@cucumber/cucumber');
+
+Given(
+	"l'usager se connecte sur AMI via France Connect en tant que {string} et suit le process de france connexion",
+	function (s) {
+		// Write code here that turns the phrase above into concrete actions
+	}
+);
+
+Given("l'usager est sur la home page de l'application AMI, non connecté", function () {
+	// Write code here that turns the phrase above into concrete actions
+});
+
+Then("l'usager devrait arriver sur la page de l'application AMI", function () {
+	// Write code here that turns the phrase above into concrete actions
+});
+
+Then(
+	"l'usager {string} devrait être enregistré(|e) dans les usagers notifiables avec son Id Pivot",
+	function (s) {
+		console.log(s);
+	}
+);
+
