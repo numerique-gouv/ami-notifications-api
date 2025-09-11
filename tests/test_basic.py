6e75a131edfc465a8a504553fe8fae1d76c564e5
Mathieu Agopian <mathieu@agopian.info>
2025-09-11 11:51:14 +0200

    Add tests for AMI login and fc_userinfo

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -315,10 +315,59 @@ async def test_enable_registration(
     assert result["enabled"] is True
 
 
+async def test_ami_login_callback(
+    test_client: TestClient[Litestar],
+    httpx_mock: HTTPXMock,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    fake_token_json_response = {
+        "access_token": "fake access token",
+        "expires_in": 60,
+        "id_token": "fake id token",
+        "scope": "openid given_name family_name preferred_username birthdate gender birthplace birthcountry email",
+        "token_type": "Bearer",
+    }
+    httpx_mock.add_response(
+        method="POST",
+        url="https://fcp-low.sbx.dev-franceconnect.fr/api/v2/token",
+        json=fake_token_json_response,
+    )
+    monkeypatch.setattr("app.FC_AMI_CLIENT_SECRET", "fake-client-secret")
+
+    response = test_client.get("/login-callback?code=fake-code", follow_redirects=False)
+
+    assert response.status_code == 302
+    redirected_url = response.headers["location"]
+    assert redirected_url.startswith("https://localhost:5173")
+    assert "access_token" in redirected_url
+    assert "scope" in redirected_url
+    assert "id_token" in redirected_url
+    assert "token_type" in redirected_url
+    assert "is_logged_in" in redirected_url
+
+
+async def test_ami_fc_get_userinfo(
+    test_client: TestClient[Litestar],
+    httpx_mock: HTTPXMock,
+) -> None:
+    fake_userinfo_token = "fake userinfo jwt token"
+    auth = {"authorization": "Bearer foobar_access_token"}
+    httpx_mock.add_response(
+        method="GET",
+        url="https://fcp-low.sbx.dev-franceconnect.fr/api/v2/userinfo",
+        match_headers=auth,
+        text=fake_userinfo_token,
+    )
+    response = test_client.get("/fc_userinfo", headers=auth)
+
+    assert response.status_code == 200
+    assert response.text == fake_userinfo_token
+
+
 async def test_rvo_login_callback(
     test_client: TestClient[Litestar],
     httpx_mock: HTTPXMock,
-    monkeypatch,  # type: ignore[reportUnknownParameterType]
+    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     fake_token_json_response = {
         "access_token": "fake access token",
@@ -344,7 +393,7 @@ async def test_rvo_login_callback(
     def fake_jwt_decode(*args: Any, **params: Any):
         return FAKE_USERINFO
 
-    monkeypatch.setattr("jwt.decode", fake_jwt_decode)  # type: ignore[reportUnknownMemberType]
+    monkeypatch.setattr("jwt.decode", fake_jwt_decode)
 
     response = test_client.get("/rvo/login-callback?code=fake-code")
 
