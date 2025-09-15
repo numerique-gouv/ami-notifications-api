523d29c2020c5970b4d41cd0eed73f6178ee0b31
Lauréline Guérin <zebuline@crocobox.org>
2025-09-15 16:03:07 +0200

    user birthdate is a date, not a datetime

--- app/models/endpoints.py
+++ app/models/endpoints.py
@@ -1,22 +1,22 @@
-from datetime import datetime
+import datetime
 from typing import Any
 
 
 class Userinfo:
-    aud: str  # "bc3ef03e2748a585005be828f9d1a9719c46df4e728115a106e6c8ee6ddcbacc"
-    birthcountry: int  # "99100"
-    birthdate: datetime  # "1962-08-24"
-    birthplace: int  # "75107"
-    email: str  # "wossewodda-3728@yopmail.com"
-    exp: str  # 1757583363
-    family_name: str  # "DUBOIS"
-    gender: str  # "female"
-    given_name: str  # "Angela Claire Louise"
+    aud: str
+    birthcountry: int
+    birthdate: datetime.date
+    birthplace: int
+    email: str
+    exp: str
+    family_name: str
+    gender: str
+    given_name: str
 
     def __init__(self, data: dict[str, Any]) -> None:
         self.aud = data["aud"]
         self.birthcountry = int(data["birthcountry"])
-        self.birthdate = datetime.strptime(data["birthdate"], "%Y-%m-%d")
+        self.birthdate = datetime.datetime.strptime(data["birthdate"], "%Y-%m-%d").date()
         self.birthplace = int(data["birthplace"])
         self.email = data["email"]
         self.exp = data["exp"]
