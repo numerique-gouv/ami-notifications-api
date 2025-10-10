eb10c5a0ff43652df1262891642da80fd3ff3066
Mathieu Agopian <mathieu@agopian.info>
2025-10-10 09:58:36 +0200

    Pretty print the age of a notification

---
+++ public/mobile-app/src/lib/prettyDate.test.js
@@ -0,0 +1,72 @@
+import { describe, expect, test } from 'vitest'
+import '@testing-library/jest-dom/vitest'
+import { prettyDate } from './prettyDate.ts'
+
+describe('prettyDate', () => {
+  test('should properly format seconds', async () => {
+    // Given
+    const now = new Date()
+    const seconds = 7
+    const before = new Date(now.getTime() - seconds * 1000)
+    // When
+    const pretty = prettyDate(before)
+
+    // Then
+    expect(pretty).equal('< 1h')
+  })
+  test('should properly format minutes', async () => {
+    // Given
+    const now = new Date()
+    const minutes = 7
+    const before = new Date(now.getTime() - minutes * 60 * 1000)
+    // When
+    const pretty = prettyDate(before)
+
+    // Then
+    expect(pretty).equal('< 1h')
+  })
+  test('should properly format hours', async () => {
+    // Given
+    const now = new Date()
+    const hours = 7
+    const before = new Date(now.getTime() - hours * 60 * 60 * 1000)
+    // When
+    const pretty = prettyDate(before)
+
+    // Then
+    expect(pretty).equal('7h')
+  })
+  test('should properly format days', async () => {
+    // Given
+    const now = new Date()
+    const days = 7
+    const before = new Date(now.getTime() - days * 24 * 60 * 60 * 1000)
+    // When
+    const pretty = prettyDate(before)
+
+    // Then
+    expect(pretty).equal('7j')
+  })
+  test('should properly format months', async () => {
+    // Given
+    const now = new Date()
+    const months = 7
+    const before = new Date(now.getTime() - months * 31 * 24 * 60 * 60 * 1000)
+    // When
+    const pretty = prettyDate(before)
+
+    // Then
+    expect(pretty).equal('7m')
+  })
+  test('should properly format years', async () => {
+    // Given
+    const now = new Date()
+    const years = 7
+    const before = new Date(now.getTime() - years * 365 * 24 * 60 * 60 * 1000)
+    // When
+    const pretty = prettyDate(before)
+
+    // Then
+    expect(pretty).equal('7a')
+  })
+})
---
+++ public/mobile-app/src/lib/prettyDate.ts
@@ -0,0 +1,26 @@
+/* The following was inspired by https://johnresig.com/blog/javascript-pretty-date/ */
+
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+export function prettyDate(data) {
+  let date
+  if (typeof data === 'string') {
+    const normalized = (data || '').replace(/-/g, '/').replace(/[TZ]/g, ' ')
+    date = new Date(normalized)
+  } else {
+    // We hope it's a proper Date object.
+    date = data
+  }
+
+  const diff = (new Date().getTime() - date.getTime()) / 1000
+  const day_diff = Math.floor(diff / 86400)
+  if (isNaN(day_diff) || day_diff < 0) return
+
+  return (
+    (day_diff == 0 &&
+      ((diff < 3600 && '< 1h') || (diff < 86400 && Math.floor(diff / 3600) + 'h'))) ||
+    (day_diff < 31 && day_diff + 'j') ||
+    (day_diff < 365 && Math.floor(day_diff / 31) + 'm') ||
+    Math.floor(day_diff / 365) + 'a'
+  )
+}
