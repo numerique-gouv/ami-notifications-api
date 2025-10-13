29c798a7c5e1b2b961de52f752b52d5a250300ac
Lauréline Guérin <zebuline@crocobox.org>
2025-10-13 18:30:06 +0200

    [AMI] extract and unit test api-particulier calls (#139)

---
+++ public/mobile-app/src/lib/api-particulier.ts
@@ -0,0 +1,22 @@
+import { PUBLIC_API_URL } from '$env/static/public'
+
+export const getQuotientData = async () => {
+  let quotientData = localStorage.getItem('quotient_data')
+  if (!quotientData) {
+    const access_token = localStorage.getItem('access_token')
+    const token_type = localStorage.getItem('token_type')
+    const quotient_endpoint_headers = {
+      Authorization: `${token_type} ${access_token}`,
+    }
+    const response = await fetch(`${PUBLIC_API_URL}/api-particulier/quotient`, {
+      headers: quotient_endpoint_headers,
+    })
+    quotientData = await response.text()
+    if (response.ok) {
+      localStorage.setItem('quotient_data', quotientData)
+    }
+  }
+  if (!!quotientData) {
+    return JSON.parse(quotientData)
+  }
+}
