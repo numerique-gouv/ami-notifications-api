71dfad6ca1bd34842ae81e5a5c84f480e550b76d
Lauréline Guérin <zebuline@crocobox.org>
2025-10-23 19:31:05 +0200

    [AMI] remove sqlmodel, use sqlachemy, pydantic and DTOs (#203)

--- app/models.py
+++ app/models.py
@@ -1,72 +1,95 @@
 import datetime
 from typing import Any
 
+from advanced_alchemy.extensions.litestar import SQLAlchemyDTO, SQLAlchemyDTOConfig
+from advanced_alchemy.types import JsonB
 from litestar.exceptions import NotFoundException
-from sqlalchemy.dialects.postgresql import JSONB
+from pydantic import BaseModel, Field
+from sqlalchemy import ForeignKey, select
 from sqlalchemy.exc import NoResultFound
-from sqlalchemy.orm import Mapped
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.sql.base import ExecutableOption
-from sqlmodel import Column, Field, Relationship, SQLModel, col, select
-from sqlmodel.ext.asyncio.session import AsyncSession
 
 
-class FCUserInfo(SQLModel, table=False):
-    birthcountry: int | None = Field(default=None)
-    birthdate: datetime.date | None = Field(default=None)
-    birthplace: int | None = Field(default=None)
-    email: str | None = Field(default=None)
-    family_name: str | None = Field(default=None)
-    gender: str | None = Field(default=None)
-    given_name: str | None = Field(default=None)
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "ami_user"  # type: ignore
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    birthcountry: Mapped[int | None]
+    birthdate: Mapped[datetime.date | None]
+    birthplace: Mapped[int | None]
+    email: Mapped[str | None]
+    family_name: Mapped[str | None]
+    gender: Mapped[str | None]
+    given_name: Mapped[str | None]
+    registrations: Mapped[list["Registration"]] = relationship(back_populates="user")
+    notifications: Mapped[list["Notification"]] = relationship(back_populates="user")
 
     @property
     def name(self):
         return f"{self.family_name} {self.given_name}"
 
 
-class User(FCUserInfo, table=True):
-    __tablename__ = "ami_user"  # type: ignore
-
-    id: int | None = Field(default=None, primary_key=True)
-    registrations: Mapped[list["Registration"]] = Relationship(back_populates="user")
-    notifications: Mapped[list["Notification"]] = Relationship(back_populates="user")
-
-
-class RegistrationBase(SQLModel):
-    user_id: int = Field(foreign_key="ami_user.id")
-    subscription: dict[str, Any] = Field(sa_column=Column(JSONB))
+class FCUserInfo(BaseModel):
+    birthcountry: int | None
+    birthdate: datetime.date | None = None
+    birthplace: int | None = None
+    email: str | None = None
+    family_name: str | None = None
+    gender: str | None = None
+    given_name: str | None = None
 
 
-class Registration(RegistrationBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    user: User = Relationship(back_populates="registrations")
-    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
+class Registration(Base):
+    __tablename__ = "registration"  # type: ignore
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("ami_user.id"))
+    user: Mapped[User] = relationship(back_populates="registrations")
+    subscription: Mapped[dict[str, Any]] = mapped_column(JsonB, nullable=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now)
 
 
-class RegistrationCreate(RegistrationBase):
-    pass
+class RegistrationCreate(BaseModel):
+    user_id: int
+    subscription: dict[str, Any]
 
 
-class NotificationBase(SQLModel):
-    user_id: int = Field(foreign_key="ami_user.id")
+class RegistrationDTO(SQLAlchemyDTO[Registration]):
+    config = SQLAlchemyDTOConfig(exclude={"user"})
+
+
+class Notification(Base):
+    __tablename__ = "notification"  # type: ignore
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("ami_user.id"))
+    user: Mapped[User] = relationship(back_populates="notifications")
+    message: Mapped[str]
+    sender: Mapped[str | None]
+    title: Mapped[str | None]
+    date: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now)
+    unread: Mapped[bool] = mapped_column(default=True)
+
+
+class NotificationCreate(BaseModel):
+    user_id: int
     message: str = Field(min_length=1)
-    sender: str | None = Field(default=None)
-    title: str | None = Field(default=None)
+    sender: str | None
+    title: str | None
 
 
-class Notification(NotificationBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    date: datetime.datetime = Field(default_factory=datetime.datetime.now)
-    user: User = Relationship(back_populates="notifications")
-    unread: bool = Field(default=True)
+class NotificationRead(BaseModel):
+    read: bool
 
 
-class NotificationCreate(NotificationBase):
-    pass
-
-
-class NotificationRead(SQLModel):
-    read: bool = Field()
+class NotificationDTO(SQLAlchemyDTO[Notification]):
+    config = SQLAlchemyDTOConfig(exclude={"user"})
 
 
 #### USERS
@@ -75,12 +98,12 @@ class NotificationRead(SQLModel):
 async def get_user_by_id(
     user_id: int, db_session: AsyncSession, options: ExecutableOption | None = None
 ) -> User:
-    query = select(User).where(col(User.id) == user_id)
+    query = select(User).where(User.id == user_id)
     if options:
         query = query.options(options)
-    result = await db_session.exec(query)
+    result = await db_session.execute(query)
     try:
-        return result.one()
+        return result.scalar_one()
     except NoResultFound as e:
         raise NotFoundException(detail=f"User with id {user_id!r} not found") from e
 
@@ -88,13 +111,15 @@ async def get_user_by_id(
 async def get_user_by_userinfo(
     userinfo: FCUserInfo, db_session: AsyncSession, options: ExecutableOption | None = None
 ) -> User:
-    constraints = [col(getattr(User, field)) == value for (field, value) in vars(userinfo).items()]
+    constraints = [
+        getattr(User, field) == value for (field, value) in userinfo.model_dump().items()
+    ]
     query = select(User).where(*constraints)
     if options:
         query = query.options(options)
-    result = await db_session.exec(query)
+    result = await db_session.execute(query)
     try:
-        return result.one()
+        return result.scalar_one()
     except NoResultFound as e:
         raise NotFoundException(detail=f"User {userinfo.given_name!r} not found") from e
 
@@ -106,9 +131,9 @@ async def get_user_list(
     query = select(User)
     if options:
         query = query.options(options)
-    query = query.order_by(col(User.id))
-    result = await db_session.exec(query)
-    return list(result.all())
+    query = query.order_by(User.id)
+    result = await db_session.execute(query)
+    return list(result.scalars().all())
 
 
 async def create_user_from_userinfo(user: User, db_session: AsyncSession) -> User:
@@ -124,12 +149,12 @@ async def create_user_from_userinfo(user: User, db_session: AsyncSession) -> Use
 async def get_notification_list_by_user(
     user: User, db_session: AsyncSession, unread: bool | None = None
 ) -> list[Notification]:
-    query = select(Notification).where(col(Notification.user) == user)
+    query = select(Notification).where(Notification.user == user)
     if unread is not None:
-        query = query.where(col(Notification.unread) == unread)
-    query = query.order_by(col(Notification.date).desc())
-    result = await db_session.exec(query)
-    return list(result.all())
+        query = query.where(Notification.unread == unread)
+    query = query.order_by(Notification.date.desc())
+    result = await db_session.execute(query)
+    return list(result.scalars().all())
 
 
 async def create_notification(notification: Notification, db_session: AsyncSession) -> Notification:
@@ -143,11 +168,11 @@ async def get_notification_by_id_and_user(
     notification_id: int, user: User, db_session: AsyncSession
 ) -> Notification:
     query = select(Notification).where(
-        col(Notification.id) == notification_id, col(Notification.user) == user
+        Notification.id == notification_id, Notification.user == user
     )
-    result = await db_session.exec(query)
+    result = await db_session.execute(query)
     try:
-        return result.one()
+        return result.scalar_one()
     except NoResultFound as e:
         raise NotFoundException(
             detail=f"Notification with id {notification_id!r} not found for the user"
@@ -168,10 +193,10 @@ async def get_registration_by_user_and_subscription(
     subscription: dict[str, Any], db_session: AsyncSession, user: User
 ) -> Registration | None:
     query = select(Registration).where(
-        col(Registration.user) == user, col(Registration.subscription) == subscription
+        Registration.user == user, Registration.subscription == subscription
     )
-    result = await db_session.exec(query)
-    existing_registration = result.first()
+    result = await db_session.execute(query)
+    existing_registration = result.scalars().first()
     return existing_registration
 
 
