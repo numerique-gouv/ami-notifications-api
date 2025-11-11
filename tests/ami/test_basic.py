fe6cf8cb3c57057dcfbf4b978f027a19d0b14355
Lauréline Guérin <zebuline@crocobox.org>
2025-11-11 10:10:28 +0100

    [AMI] router for external data as api-particulier (#225)

--- tests/ami/test_basic.py
+++ tests/ami/test_basic.py
@@ -771,24 +771,6 @@ async def test_fc_get_userinfo(
     }
 
 
-async def test_get_api_particulier_quotient(
-    test_client: TestClient[Litestar],
-    httpx_mock: HTTPXMock,
-) -> None:
-    fake_quotient_data = {"foo": "bar"}
-    auth = {"authorization": "Bearer foobar_access_token"}
-    httpx_mock.add_response(
-        method="GET",
-        url="https://staging.particulier.api.gouv.fr/v3/dss/quotient_familial/france_connect?recipient=13002526500013",
-        match_headers=auth,
-        json=fake_quotient_data,
-    )
-    response = test_client.get("/api-particulier/quotient", headers=auth)
-
-    assert response.status_code == 200
-    assert json.loads(response.text) == fake_quotient_data
-
-
 async def test_get_sector_identifier_url(
     test_client: TestClient[Litestar],
     monkeypatch: pytest.MonkeyPatch,
