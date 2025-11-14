aa2aeed644087fab3c1159b1022a617f486d8e05
Lauréline Guérin <zebuline@crocobox.org>
2025-11-14 16:11:55 +0100

    [AMI] get and display holidays in front (#225)

--- public/mobile-app/src/routes/agenda/+page.svelte
+++ public/mobile-app/src/routes/agenda/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 import Navigation from '$lib/Navigation.svelte'
 import { onMount } from 'svelte'
+import { goto } from '$app/navigation'
+import { dayName, monthName, holidayPeriod, retrieveHolidays } from '$lib/api-holidays'
+import type { Holiday } from '$lib/api-holidays'
 
 let isFranceConnected: boolean = $state(false)
+let holidays: Object = $state({ now: [] as Holiday[], next: [] as Holiday[] })
 
 onMount(async () => {
   isFranceConnected = !!localStorage.getItem('access_token')
   if (!isFranceConnected) {
     goto('/')
   }
+
+  holidays = await retrieveHolidays()
+  console.log($state.snapshot(holidays))
 })
 </script>
 
@@ -20,93 +27,88 @@ onMount(async () => {
     </div>
   </div>
 
-  <div class="agenda--events">
+  {#if holidays.now.length}
+  <div class="agenda--events" data-testid="events-now">
     <div class="agenda--events--header">
-      <span class="title">Ce mois-ci</span>
+      <span class="title">D'ici un mois</span>
     </div>
     <div class="agenda--events--container">
-      <div class="agenda--event">
-        <div class="agenda--event--date">
-          <span class="day-name">sam</span> 
-          <span class="day-num">20</span> 
+      {#each holidays.now as holiday, i}
+        {#if i == 0 || i > 0 && holiday.start_date.getMonth() !== holidays.now[i - 1].start_date.getMonth()}
+        <div class="agenda--events--month">
+          {monthName(holiday.start_date)}
         </div>
-        <div class="agenda--event--detail fr-tile fr-tile-sm fr-tile--horizontal fr-enlarge-link">
-          <div class="fr-tile__body">
-            <div class="fr-tile__content">
-              <h3 class="fr-tile__title">
-                <a href="/#/agenda/">Vacances de Noël 🎄</a>
-              </h3>
-              <div class="fr-tile__start">
-                <p class="fr-badge">
-                  <img src="/remixicons/calendar-event-line.svg" alt="Icône de calendrier" />
-                  Vacances et jours fériés
-                </p>
-                <p class="fr-tag">
-                  Du 20 décembre au 5 janvier
-                </p>
+        {/if}
+        <div class="agenda--event">
+          <div class="agenda--event--date">
+            <span class="day-name">{dayName(holiday.start_date)}</span> 
+            <span class="day-num">{holiday.start_date.getDate()}</span> 
+          </div>
+          <div class="agenda--event--detail fr-tile fr-tile-sm fr-tile--horizontal fr-enlarge-link">
+            <div class="fr-tile__body">
+              <div class="fr-tile__content">
+                <h3 class="fr-tile__title">
+                  <a href="/#/agenda/">{holiday.description} {holiday.zones} {holiday.emoji}</a>
+                </h3>
+                <div class="fr-tile__start">
+                  <p class="fr-badge">
+                    <img src="/remixicons/calendar-event-line.svg" alt="Icône de calendrier" />
+                    Vacances et jours fériés
+                  </p>
+                  <p class="fr-tag">
+                    {holidayPeriod(holiday.start_date, holiday.end_date)}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      {/each}
     </div>
   </div>
+  {/if}
 
-  <div class="agenda--events">
+  {#if holidays.next.length}
+  <div class="agenda--events" data-testid="events-next">
     <div class="agenda--events--header">
       <span class="title">Prochainement</span>
     </div>
     <div class="agenda--events--container">
-      <div class="agenda--event">
-        <div class="agenda--event--date">
-          <span class="day-name">sam</span> 
-          <span class="day-num">7</span> 
+      {#each holidays.next as holiday, i}
+        {#if i > 0 && holiday.start_date.getMonth() !== holidays.next[i - 1].start_date.getMonth() || i == 0 && (holidays.now.length && holiday.start_date.getMonth() !== holidays.now[holidays.now.length - 1].start_date.getMonth() || !holidays.now.length)}
+        <div class="agenda--events--month">
+          {monthName(holiday.start_date)}
         </div>
-        <div class="agenda--event--detail fr-tile fr-tile-sm fr-tile--horizontal fr-enlarge-link">
-          <div class="fr-tile__body">
-            <div class="fr-tile__content">
-              <h3 class="fr-tile__title">
-                <a href="/#/agenda/">Vacances d’hiver Zone A ❄️</a>
-              </h3>
-              <div class="fr-tile__start">
-                <p class="fr-badge">
-                  <img src="/remixicons/calendar-event-line.svg" alt="Icône de calendrier" />
-                  Vacances et jours fériés
-                </p>
-                <p class="fr-tag">
-                  Du 7 au 23 février 2026
-                </p>
+        {/if}
+        <div class="agenda--event">
+          <div class="agenda--event--date">
+            <span class="day-name">{dayName(holiday.start_date)}</span> 
+            <span class="day-num">{holiday.start_date.getDate()}</span> 
+          </div>
+          <div class="agenda--event--detail fr-tile fr-tile-sm fr-tile--horizontal fr-enlarge-link">
+            <div class="fr-tile__body">
+              <div class="fr-tile__content">
+                <h3 class="fr-tile__title">
+                  <a href="/#/agenda/">{holiday.description} {holiday.zones} {holiday.emoji}</a>
+                </h3>
+                <div class="fr-tile__start">
+                  <p class="fr-badge">
+                    <img src="/remixicons/calendar-event-line.svg" alt="Icône de calendrier" />
+                    Vacances et jours fériés
+                  </p>
+                  <p class="fr-tag">
+                    {holidayPeriod(holiday.start_date, holiday.end_date)}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="agenda--event">
-        <div class="agenda--event--date">
-          <span class="day-name">sam</span> 
-          <span class="day-num">14</span> 
-        </div>
-        <div class="agenda--event--detail fr-tile fr-tile-sm fr-tile--horizontal fr-enlarge-link">
-          <div class="fr-tile__body">
-            <div class="fr-tile__content">
-              <h3 class="fr-tile__title">
-                <a href="/#/agenda/">Vacances d’hiver Zone B ❄️</a>
-              </h3>
-              <div class="fr-tile__start">
-                <p class="fr-badge">
-                  <img src="/remixicons/calendar-event-line.svg" alt="Icône de calendrier" />
-                  Vacances et jours fériés
-                </p>
-                <p class="fr-tag">
-                  Du 14 février au 2 mars 2026
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/each}
     </div>
   </div>
+  {/if}
+
 </div>
 <Navigation currentItem="agenda" />
 
@@ -127,13 +129,19 @@ onMount(async () => {
     }
     .agenda--events {
       .agenda--events--header {
-        margin-bottom: 1.5rem;
+        margin-bottom: 0.75rem;
         .title {
           font-weight: 500;
           color: #000;
         }
       }
       .agenda--events--container {
+        .agenda--events--month {
+          font-weight: 500;
+          color: #000;
+          font-size: 14px;
+          margin-bottom: 0.5rem;
+        }
         .agenda--event {
           display: flex;
           .agenda--event--date {
@@ -151,7 +159,7 @@ onMount(async () => {
             }
           }
           .agenda--event--detail {
-            margin-bottom: 1.5rem;
+            margin-bottom: 0.75rem;
             padding: 1.5rem;
             padding-bottom: 1rem;
             width: 100%;
---
+++ public/mobile-app/src/routes/agenda/page.svelte.test.ts
@@ -0,0 +1,267 @@
+import { describe, test, expect, vi } from 'vitest'
+import { render, screen } from '@testing-library/svelte'
+import Page from './+page.svelte'
+import * as navigationMethods from '$app/navigation'
+import * as holidayMethods from '$lib/api-holidays'
+import { monthName } from '$lib/api-holidays'
+
+const oneday_in_ms = 24 * 60 * 60 * 1000
+const today = new Date()
+const in15days = new Date(today.getTime() + 15 * oneday_in_ms)
+const in32days = new Date(today.getTime() + 32 * oneday_in_ms) // 32 days, so we are sure that month is different than today's
+const in45days = new Date(today.getTime() + 45 * oneday_in_ms)
+const in60days = new Date(today.getTime() + 60 * oneday_in_ms)
+
+describe('/+page.svelte', () => {
+  test('user has to be connected', () => {
+    // Given
+    expect(window.localStorage.getItem('access_token')).toEqual(null)
+    const spy = vi.spyOn(navigationMethods, 'goto').mockImplementation(() => 'mocked')
+
+    // When
+    render(Page)
+
+    // Then
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith('/')
+  })
+  test('Should display holidays from API', async () => {
+    // Given
+    window.localStorage.setItem('access_token', 'fake-access-token')
+    const spy = vi
+      .spyOn(holidayMethods, 'retrieveHolidays')
+      .mockImplementation(async () => {
+        return {
+          now: [
+            {
+              description: 'Holiday 1',
+              start_date: today,
+              end_date: in15days,
+            },
+          ],
+          next: [
+            {
+              description: 'Holiday 2',
+              start_date: in32days,
+              end_date: in45days,
+            },
+          ],
+        }
+      })
+
+    // When
+    render(Page)
+    await new Promise(setTimeout) // wait for async calls
+
+    // Then
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(screen.getByTestId('events-now')).toHaveTextContent("D'ici un mois")
+    expect(screen.getByTestId('events-now')).toHaveTextContent(monthName(today))
+    expect(screen.getByTestId('events-now')).toHaveTextContent('Holiday 1')
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Prochainement')
+    expect(screen.getByTestId('events-next')).toHaveTextContent(monthName(in32days))
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Holiday 2')
+  })
+  test('Should not display "next" section if empty', async () => {
+    // Given
+    window.localStorage.setItem('access_token', 'fake-access-token')
+    const spy = vi
+      .spyOn(holidayMethods, 'retrieveHolidays')
+      .mockImplementation(async () => {
+        return {
+          now: [],
+          next: [
+            {
+              description: 'Holiday 2',
+              start_date: in32days,
+              end_date: in45days,
+            },
+          ],
+        }
+      })
+
+    // When
+    render(Page)
+    await new Promise(setTimeout) // wait for async calls
+
+    // Then
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(screen.queryByTestId('events-now')).toBeNull()
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Prochainement')
+    expect(screen.getByTestId('events-next')).toHaveTextContent(monthName(in32days))
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Holiday 2')
+  })
+  test('Should not display "next" section if empty', async () => {
+    // Given
+    window.localStorage.setItem('access_token', 'fake-access-token')
+    const spy = vi
+      .spyOn(holidayMethods, 'retrieveHolidays')
+      .mockImplementation(async () => {
+        return {
+          now: [
+            {
+              description: 'Holiday 1',
+              start_date: today,
+              end_date: in15days,
+            },
+          ],
+          next: [],
+        }
+      })
+
+    // When
+    render(Page)
+    await new Promise(setTimeout) // wait for async calls
+
+    // Then
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(screen.getByTestId('events-now')).toHaveTextContent("D'ici un mois")
+    expect(screen.getByTestId('events-now')).toHaveTextContent(monthName(today))
+    expect(screen.getByTestId('events-now')).toHaveTextContent('Holiday 1')
+    expect(screen.queryByTestId('events-next')).toBeNull()
+  })
+  test('Should not repeat month', async () => {
+    const spy = vi
+      .spyOn(holidayMethods, 'retrieveHolidays')
+      .mockImplementation(async () => {
+        return {
+          now: [
+            {
+              description: 'Holiday 1',
+              start_date: today,
+              end_date: in15days,
+            },
+            {
+              description: 'Holiday 2',
+              start_date: today,
+              end_date: in32days,
+            },
+          ],
+          next: [
+            {
+              description: 'Holiday 3',
+              start_date: in32days,
+              end_date: in45days,
+            },
+            {
+              description: 'Holiday 4',
+              start_date: in32days,
+              end_date: in60days,
+            },
+          ],
+        }
+      })
+
+    // When
+    render(Page)
+    await new Promise(setTimeout) // wait for async calls
+
+    // Then
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(screen.getByTestId('events-now')).toHaveTextContent("D'ici un mois")
+    expect(screen.getByTestId('events-now')).toHaveTextContent(monthName(today))
+    const today_month_occurrences = (
+      screen
+        .getByTestId('events-now')
+        .textContent.match(new RegExp(monthName(today), 'g')) || []
+    ).length
+    expect(today_month_occurrences).toBe(1)
+    expect(screen.getByTestId('events-now')).toHaveTextContent('Holiday 1')
+    expect(screen.getByTestId('events-now')).toHaveTextContent('Holiday 2')
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Prochainement')
+    expect(screen.getByTestId('events-next')).toHaveTextContent(monthName(in32days))
+    const in32days_month_occurrences = (
+      screen
+        .getByTestId('events-now')
+        .textContent.match(new RegExp(monthName(today), 'g')) || []
+    ).length
+    expect(in32days_month_occurrences).toBe(1)
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Holiday 3')
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Holiday 4')
+  })
+  test('Should not repeat month (with "next" part empty)', async () => {
+    // Given
+    window.localStorage.setItem('access_token', 'fake-access-token')
+    const spy = vi
+      .spyOn(holidayMethods, 'retrieveHolidays')
+      .mockImplementation(async () => {
+        return {
+          now: [],
+          next: [
+            {
+              description: 'Holiday 1',
+              start_date: in32days,
+              end_date: in45days,
+            },
+            {
+              description: 'Holiday 2',
+              start_date: in32days,
+              end_date: in60days,
+            },
+          ],
+        }
+      })
+
+    // When
+    render(Page)
+    await new Promise(setTimeout) // wait for async calls
+
+    // Then
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(screen.queryByTestId('events-now')).toBeNull()
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Prochainement')
+    expect(screen.getByTestId('events-next')).toHaveTextContent(monthName(in32days))
+    const in32days_month_occurrences = (
+      screen
+        .getByTestId('events-next')
+        .textContent.match(new RegExp(monthName(in32days), 'g')) || []
+    ).length
+    expect(in32days_month_occurrences).toBe(1)
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Holiday 1')
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Holiday 2')
+  })
+  test('Should not repeat month (when last "next" event month is the same as first "next" event month)', async () => {
+    // Given
+    window.localStorage.setItem('access_token', 'fake-access-token')
+    var start1 = today
+    var start2 = new Date(today.getTime() + 1 * oneday_in_ms)
+    if (start1.getMonth() != start2.getMonth()) {
+      start1 = new Date(today.getTime() + 1 * oneday_in_ms)
+      start2 = new Date(today.getTime() + 2 * oneday_in_ms)
+    }
+    const spy = vi
+      .spyOn(holidayMethods, 'retrieveHolidays')
+      .mockImplementation(async () => {
+        return {
+          now: [
+            {
+              description: 'Holiday 1',
+              start_date: start1,
+              end_date: start2,
+            },
+          ],
+          next: [
+            {
+              description: 'Holiday 2',
+              start_date: start2,
+              end_date: in32days,
+            },
+          ],
+        }
+      })
+
+    // When
+    render(Page)
+    await new Promise(setTimeout) // wait for async calls
+
+    // Then
+    expect(monthName(start1)).toEqual(monthName(start2))
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(screen.getByTestId('events-now')).toHaveTextContent("D'ici un mois")
+    expect(screen.getByTestId('events-now')).toHaveTextContent(monthName(start1))
+    expect(screen.getByTestId('events-now')).toHaveTextContent('Holiday 1')
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Prochainement')
+    expect(screen.getByTestId('events-next')).not.toHaveTextContent(monthName(start2))
+    expect(screen.getByTestId('events-next')).toHaveTextContent('Holiday 2')
+  })
+})
