f7b3a0b796e8a59a29fd9185636f072a41f32fc3
Lauréline Guérin <zebuline@crocobox.org>
2025-11-14 16:10:30 +0100

    [AMI] endpoint to get holidays from zones A, B, C (#225)

--- app/data/routes.py
+++ app/data/routes.py
@@ -1,9 +1,11 @@
+import datetime
 from typing import Any
 
 import httpx
 from litestar import Request, Response, Router, get
 
 from app import env
+from app.schemas import Holiday
 
 
 @get(path="/api-particulier/quotient", include_in_schema=False)
@@ -23,9 +25,45 @@ async def get_api_particulier_quotient(
     return Response(response.content, status_code=response.status_code)
 
 
+@get(path="/holidays", include_in_schema=False)
+async def get_holidays(
+    request: Request[Any, Any, Any],
+    current_date: datetime.date,
+) -> list[Holiday]:
+    # target one region per zone, to limit results
+    locations = ["Bordeaux", "Lille", "Versailles"]
+    locations_query = " OR ".join(f"location = '{location}'" for location in locations)
+
+    # if the school year that has just begun, take holidays until the end of the current school year
+    # else, take the holidays until the end of the following school year
+    end_date = f"{current_date.year + 1}-09-15"
+
+    response = httpx.get(
+        f"{env.PUBLIC_API_DATA_EDUCATION_BASE_URL}{env.PUBLIC_API_DATA_EDUCATION_HOLIDAYS_ENDPOINT}",
+        params={
+            "where": f"end_date >= date'{current_date}' AND start_date < date'{end_date}' AND ({locations_query}) AND population IN ('-', 'Élèves')",
+            "order_by": "start_date",
+            "limit": 100,
+        },
+    )
+
+    holidays: dict[Any, Holiday] = {}
+    for data in response.json()["results"]:
+        holiday = Holiday.from_dict(data)
+        key = (holiday.description, holiday.start_date, holiday.end_date)
+        if key in holidays:
+            # if the dates are the same for all zones, keep only one result and clear zones
+            holidays[key].zones = ""
+            continue
+        holidays[key] = holiday
+
+    return sorted(holidays.values(), key=lambda a: a.start_date)
+
+
 data_router: Router = Router(
     path="/data",
     route_handlers=[
         get_api_particulier_quotient,
+        get_holidays,
     ],
 )
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+from dataclasses import dataclass, fields
 from enum import Enum
 from typing import Any
 
@@ -60,3 +61,29 @@ class Registration(BaseModel):
 class RegistrationCreate(BaseModel):
     user_id: uuid.UUID
     subscription: dict[str, Any]
+
+
+@dataclass
+class Holiday:
+    description: str
+    zones: str
+    start_date: datetime.datetime
+    end_date: datetime.datetime
+    emoji: str
+
+    emoji_mapping = {
+        "Vacances de la Toussaint": "🍁",
+        "Vacances de Noël": "🎄",
+        "Vacances d'Hiver": "❄️",
+        "Vacances de Printemps": "🌸",
+        "Vacances d'Été": "☀️",
+    }
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]) -> "Holiday":
+        cls_fields = {f.name for f in fields(cls)}
+        filtered = {k: v for k, v in data.items() if k in cls_fields}
+        filtered["start_date"] = datetime.datetime.fromisoformat(filtered["start_date"])
+        filtered["end_date"] = datetime.datetime.fromisoformat(filtered["end_date"])
+        filtered["emoji"] = cls.emoji_mapping.get(filtered["description"], "")
+        return cls(**filtered)
