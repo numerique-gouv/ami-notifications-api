bc1a1d971b66718c707d603b2da7612b6c8afc1c
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2025-11-17 15:42:56 +0100

    Add form page in front end (#228)

--- public/mobile-app/src/lib/ConnectedHomepage.svelte
+++ public/mobile-app/src/lib/ConnectedHomepage.svelte
@@ -164,7 +164,7 @@ const clickLogout = async () => {
           <div class="fr-tile__content">
             <img class="address-icon" src="/remixicons/house.svg" alt="Icône adresse" />
             <h3 class="fr-tile__title">
-              <a href="/"><b>Gagnez du temps</b> en <b>renseignant votre adresse</b> une seule fois !</a>
+              <a href="/#/adresse"><b>Gagnez du temps</b> en <b>renseignant votre adresse</b> une seule fois !</a>
             </h3>
           </div>
         </div>
