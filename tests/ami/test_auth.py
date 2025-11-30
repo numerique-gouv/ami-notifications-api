a900b4503f074f67b00a762b6f60a95f3a770eae
Lauréline Guérin <zebuline@crocobox.org>
2025-11-30 17:33:26 +0100

    [AMI] endpoint to check user authentication (#314)

--- tests/ami/test_auth.py
+++ tests/ami/test_auth.py
@@ -333,3 +333,19 @@ async def test_logout_without_auth(
     test_client: TestClient[Litestar],
 ) -> None:
     await assert_query_fails_without_auth("/logout", test_client, method="post")
+
+
+async def test_check_auth(
+    user: User,
+    test_client: TestClient[Litestar],
+) -> None:
+    login(user, test_client)
+    response = test_client.get("/check-auth")
+    assert response.status_code == 200
+    assert response.json() == {"authenticated": True}
+
+
+async def test_check_auth_without_auth(
+    test_client: TestClient[Litestar],
+) -> None:
+    await assert_query_fails_without_auth("/check-auth", test_client)
