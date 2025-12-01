43d82498c6c382ef7332e24f6bb1acef37413e36
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2025-12-01 16:53:26 +0100

    Add idBAN in address object (#228)

--- public/mobile-app/src/routes/adresse/page.svelte.test.ts
+++ public/mobile-app/src/routes/adresse/page.svelte.test.ts
@@ -10,6 +10,7 @@ describe('/+page.svelte', () => {
         {
           city: 'Orléans',
           context: '45, Loiret, Centre-Val de Loire',
+          idBAN: '45234_0420_00023',
           label: '23 Rue des Aubépines 45100 Orléans',
           name: '23 Rue des Aubépines',
           postcode: '45100',
@@ -17,6 +18,7 @@ describe('/+page.svelte', () => {
         {
           city: 'Orly',
           context: '94, Val-de-Marne, Île-de-France',
+          idBAN: '94054_0070_00023',
           label: '23 Rue des Aubépines 94310 Orly',
           name: '23 Rue des Aubépines',
           postcode: '94310',
@@ -24,6 +26,7 @@ describe('/+page.svelte', () => {
         {
           city: 'Orléat',
           context: '63, Puy-de-Dôme, Auvergne-Rhône-Alpes',
+          idBAN: '63265_0008',
           label: 'Allée des Aubépines 63190 Orléat',
           name: 'Allée des Aubépines',
           postcode: '63190',
