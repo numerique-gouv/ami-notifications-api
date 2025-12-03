060e6d7b6c4fb6f09c58e2cdc8d3645d3fa18fc1
Mathieu Agopian <mathieu@agopian.info>
2025-12-03 10:26:21 +0100

    FranceConnect button should not be stretched

--- public/mobile-app/src/routes/+page.svelte
+++ public/mobile-app/src/routes/+page.svelte
@@ -207,14 +207,6 @@ function dismissError() {
 
       .fr-connect-group {
         text-align: center;
-        width: 100%;
-
-        .fr-connect {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          width: 100%;
-        }
       }
     }
   }
