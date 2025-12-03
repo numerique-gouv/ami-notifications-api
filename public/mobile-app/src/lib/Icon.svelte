131385f6c8cf79133ee410b76d13b6e546d0cf61
Mathieu Agopian <mathieu@agopian.info>
2025-12-03 16:23:33 +0100

    Add a logout icon in the user's dropdown menu (#262)
    Add an icon for the notifications and for the logout in the user's dropdown menu

---
+++ public/mobile-app/src/lib/Icon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+interface Props {
+  href: string
+  alt: string
+  className: string
+}
+let { alt, className, href }: Props = $props()
+</script>
+
+<span class="icon {className}" style="--svg: url('{href}')"></span>
+
+<style>
+.icon {
+  background-color: var(--text-active-blue-france);
+  mask: var(--svg) no-repeat center;
+  mask-size: contain;
+  width: 20px;
+  height: 20px;
+}
+</style>
