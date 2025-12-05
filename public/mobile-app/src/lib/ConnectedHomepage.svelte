11a6539c4db88ff0f26bfa255423eb44780ad90c
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2025-12-05 15:09:30 +0100

    Updates after code review: change adresse to address (#269)

--- public/mobile-app/src/lib/ConnectedHomepage.svelte
+++ public/mobile-app/src/lib/ConnectedHomepage.svelte
@@ -146,7 +146,7 @@ const clickDisableNotifications = async () => {
             <div class="fr-tile__content">
               <img class="address-icon" src="/remixicons/house.svg" alt="Icône adresse" />
               <h3 class="fr-tile__title">
-                <a href="/#/adresse"><b>Gagnez du temps</b> en <b>renseignant votre adresse</b> une seule fois !</a>
+                <a href="/#/address"><b>Gagnez du temps</b> en <b>renseignant votre adresse</b> une seule fois !</a>
               </h3>
             </div>
           </div>
