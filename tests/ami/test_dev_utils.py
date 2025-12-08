c6bdb1fa9c527ab85c9914ee9c44fa0a5f773c2b
Lauréline Guérin <zebuline@crocobox.org>
2025-12-08 09:43:17 +0100

    [AMI] rename endpoint (#328)

--- tests/ami/test_dev_utils.py
+++ tests/ami/test_dev_utils.py
@@ -2,7 +2,7 @@ from litestar import Litestar
 from litestar.testing import TestClient
 
 
-async def test_ami_hash(
+async def test_recipient_fc_hash(
     test_client: TestClient[Litestar],
 ) -> None:
     params = {
@@ -13,5 +13,5 @@ async def test_ami_hash(
         "birthplace": "75107",
         "birthcountry": "99100",
     }
-    response = test_client.get("/dev-utils/ami-hash", params=params)
+    response = test_client.get("/dev-utils/recipient-fc-hash", params=params)
     assert response.text == "4abd71ec1f581dce2ea2221cbeac7c973c6aea7bcb835acdfe7d6494f1528060"
