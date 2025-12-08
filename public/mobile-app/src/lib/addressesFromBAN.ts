0169ca6c3e4c49e5d6527bbaebb6c03e19783a04
Lauréline Guérin <zebuline@crocobox.org>
2025-12-08 15:05:05 +0100

    [AMI] simplify AddressFromBAN constructor (#227)

--- public/mobile-app/src/lib/addressesFromBAN.ts
+++ public/mobile-app/src/lib/addressesFromBAN.ts
@@ -23,28 +23,14 @@ export type ResponseFromBAN = {
 }
 
 export class AddressFromBAN {
-  private _city: string = ''
-  private _context: string = ''
-  private _id: string = ''
-  private _label: string = ''
-  private _name: string = ''
-  private _postcode: string = ''
-
   constructor(
-    city: string,
-    context: string,
-    id: string,
-    label: string,
-    name: string,
-    postcode: string
-  ) {
-    this._city = city
-    this._context = context
-    this._id = id
-    this._label = label
-    this._name = name
-    this._postcode = postcode
-  }
+    private _city: string = '',
+    private _context: string = '',
+    private _id: string = '',
+    private _label: string = '',
+    private _name: string = '',
+    private _postcode: string = ''
+  ) {}
 
   get city(): string {
     return this._city
