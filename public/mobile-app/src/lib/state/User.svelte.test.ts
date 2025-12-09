951623ccad1025d2eca2577d2e60eac70b8a8729
Mathieu Agopian <mathieu@agopian.info>
2025-12-09 09:51:34 +0100

    @cdesquilbet-dila second review

--- public/mobile-app/src/lib/state/User.svelte.test.ts
+++ public/mobile-app/src/lib/state/User.svelte.test.ts
@@ -1,13 +1,9 @@
 import { describe, test, expect, vi, afterEach, beforeEach } from 'vitest'
 import '@testing-library/jest-dom/vitest'
-import {
-  PUBLIC_API_GEO_CITY_QUERY_BASE_URL,
-  PUBLIC_API_GEO_COUNTRY_QUERY_BASE_URL,
-} from '$env/static/public'
 import * as franceConnectHelpers from '$lib/france-connect'
 import { User, userStore } from '$lib/state/User.svelte'
 import * as authHelpers from '$lib/auth'
-import { mockAddress, mockUserIdentity, mockUserInfo } from '$tests/utils'
+import { mockUserIdentity, mockUserInfo } from '$tests/utils'
 import { Address } from '$lib/address'
 import { fetchSpy } from '../../../vitest-setup-client'
 
