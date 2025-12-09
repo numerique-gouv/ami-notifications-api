f117ee63724c076b465e6ed2eb66883898e2c99b
Lauréline Guérin <zebuline@crocobox.org>
2025-12-09 11:23:59 +0100

    [AMI] simplify Address constructor (#227)

--- public/mobile-app/src/lib/address.ts
+++ public/mobile-app/src/lib/address.ts
@@ -1,26 +1,12 @@
 export class Address {
-  private _city: string
-  private _context: string
-  private _idBAN: string
-  private _label: string
-  private _name: string
-  private _postcode: string
-
   constructor(
-    city: string = '',
-    context: string = '',
-    idBAN: string = '',
-    label: string = '',
-    name: string = '',
-    postcode: string = ''
-  ) {
-    this._city = city
-    this._context = context
-    this._idBAN = idBAN
-    this._label = label
-    this._name = name
-    this._postcode = postcode
-  }
+    private _city: string = '',
+    private _context: string = '',
+    private _idBAN: string = '',
+    private _label: string = '',
+    private _name: string = '',
+    private _postcode: string = ''
+  ) {}
 
   get city(): string {
     return this._city
