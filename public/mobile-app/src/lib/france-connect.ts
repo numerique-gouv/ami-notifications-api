2b3797a8dc212b63f479c2fee721ea6d353d7d46
Lauréline Guérin <zebuline@crocobox.org>
2025-12-10 10:41:40 +0100

    [AMI] fix parseJwt function (#355)

--- public/mobile-app/src/lib/france-connect.ts
+++ public/mobile-app/src/lib/france-connect.ts
@@ -13,7 +13,7 @@ export function parseJwt(token: string): UserInfo {
     window
       .atob(base64)
       .split('')
-      .map((c) => `%00${c.charCodeAt(0).toString(16)}`.slice(-2))
+      .map((c) => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`)
       .join('')
   )
 
