224ec5bd7156be646dbc9ea300ee3463c1a5c83a
Mathieu Agopian <mathieu@agopian.info>
2025-12-11 18:57:38 +0100

    Use dependencies for the service providers

--- app/services/user.py
+++ app/services/user.py
@@ -1,7 +1,6 @@
 from typing import Any
 
 from advanced_alchemy.extensions.litestar import repository, service
-from advanced_alchemy.extensions.litestar.providers import create_service_provider
 from litestar import Request
 
 from app import models
@@ -12,9 +11,6 @@ class UserService(service.SQLAlchemyAsyncRepositoryService[models.User]):
         model_type = models.User
 
     repository_type = Repo
-
-
-provide_users_service = create_service_provider(UserService)
 
 
 async def provide_user(request: Request[models.User, Any, Any]) -> models.User:
