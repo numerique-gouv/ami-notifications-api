2cc255b1b825db89ae302b5db992ac1f3bf23c17
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2025-12-12 11:22:13 +0100

    Add address in Profile (#224)

--- public/mobile-app/src/routes/profile/page.svelte.test.ts
+++ public/mobile-app/src/routes/profile/page.svelte.test.ts
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/svelte'
+import { fireEvent, render, screen, waitFor } from '@testing-library/svelte'
 import { describe, expect, test, vi } from 'vitest'
 import * as navigationMethods from '$app/navigation'
 import { userStore } from '$lib/state/User.svelte'
@@ -55,6 +55,24 @@ describe('/+page.svelte', () => {
       expect(profileIdentity).toHaveTextContent('Pierre DUBOIS,')
       expect(profileIdentity).toHaveTextContent('né MERCIER le 1969-03-17')
       expect(screen.getByText('some-other@email.com', { exact: false }))
+    })
+  })
+
+  test('should navigate to Address page when user clicks on "Définir une adresse" button', async () => {
+    // Given
+    const spy = vi
+      .spyOn(navigationMethods, 'goto')
+      .mockImplementation(() => Promise.resolve())
+    render(Page)
+
+    // When
+    const button = screen.getByTestId('address-button')
+    await fireEvent.click(button)
+
+    // Then
+    await waitFor(() => {
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenNthCalledWith(1, '/#/address')
     })
   })
 })
