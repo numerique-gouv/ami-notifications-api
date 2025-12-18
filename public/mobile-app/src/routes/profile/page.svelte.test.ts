cc1f6759e70b79359a76d334350301b6a4649125
Lauréline Guérin <zebuline@crocobox.org>
2025-12-18 15:02:06 +0100

    [AMI] space after preferred username (#385)

--- public/mobile-app/src/routes/profile/page.svelte.test.ts
+++ public/mobile-app/src/routes/profile/page.svelte.test.ts
@@ -2,7 +2,7 @@ import { render, screen, waitFor } from '@testing-library/svelte'
 import { describe, expect, test, vi } from 'vitest'
 import * as navigationMethods from '$app/navigation'
 import { userStore } from '$lib/state/User.svelte'
-import { mockUser } from '$tests/utils'
+import { mockUser, mockUserWithPreferredUsername } from '$tests/utils'
 import Page from './+page.svelte'
 
 describe('/+page.svelte', () => {
@@ -30,8 +30,31 @@ describe('/+page.svelte', () => {
     // Then
     await waitFor(() => {
       expect(spy).toHaveBeenCalled()
-      expect(screen.getByText('Angela Claire Louise DUBOIS', { exact: false }))
+      const profile = screen.getByTestId('profile')
+      const profileIdentity = profile.querySelector('#profile-identity')
+      expect(profileIdentity).toHaveTextContent('Angela Claire Louise DUBOIS,')
+      expect(profileIdentity).toHaveTextContent('née le 1962-08-24')
       expect(screen.getByText('some@email.com', { exact: false }))
+    })
+  })
+
+  test('profile page displays the proper user info - with preferred username', async () => {
+    // Given
+    const spy = vi
+      .spyOn(userStore, 'connected', 'get')
+      .mockReturnValue(mockUserWithPreferredUsername)
+
+    // When
+    render(Page)
+
+    // Then
+    await waitFor(() => {
+      expect(spy).toHaveBeenCalled()
+      const profile = screen.getByTestId('profile')
+      const profileIdentity = profile.querySelector('#profile-identity')
+      expect(profileIdentity).toHaveTextContent('Pierre DUBOIS,')
+      expect(profileIdentity).toHaveTextContent('né MERCIER le 1969-03-17')
+      expect(screen.getByText('some-other@email.com', { exact: false }))
     })
   })
 })
