3e9e48ad5a428e3d37c3cd08e01913ede1534a2e
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2025-12-23 13:27:45 +0100

    Add BackButton component (#420)

---
+++ public/mobile-app/src/lib/components/BackButton.svelte.test.ts
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/svelte'
+import { describe, expect, test, vi } from 'vitest'
+import Page from './BackButton.svelte'
+
+describe('/+BackButton.svelte', () => {
+  test('should navigate to previous page when user clicks on Back button', async () => {
+    // Given
+    const backSpy = vi.spyOn(window.history, 'back').mockImplementation(() => {})
+
+    // When
+    render(Page)
+    const backButton = screen.getByTestId('back-button')
+    await fireEvent.click(backButton)
+
+    // Then
+    expect(backSpy).toHaveBeenCalledTimes(1)
+    backSpy.mockRestore()
+  })
+})
