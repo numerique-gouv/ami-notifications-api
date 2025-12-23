3919a630b46dc1248fc6c2baa9685f9766a4f171
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2025-12-23 13:27:45 +0100

    Add BackButton component (#420)

---
+++ public/mobile-app/src/lib/components/BackButton.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  interface Props {
+    title?: string
+  }
+  let { title }: Props = $props()
+
+  const navigateToPreviousPage = async () => {
+    window.history.back()
+  }
+</script>
+
+<nav>
+  <div class="back-button-wrapper">
+    <button onclick={navigateToPreviousPage}
+            title="Retour à la page précédente"
+            aria-label="Retour à la page précédente"
+            data-testid="back-button"
+    >
+      <span aria-hidden="true" class="fr-icon-arrow-left-line"></span>
+    </button>
+  </div>
+  <div class="title">
+    <h2>{title}</h2>
+  </div>
+</nav>
+
+<style>
+  nav {
+    padding: 1.5rem 1rem;
+    .back-button-wrapper {
+      margin-bottom: .5rem;
+      color: var(--text-active-blue-france);
+      button {
+        padding: 0;
+      }
+    }
+    .title {
+      display: flex;
+      h2 {
+        flex-grow: 1;
+        margin-bottom: 0;
+      }
+    }
+  }
+</style>
