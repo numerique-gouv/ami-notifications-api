fe784849b75ff9ed667f0660f9993c13346c3bd5
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-01-13 15:10:03 +0100

    Remove unused line (#458)

--- public/mobile-app/src/routes/+layout.svelte
+++ public/mobile-app/src/routes/+layout.svelte
@@ -3,7 +3,6 @@
   import '@gouvfr/dsfr/dist/utility/utility.min.css'
   import { onMount } from 'svelte'
   import Toasts from '$lib/components/Toasts.svelte'
-  import { userStore } from '$lib/state/User.svelte'
 
   let { children } = $props()
 
