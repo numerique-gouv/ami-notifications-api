0a1cddeee22303ff259ece6475cfc25f75e911b3
Lauréline Guérin <zebuline@crocobox.org>
2026-01-16 15:54:17 +0100

    do fc get userinfo on login callback (#391)

--- tests/conftest.py
+++ tests/conftest.py
@@ -260,6 +260,20 @@ async def mobileAppSubscription() -> dict[str, Any]:
 
 
 @pytest.fixture
+async def decoded_id_token() -> dict[str, Any]:
+    return {
+        "sub": "cff67ebe00792a2f2b5115dcc1a65d115adb3b73653fb3ed1b88ea11a7a2589av1",
+        "auth_time": 1763455959,
+        "acr": "eidas1",
+        "nonce": "YTc3NzZlNjUtNmY3OC00YzExLThmODItMTg0MDg2ZjQ0YzEyLTIwMjUtMTEtMTggMDg6NTI6MzUuNjM1OTYyKzAwOjAw",
+        "aud": "33fe498cc172fe691778912a2967baa650b24f1ae0ebbe47ae552f37b2d25ead",
+        "exp": 1763456019,
+        "iat": 1763455959,
+        "iss": "https://fcp-low.sbx.dev-franceconnect.fr/api/v2",
+    }
+
+
+@pytest.fixture
 async def userinfo() -> dict[str, Any]:
     return {
         "sub": "fake sub",
