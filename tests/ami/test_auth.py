78b222f1294c6a4dcdaa61e44f4bfcf5b7247952
Lauréline Guérin <zebuline@crocobox.org>
2026-01-20 09:56:51 +0100

    complete tests on query failures (#348)

--- tests/ami/test_auth.py
+++ tests/ami/test_auth.py
@@ -392,41 +392,6 @@ async def test_login_callback_user_never_seen(
     assert scheduled_notification.sent_at is None
 
 
-async def test_login_callback_token_query_failure(
-    test_client: TestClient[Litestar],
-    db_session: AsyncSession,
-    httpx_mock: HTTPXMock,
-) -> None:
-    NONCE = "some random nonce"
-    nonce = Nonce(nonce=NONCE)
-    db_session.add(nonce)
-    await db_session.commit()
-
-    token_failure_response = {
-        "error": "invalid_grant",
-        "error_description": " grant request is invalid (authorization code not found)",
-        "error_uri": "https://docs.partenaires.franceconnect.gouv.fr/fs/fs-technique/fs-technique-erreurs/"
-        "?code=Y049E20B&id=801d508c-72d7-459d-8947-104cf89ce015",
-    }
-    httpx_mock.add_response(
-        method="POST",
-        url="https://fcp-low.sbx.dev-franceconnect.fr/api/v2/token",
-        json=token_failure_response,
-        status_code=401,
-    )
-
-    response = test_client.get(
-        f"/login-callback?code=fake-code&state={nonce.id}", follow_redirects=False
-    )
-
-    assert response.status_code == 302
-    redirected_url = response.headers["location"]
-    assert (
-        redirected_url
-        == "https://localhost:5173/?error=Erreur+lors+de+la+FranceConnexion%2C+veuillez+r%C3%A9essayer+plus+tard.&error_type=FranceConnect"
-    )
-
-
 async def test_login_callback_bad_state(
     test_client: TestClient[Litestar],
     db_session: AsyncSession,
@@ -639,6 +604,92 @@ async def test_login_callback_bad_token_info(
     assert url_contains_param("code", "missing_access_token", redirected_url)
     all_nonces = (await db_session.execute(select(Nonce))).scalars().all()
     assert len(all_nonces) == 0
+
+
+async def test_login_callback_token_query_failure(
+    test_client: TestClient[Litestar],
+    db_session: AsyncSession,
+    httpx_mock: HTTPXMock,
+) -> None:
+    NONCE = "some random nonce"
+    nonce = Nonce(nonce=NONCE)
+    db_session.add(nonce)
+    await db_session.commit()
+
+    token_failure_response = {
+        "error": "invalid_grant",
+        "error_description": " grant request is invalid (authorization code not found)",
+        "error_uri": "https://docs.partenaires.franceconnect.gouv.fr/fs/fs-technique/fs-technique-erreurs/"
+        "?code=Y049E20B&id=801d508c-72d7-459d-8947-104cf89ce015",
+    }
+    httpx_mock.add_response(
+        method="POST",
+        url="https://fcp-low.sbx.dev-franceconnect.fr/api/v2/token",
+        json=token_failure_response,
+        status_code=401,
+    )
+
+    response = test_client.get(
+        f"/login-callback?code=fake-code&state={nonce.id}", follow_redirects=False
+    )
+
+    assert response.status_code == 302
+    redirected_url = response.headers["location"]
+    assert (
+        redirected_url
+        == "https://localhost:5173/?error=Erreur+lors+de+la+FranceConnexion%2C+veuillez+r%C3%A9essayer+plus+tard.&error_type=FranceConnect"
+    )
+
+
+async def test_login_callback_userinfo_query_failure(
+    test_client: TestClient[Litestar],
+    db_session: AsyncSession,
+    httpx_mock: HTTPXMock,
+    monkeypatch: pytest.MonkeyPatch,
+    decoded_id_token: dict[str, Any],
+) -> None:
+    def fake_jwt_decode(*args: Any, **params: Any):
+        return decoded_id_token
+
+    monkeypatch.setattr("jwt.decode", fake_jwt_decode)
+    monkeypatch.setattr("app.controllers.auth.env.FC_AMI_CLIENT_SECRET", "fake-client-secret")
+
+    NONCE = decoded_id_token["nonce"]
+    nonce = Nonce(nonce=NONCE)
+    db_session.add(nonce)
+    await db_session.commit()
+
+    fake_token_json_response = {
+        "access_token": "fake access token",
+        "expires_in": 60,
+        "id_token": "fake id token",
+        "scope": "openid given_name family_name preferred_username birthdate gender birthplace birthcountry email",
+        "token_type": "Bearer",
+    }
+    httpx_mock.add_response(
+        method="POST",
+        url="https://fcp-low.sbx.dev-franceconnect.fr/api/v2/token",
+        json=fake_token_json_response,
+    )
+
+    auth = {"authorization": "Bearer fake access token"}
+    httpx_mock.add_response(
+        method="GET",
+        url="https://fcp-low.sbx.dev-franceconnect.fr/api/v2/userinfo",
+        match_headers=auth,
+        status_code=500,
+    )
+
+    response = test_client.get(
+        f"/login-callback?code=fake-code&state={nonce.id}", follow_redirects=False
+    )
+
+    assert response.status_code == 302
+    redirected_url = response.headers["location"]
+    assert (
+        redirected_url
+        == "https://localhost:5173/?error=Erreur+lors+de+la+FranceConnexion%2C+veuillez+r%C3%A9essayer+plus+tard.&error_type=FranceConnect"
+    )
 
 
 async def test_login_callback_fc_error(
