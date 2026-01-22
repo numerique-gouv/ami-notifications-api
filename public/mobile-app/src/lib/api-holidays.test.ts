25ce12e4e38bd5ebf6960b86acb470f1c6b12c9b
Lauréline Guérin <zebuline@crocobox.org>
2026-01-22 17:48:47 +0100

    call public holidays endpoint from front (#507)

--- public/mobile-app/src/lib/api-holidays.test.ts
+++ public/mobile-app/src/lib/api-holidays.test.ts
@@ -1,12 +1,13 @@
 import { afterEach, describe, expect, test, vi } from 'vitest'
 import '@testing-library/jest-dom/vitest'
-import { retrieveSchoolHolidays } from '$lib/api-holidays'
+import { retrievePublicHolidays, retrieveSchoolHolidays } from '$lib/api-holidays'
 
 describe('/api-holidays', () => {
+  afterEach(() => {
+    window.localStorage.clear()
+  })
+
   describe('retrieveSchoolHolidays', () => {
-    afterEach(() => {
-      window.localStorage.clear()
-    })
     test('should get school holidays from API', async () => {
       // Given
       const holidaysData = [
@@ -73,6 +74,70 @@ describe('/api-holidays', () => {
 
       // When
       const result = await retrieveSchoolHolidays(new Date('2025-11-01T12:00:00Z'))
+
+      // Then
+      expect(result).toEqual(holidaysData)
+    })
+  })
+  describe('retrievePublicHolidays', () => {
+    test('should get public holidays from API', async () => {
+      // Given
+      const holidaysData = [
+        {
+          description: 'Day 1',
+          date: new Date('2025-09-20T23:00:00Z'),
+          emoji: '',
+        },
+        {
+          description: 'Day 2',
+          date: new Date('2025-10-20T23:00:00Z'),
+          emoji: '',
+        },
+      ]
+      vi.spyOn(globalThis, 'fetch').mockResolvedValue(
+        new Response(JSON.stringify(holidaysData), { status: 200 })
+      )
+
+      // When
+      const result = await retrievePublicHolidays(new Date('2025-11-01T12:00:00Z'))
+
+      // Then
+      expect(result).toEqual(holidaysData)
+      expect(window.localStorage.getItem('public_holidays_data')).toEqual(
+        JSON.stringify(holidaysData)
+      )
+    })
+    test('should get public holidays from API with error', async () => {
+      // Given
+      vi.spyOn(globalThis, 'fetch').mockResolvedValue(
+        new Response('error', { status: 400 })
+      )
+
+      // When
+      const result = await retrievePublicHolidays(new Date('2025-11-01T12:00:00Z'))
+
+      // Then
+      expect(result).toEqual([])
+      expect(window.localStorage.getItem('public_holidays_data')).toEqual(null)
+    })
+    test('should get public holidays data from local storage', async () => {
+      // Given
+      const holidaysData = [
+        {
+          description: 'Day 1',
+          date: new Date('2025-09-20T23:00:00Z'),
+          emoji: '',
+        },
+        {
+          description: 'Day 2',
+          date: new Date('2025-10-20T23:00:00Z'),
+          emoji: '',
+        },
+      ]
+      window.localStorage.setItem('public_holidays_data', JSON.stringify(holidaysData))
+
+      // When
+      const result = await retrievePublicHolidays(new Date('2025-11-01T12:00:00Z'))
 
       // Then
       expect(result).toEqual(holidaysData)
--- public/mobile-app/src/lib/api-holidays.ts
+++ public/mobile-app/src/lib/api-holidays.ts
@@ -14,7 +14,6 @@ export const retrieveSchoolHolidays = async (
   const today = date || new Date()
   today.setHours(0, 0, 0, 0)
   const current_date = today.toLocaleDateString('sv-SE') // this gives the locale date in ISO format ...
-  console.log('current_date', current_date)
   let holidaysData = localStorage.getItem('school_holidays_data')
   if (!holidaysData) {
     const response = await apiFetch(
@@ -34,6 +33,42 @@ export const retrieveSchoolHolidays = async (
       // convert dates
       holiday.start_date = new Date(holiday.start_date)
       holiday.end_date = new Date(holiday.end_date)
+    })
+    return holidays
+  }
+  return []
+}
+
+export type PublicHoliday = {
+  description: string
+  date: Date
+  emoji: string
+}
+
+export const retrievePublicHolidays = async (
+  date: Date | null = null
+): Promise<PublicHoliday[]> => {
+  const today = date || new Date()
+  today.setHours(0, 0, 0, 0)
+  const current_date = today.toLocaleDateString('sv-SE') // this gives the locale date in ISO format ...
+  let holidaysData = localStorage.getItem('public_holidays_data')
+  if (!holidaysData) {
+    const response = await apiFetch(
+      `/data/public-holidays?current_date=${current_date}`,
+      {
+        credentials: 'include',
+      }
+    )
+    if (response.ok) {
+      holidaysData = await response.text()
+      localStorage.setItem('public_holidays_data', holidaysData)
+    }
+  }
+  if (holidaysData) {
+    const holidays = JSON.parse(holidaysData) as PublicHoliday[]
+    holidays.forEach((holiday) => {
+      // convert date
+      holiday.date = new Date(holiday.date)
     })
     return holidays
   }
