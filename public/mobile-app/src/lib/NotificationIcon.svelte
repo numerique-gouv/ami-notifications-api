c57b638b54407acbecd6b1a23e26c63fd9625591
Lauréline Guérin <zebuline@crocobox.org>
2026-01-26 10:35:03 +0100

    notification, display default icon if given icon is unknown (#487)

---
+++ public/mobile-app/src/lib/NotificationIcon.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import dsfrIconList from '@gouvfr/dsfr/.config/icon.json'
+  import { onMount } from 'svelte'
+
+  interface Props {
+    defaultIcon: string
+    icon?: string
+  }
+  let { defaultIcon, icon = '' }: Props = $props()
+  let checkedIcon: string = $state('')
+
+  onMount(async () => {
+    checkedIcon = dsfrIconList.filter((i) => i.name === icon.replace('fr-icon-', ''))
+      .length
+      ? icon
+      : defaultIcon
+  })
+</script>
+
+<span class="notification__icon {checkedIcon}" aria-hidden="true"></span>
+
+<style>
+  .notification__icon {
+    line-height: 2rem;
+    color: var(--blue-france-sun-113-625);
+    &::before {
+      --icon-size: 1.25rem;
+    }
+  }
+</style>
