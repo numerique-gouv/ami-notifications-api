b63ef5b57316f4edcb3677e5bc76759ef43a7c45
Lauréline Guérin <zebuline@crocobox.org>
2026-01-29 15:44:48 +0100

    endpoint to get agenda items (#509)

--- app/data/routes.py
+++ app/data/routes.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 from typing import Any
 
@@ -6,11 +7,13 @@ from litestar import Request, Response, Router, get
 from app.data.holidays import (
     SchoolHolidaysError,
     get_holidays_dates,
+    get_public_holidays_catalog,
     get_public_holidays_data,
+    get_school_holidays_catalog,
     get_school_holidays_data,
 )
 from app.httpx import AsyncClient
-from app.schemas import PublicHoliday, SchoolHoliday
+from app.schemas import Agenda, PublicHoliday, SchoolHoliday
 from app.utils import error_from_message
 
 
@@ -39,10 +42,34 @@ def get_public_holidays(
     return get_public_holidays_data(start_date, end_date)
 
 
+@get(path="/agenda/items", include_in_schema=False)
+async def get_agenda_items(
+    request: Request[Any, Any, Any],
+    current_date: datetime.date,
+    httpx_async_client: AsyncClient,
+) -> Agenda:
+    start_date, end_date = get_holidays_dates(current_date)
+    agenda = Agenda()
+
+    task_school_holidays = None
+    task_public_holidays = None
+    async with asyncio.TaskGroup() as task_group:
+        task_school_holidays = task_group.create_task(
+            get_school_holidays_catalog(start_date, end_date, httpx_async_client)
+        )
+        task_public_holidays = task_group.create_task(
+            get_public_holidays_catalog(start_date, end_date)
+        )
+    agenda.school_holidays = task_school_holidays.result()
+    agenda.public_holidays = task_public_holidays.result()
+    return agenda
+
+
 data_router: Router = Router(
     path="/data",
     route_handlers=[
         get_school_holidays,
         get_public_holidays,
+        get_agenda_items,
     ],
 )
--- tests/ami/data/test_all.py
+++ tests/ami/data/test_all.py
@@ -8,7 +8,13 @@ from litestar.testing import TestClient
 
 from app.data.holidays import SchoolHolidaysError
 from app.models import User
-from app.schemas import PublicHoliday, SchoolHoliday
+from app.schemas import (
+    AgendaCatalog,
+    AgendaCatalogItem,
+    AgendaCatalogStatus,
+    PublicHoliday,
+    SchoolHoliday,
+)
 from tests.ami.utils import assert_query_fails_without_auth, login
 
 
@@ -125,3 +131,104 @@ async def test_get_public_holidays_without_auth(
     test_client: TestClient[Litestar],
 ) -> None:
     await assert_query_fails_without_auth("/data/public-holidays", test_client)
+
+
+async def test_get_agenda_items(
+    user: User,
+    test_client: TestClient[Litestar],
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    login(user, test_client)
+
+    today = datetime.date.today()
+    dates_mock = mock.Mock(return_value=(today, today + datetime.timedelta(days=2)))
+    monkeypatch.setattr("app.data.routes.get_holidays_dates", dates_mock)
+    school_catalog = AgendaCatalog(
+        status=AgendaCatalogStatus.SUCCESS,
+        items=[
+            AgendaCatalogItem(
+                title="Vacances de Noël",
+                start_date=datetime.date(2025, 12, 20),
+                end_date=datetime.date(2026, 1, 5),
+                zones="",
+                emoji="🎄",
+            ),
+            AgendaCatalogItem(
+                title="Vacances d'Hiver",
+                start_date=datetime.date(2026, 2, 7),
+                end_date=datetime.date(2026, 2, 23),
+                zones="Zone A",
+                emoji="❄️",
+            ),
+        ],
+    )
+    school_data_mock = mock.AsyncMock(return_value=school_catalog)
+    monkeypatch.setattr("app.data.routes.get_school_holidays_catalog", school_data_mock)
+    public_catalog = AgendaCatalog(
+        status=AgendaCatalogStatus.SUCCESS,
+        items=[
+            AgendaCatalogItem(title="Noël", date=datetime.date(2025, 12, 25), emoji="📅"),
+            AgendaCatalogItem(title="Jour de l’An", date=datetime.date(2026, 1, 1), emoji="🎉"),
+        ],
+    )
+    public_data_mock = mock.AsyncMock(return_value=public_catalog)
+    monkeypatch.setattr("app.data.routes.get_public_holidays_catalog", public_data_mock)
+
+    response = test_client.get("/data/agenda/items", params={"current_date": "2025-12-12"})
+    assert response.status_code == HTTP_200_OK
+    assert response.json() == {
+        "school_holidays": {
+            "status": "success",
+            "items": [
+                {
+                    "title": "Vacances de Noël",
+                    "date": None,
+                    "start_date": "2025-12-20",
+                    "end_date": "2026-01-05",
+                    "zones": "",
+                    "emoji": "🎄",
+                },
+                {
+                    "title": "Vacances d'Hiver",
+                    "date": None,
+                    "start_date": "2026-02-07",
+                    "end_date": "2026-02-23",
+                    "zones": "Zone A",
+                    "emoji": "❄️",
+                },
+            ],
+        },
+        "public_holidays": {
+            "status": "success",
+            "items": [
+                {
+                    "title": "Noël",
+                    "date": "2025-12-25",
+                    "start_date": None,
+                    "end_date": None,
+                    "zones": "",
+                    "emoji": "📅",
+                },
+                {
+                    "title": "Jour de l’An",
+                    "date": "2026-01-01",
+                    "start_date": None,
+                    "end_date": None,
+                    "zones": "",
+                    "emoji": "🎉",
+                },
+            ],
+        },
+    }
+
+    assert dates_mock.call_args_list == [mock.call(datetime.date(2025, 12, 12))]
+    assert school_data_mock.call_args_list == [
+        mock.call(today, today + datetime.timedelta(days=2), mock.ANY)
+    ]
+    assert public_data_mock.call_args_list == [mock.call(today, today + datetime.timedelta(days=2))]
+
+
+async def test_get_agenda_items_without_auth(
+    test_client: TestClient[Litestar],
+) -> None:
+    await assert_query_fails_without_auth("/data/agenda/items", test_client)
