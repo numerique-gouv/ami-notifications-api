cedf9a7adfb57aa6593180087940643867642604
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-01-30 15:02:51 +0100

    Extract buildUser method (#468)

--- public/mobile-app/src/routes/+layout.ts
+++ public/mobile-app/src/routes/+layout.ts
@@ -3,5 +3,6 @@ import type { LayoutLoad } from './$types'
 
 export const load: LayoutLoad = async () => {
   // Initialize user state from localStorage before anything else
+  // TODO CLO : utile de faire ça si c'est déjà fait dans src/routes/+page.svelte ?
   await userStore.checkLoggedIn()
 }
