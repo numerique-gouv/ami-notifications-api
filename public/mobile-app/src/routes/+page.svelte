df1427abe535b240a601c03c63a1c7e507239dd2
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-01-30 14:43:06 +0100

    Extract initialization method in root svelte page (#468)

--- public/mobile-app/src/routes/+page.svelte
+++ public/mobile-app/src/routes/+page.svelte
@@ -5,7 +5,7 @@
   import { page } from '$app/state'
   import { PUBLIC_API_URL, PUBLIC_CONTACT_URL } from '$env/static/public'
   import ConnectedHomepage from '$lib/ConnectedHomepage.svelte'
-  import { initializeLocalStorage } from '$lib/initializeDataFromAPI'
+  import { initializeData } from '$lib/initializeDataFromAPI'
   import Navigation from '$lib/Navigation.svelte'
   import { toastStore } from '$lib/state/toast.svelte'
   import { userStore } from '$lib/state/User.svelte'
@@ -37,8 +37,7 @@
         error_description = ''
       }
       if (page.url.searchParams.has('is_logged_in')) {
-        await initializeLocalStorage(page.url.searchParams)
-        await userStore.checkLoggedIn()
+        await initializeData(page.url.searchParams)
         if (page.url.searchParams.get('user_first_login') === 'true') {
           goto('/#/notifications-welcome-page')
         } else {
