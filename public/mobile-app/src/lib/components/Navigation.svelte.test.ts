cd0b458f62f92875b989614ae45f14ef8a7c162d
Lauréline Guérin <zebuline@crocobox.org>
2026-02-16 10:25:51 +0100

    add "Suivi" entry in menu (#264)

--- public/mobile-app/src/lib/components/Navigation.svelte.test.ts
+++ public/mobile-app/src/lib/components/Navigation.svelte.test.ts
@@ -1,14 +1,44 @@
-import { describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { render } from '@testing-library/svelte';
+import * as envModule from '$env/static/public';
 import Navigation from './Navigation.svelte';
 
 describe('/Navigation.svelte', () => {
+  beforeEach(async () => {
+    vi.mock('$env/static/public', async (importOriginal) => {
+      const original = (await importOriginal()) as Record<string, any>;
+      return Promise.resolve({
+        ...original,
+      });
+    });
+  });
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   test('should highlight nothing', async () => {
     // Given
+    vi.mocked(envModule).PUBLIC_FEATUREFLAG_REQUESTS_ENABLED = 'true';
     const { container } = render(Navigation);
 
     // Then
+    expect(container).toHaveTextContent('Accueil');
+    expect(container).toHaveTextContent('Agenda');
+    expect(container).toHaveTextContent('Suivi');
+    const highlight = container.querySelector('.highlight');
+    expect(highlight).toBeNull();
+  });
+
+  test('should highlight nothing - no requests', async () => {
+    // Given
+    vi.mocked(envModule).PUBLIC_FEATUREFLAG_REQUESTS_ENABLED = 'false';
+    const { container } = render(Navigation);
+
+    // Then
+    expect(container).toHaveTextContent('Accueil');
+    expect(container).toHaveTextContent('Agenda');
+    expect(container).not.toHaveTextContent('Suivi');
     const highlight = container.querySelector('.highlight');
     expect(highlight).toBeNull();
   });
@@ -29,5 +59,15 @@ describe('/Navigation.svelte', () => {
     // Then
     const highlight = container.querySelector('.highlight');
     expect(highlight).toHaveTextContent('Agenda');
+  });
+
+  test('should highlight followup item', async () => {
+    // Given
+    vi.mocked(envModule).PUBLIC_FEATUREFLAG_REQUESTS_ENABLED = 'true';
+    const { container } = render(Navigation, { currentItem: 'requests' });
+
+    // Then
+    const highlight = container.querySelector('.highlight');
+    expect(highlight).toHaveTextContent('Suivi');
   });
 });
