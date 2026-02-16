e3054e3466dae3f1e56135018ec9a0783bc687e1
Lauréline Guérin <zebuline@crocobox.org>
2026-02-16 09:40:57 +0100

    build psl inventory from notifications (#264)

--- app/data/schemas.py
+++ app/data/schemas.py
@@ -2,8 +2,9 @@ import datetime
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field, fields
 from enum import Enum
-from typing import Any
+from typing import Any, Self
 
+from app import models
 from app.schemas import ItemGenericStatus
 
 
@@ -224,6 +225,38 @@ class FollowUpInventoryItem:
 
     created_at: datetime.datetime
     updated_at: datetime.datetime
+
+    kind_mapping = {
+        ("psl", "OperationTranquilliteVacances"): FollowUpInventoryItemKind.OTV,
+    }
+
+    @classmethod
+    def from_notifications(cls, notifications: list[models.Notification]) -> Self | None:
+        first_notification = notifications[0]
+        last_notification = notifications[-1]
+        external_urls = [n.item_external_url for n in notifications if n.item_external_url]
+        assert last_notification.partner_id
+        assert last_notification.item_type
+        kind = cls.kind_mapping.get((last_notification.partner_id, last_notification.item_type))
+        if kind is None:
+            return None
+        try:
+            status_id = ItemGenericStatus(last_notification.item_generic_status)
+        except ValueError:
+            return None
+        return cls(
+            external_id=f"{last_notification.item_type}:{last_notification.item_id}",
+            kind=kind,
+            status_id=status_id,
+            status_label=last_notification.item_status_label or "",
+            milestone_start_date=last_notification.item_milestone_start_date,
+            milestone_end_date=last_notification.item_milestone_end_date,
+            title=last_notification.content_title,
+            description=last_notification.content_body,
+            external_url=external_urls[-1] if external_urls else None,
+            created_at=first_notification.send_date,
+            updated_at=last_notification.send_date,
+        )
 
 
 @dataclass
