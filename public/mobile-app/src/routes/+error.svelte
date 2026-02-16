302ca939d64d8bced8b7f872f527e46a9e538c74
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-02-16 17:09:48 +0100

    test with invalidateAll

--- public/mobile-app/src/routes/+error.svelte
+++ public/mobile-app/src/routes/+error.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import { goto } from '$app/navigation'
   import { page } from '$app/state'
 
   onMount(() => {
@@ -12,6 +13,10 @@
       console.error('Error message: ', page.error.message)
     }
   })
+
+  const goToHomepage = () => {
+    goto('/', { invalidateAll: true })
+  }
 </script>
 
 <div class="technical-error">
@@ -21,15 +26,14 @@
     <p>(Veuillez éventuellement vérifier la connexion)</p>
   </div>
   <div class="action-buttons">
-    <a
-      data-sveltekit-reload
-      href="/"
+    <button
       class="fr-btn"
       type="button"
+      onclick="{goToHomepage}"
       data-testid="back-button"
     >
       Revenir à l'accueil
-    </a>
+    </button>
   </div>
 </div>
 
