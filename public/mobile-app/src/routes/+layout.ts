8717a99ceea26f6f84a22a630bee2ae9af133f8f
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-02-27 10:53:42 +0100

    Initialize data in layout too (#468)

--- public/mobile-app/src/routes/+layout.ts
+++ public/mobile-app/src/routes/+layout.ts
@@ -1,8 +1,11 @@
+import { page } from '$app/state';
+import { initializeData } from '$lib/initializeDataFromAPI';
 import { userStore } from '$lib/state/User.svelte';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async () => {
   // Initialize user state from localStorage before anything else
-  // TODO CLO : utile de faire ça si c'est déjà fait dans src/routes/+page.svelte ?
-  await userStore.checkLoggedIn();
+  if (localStorage.getItem('is_logged_in')) {
+    await initializeData(page.url.searchParams, userStore);
+  }
 };
