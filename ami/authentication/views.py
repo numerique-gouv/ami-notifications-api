4228e63a153966c360f890ce44f96a6e0d63469e
Lauréline Guérin <zebuline@crocobox.org>
2026-03-02 19:08:06 +0100

    login-france-connect view (#635)

---
+++ ami/authentication/views.py
@@ -0,0 +1,38 @@
+from urllib.parse import urlencode
+
+from django.conf import settings
+from django.shortcuts import redirect
+from django.views.decorators.http import require_GET
+
+from ami.authentication.models import Nonce
+from ami.authentication.utils import generate_nonce
+
+
+@require_GET
+def login_france_connect(request):
+    try:
+        NONCE = generate_nonce()
+        nonce = Nonce.objects.create(nonce=NONCE)
+
+        params = {
+            "scope": settings.PUBLIC_FC_SCOPE,
+            "redirect_uri": settings.PUBLIC_FC_PROXY or settings.PUBLIC_FC_AMI_REDIRECT_URL,
+            "response_type": "code",
+            "client_id": settings.PUBLIC_FC_AMI_CLIENT_ID,
+            "state": (
+                f"{settings.PUBLIC_FC_AMI_REDIRECT_URL}?state={nonce.id}"
+                if settings.PUBLIC_FC_PROXY
+                else str(nonce.id)
+            ),
+            "nonce": NONCE,
+            "acr_values": "eidas1",
+            "prompt": "login",
+        }
+
+        login_url = (
+            f"{settings.PUBLIC_FC_BASE_URL}{settings.PUBLIC_FC_AUTHORIZATION_ENDPOINT}"
+            f"?{urlencode(params)}"
+        )
+        return redirect(login_url)
+    except Exception:
+        return redirect(f"{settings.PUBLIC_APP_URL}/#/technical-error")
--- ami/urls.py
+++ ami/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
+from ami.authentication import urls as authentication_urls
 from ami.utils import views as ami_views
 
 urlpatterns = [
     path("ping", ami_views.ping),
+    path("", include(authentication_urls)),
 ]
