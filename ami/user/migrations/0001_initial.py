07f9a67587e7c3eb6a5613522a233523be62466a
Lauréline Guérin <zebuline@crocobox.org>
2026-03-02 17:50:00 +0100

    initial migrations for user app (#635)

---
+++ ami/user/migrations/0001_initial.py
@@ -0,0 +1,60 @@
+import uuid
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="User",
+            fields=[
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("sa_orm_sentinel", models.IntegerField(blank=True, null=True)),
+                ("fc_hash", models.CharField(unique=True)),
+                ("last_logged_in", models.DateTimeField(blank=True, null=True)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                "db_table": "ami_user",
+            },
+        ),
+        migrations.CreateModel(
+            name="Registration",
+            fields=[
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("subscription", models.JSONField(blank=True, null=True)),
+                ("sa_orm_sentinel", models.IntegerField(blank=True, null=True)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "user",
+                    models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to="user.user"),
+                ),
+            ],
+            options={
+                "db_table": "registration",
+            },
+        ),
+    ]
