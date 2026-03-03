bb232ea3e355319c8e884b7c3ce51f293d91c060
Lauréline Guérin <zebuline@crocobox.org>
2026-03-03 11:14:03 +0100

    rename authentication.utils into authentication.auth (#635)

--- ami/authentication/tests/test_all.py
+++ ami/authentication/tests/test_all.py
@@ -3,15 +3,15 @@ from base64 import urlsafe_b64encode
 
 import pytest
 
-from ami.authentication.utils import generate_nonce
+from ami.authentication.auth import generate_nonce
 
 
 def test_generate_nonce(monkeypatch: pytest.MonkeyPatch) -> None:
     FAKE_TIME = datetime.datetime(2020, 12, 25, 17, 5, 55)
     FAKE_UUID = "fake-uuid"
 
-    monkeypatch.setattr("ami.authentication.utils.uuid4", lambda: FAKE_UUID)
-    monkeypatch.setattr("ami.authentication.utils.now", lambda: FAKE_TIME)
+    monkeypatch.setattr("ami.authentication.auth.uuid4", lambda: FAKE_UUID)
+    monkeypatch.setattr("ami.authentication.auth.now", lambda: FAKE_TIME)
     assert generate_nonce() == urlsafe_b64encode(f"{FAKE_UUID}-{FAKE_TIME}".encode("utf8")).decode(
         "utf8"
     )
