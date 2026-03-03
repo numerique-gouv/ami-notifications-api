b434a6aa5067b72f3018d5009b6137850eb611fb
Lauréline Guérin <zebuline@crocobox.org>
2026-03-03 11:34:37 +0100

    move build_fc_hash function (#635)

---
+++ ami/user/tests/test_utils.py
@@ -0,0 +1,30 @@
+from ami.user.utils import build_fc_hash
+
+
+def test_build_fc_hash() -> None:
+    given_name = "Angela Claire Louise"
+    family_name = "DUBOIS"
+    birthdate = "1962-08-24"
+    gender = "female"
+    birthplace = "75107"
+    birthcountry = "99100"
+    response = build_fc_hash(
+        given_name=given_name,
+        family_name=family_name,
+        birthdate=birthdate,
+        gender=gender,
+        birthplace=birthplace,
+        birthcountry=birthcountry,
+    )
+    assert response == "4abd71ec1f581dce2ea2221cbeac7c973c6aea7bcb835acdfe7d6494f1528060"
+
+    birthplace = ""
+    response = build_fc_hash(
+        given_name=given_name,
+        family_name=family_name,
+        birthdate=birthdate,
+        gender=gender,
+        birthplace=birthplace,
+        birthcountry=birthcountry,
+    )
+    assert response == "7e74df2cbebae761eccedbc24b7fe589bb83137f7808a2930031f52c73d75efe"
