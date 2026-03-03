f6f0febacff650c814e3c01455f7e5b392f7e71f
Lauréline Guérin <zebuline@crocobox.org>
2026-03-03 11:34:37 +0100

    move build_fc_hash function (#635)

--- tests/ami/test_basic.py
+++ tests/ami/test_basic.py
@@ -2,8 +2,6 @@ import pytest
 from litestar import Litestar
 from litestar.testing import TestClient
 
-from app.utils import build_fc_hash
-
 pytestmark = pytest.mark.skip("skip tests for Django migration")
 
 
@@ -23,34 +21,3 @@ async def test_get_sector_identifier_url(
     )
     response = test_client.get("/sector_identifier_url")
     assert response.json() == ["https://example.com", "foobar"]
-
-
-async def test_build_fc_hash(
-    test_client: TestClient[Litestar],
-) -> None:
-    given_name = "Angela Claire Louise"
-    family_name = "DUBOIS"
-    birthdate = "1962-08-24"
-    gender = "female"
-    birthplace = "75107"
-    birthcountry = "99100"
-    response = build_fc_hash(
-        given_name=given_name,
-        family_name=family_name,
-        birthdate=birthdate,
-        gender=gender,
-        birthplace=birthplace,
-        birthcountry=birthcountry,
-    )
-    assert response == "4abd71ec1f581dce2ea2221cbeac7c973c6aea7bcb835acdfe7d6494f1528060"
-
-    birthplace = ""
-    response = build_fc_hash(
-        given_name=given_name,
-        family_name=family_name,
-        birthdate=birthdate,
-        gender=gender,
-        birthplace=birthplace,
-        birthcountry=birthcountry,
-    )
-    assert response == "7e74df2cbebae761eccedbc24b7fe589bb83137f7808a2930031f52c73d75efe"
