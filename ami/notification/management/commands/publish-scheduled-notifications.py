8534f5d70080c65ef0f39ddbb7dda09812566428
Mathieu Agopian <mathieu@agopian.info>
2026-03-04 14:46:41 +0100

    Migrate Litestar CLI commands publish-scheduled-notifications and delete-published-scheduled-notifications to django management commands (#635)

---
+++ ami/notification/management/commands/publish-scheduled-notifications.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.utils import timezone
+
+from ami.notification.models import ScheduledNotification
+from ami.notification.push import push
+
+
+class Command(BaseCommand):
+    help = "Create scheduled notifications to be distributed automatically"
+
+    def handle(self, *args, **kwargs):
+        now = timezone.now()
+        scheduled_notifications = (
+            ScheduledNotification.objects.select_for_update(skip_locked=True)
+            .filter(
+                scheduled_at__lt=now,
+                sent_at__isnull=True,
+            )
+            .order_by("created_at")
+        )
+        with transaction.atomic():
+            for notification in scheduled_notifications:
+                self._publish_scheduled_notifications(notification)
+        print(
+            f"Pushed {scheduled_notifications.count()} scheduled notifications",
+            scheduled_notifications,
+        )
+
+    def _publish_scheduled_notifications(self, scheduled_notification: ScheduledNotification):
+        notification = scheduled_notification.create_notification()
+        scheduled_notification.sent_at = notification.created_at
+        scheduled_notification.save()
+        push(notification, True)
