f7900f8dd7268c95b58b14654b9a6f2d75955e33
Mathieu Agopian <mathieu@agopian.info>
2026-03-09 14:18:46 +0100

    Add sentry for django (#635)

--- ami/notification/channel_consumer.py
+++ ami/notification/channel_consumer.py
@@ -3,6 +3,8 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
 
+import ami.utils.sentry as sentry
+
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -14,12 +16,14 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         assert self.channel_layer is not None, "CHANNEL_LAYERS is not configured"
         self.group_name = f"user_{user_id}"
         await self.channel_layer.group_add(self.group_name, self.channel_name)
+        sentry.add_counter("notification_websocket.connected")
         await self.accept()
 
     async def disconnect(self, code: int):
         if hasattr(self, "group_name"):
             assert self.channel_layer is not None
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
+            sentry.add_counter("notification_websocket.disconnected")
 
     async def notification_event(self, event: dict):
         await self.send(
