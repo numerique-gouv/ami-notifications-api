cd669b55cc094e870171e724a1e426fb366c9f5f
Mathieu Agopian <mathieu@agopian.info>
2026-03-09 16:07:09 +0100

    Django: Send push notifications (web and mobile)

--- ami/user/models.py
+++ ami/user/models.py
@@ -1,6 +1,8 @@
 import uuid
 
 from django.db import models
+from pydantic import BaseModel
+from webpush import WebPushSubscription
 
 
 class User(models.Model):
@@ -17,6 +19,14 @@ class User(models.Model):
         db_table = "ami_user"
 
 
+class MobileAppSubscription(BaseModel):
+    app_version: str
+    device_id: str
+    fcm_token: str
+    model: str
+    platform: str
+
+
 class Registration(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
@@ -27,5 +37,20 @@ class Registration(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @property
+    def typed_subscription(self) -> WebPushSubscription | MobileAppSubscription:
+        """Convert the stored dict to the proper subscription type."""
+        try:
+            return WebPushSubscription.model_validate(self.subscription)
+        except Exception:
+            return MobileAppSubscription.model_validate(self.subscription)
+
     class Meta:
         db_table = "registration"
+
+
+class NotificationPush(BaseModel):
+    title: str
+    message: str
+    content_icon: str | None
+    sender: str
