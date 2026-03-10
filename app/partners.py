1322c28a36bf449e994853ca891219c6c82032e5
Lauréline Guérin <zebuline@crocobox.org>
2026-03-10 13:36:25 +0100

    add new partners (#605)

--- app/partners.py
+++ app/partners.py
@@ -14,7 +14,18 @@ class Partner:
     icon: str
 
 
-partners: dict[str, Partner] = {"psl": Partner("psl", "PSL", env.PARTNERS_PSL_SECRET, "")}
+partners: dict[str, Partner] = {
+    "psl": Partner("psl", "PSL", env.PARTNERS_PSL_SECRET, ""),
+    "dinum-dn": Partner(
+        "dinum-dn",
+        "demarche.numerique.gouv.fr",
+        env.PARTNERS_DINUM_DN_SECRET,
+        "fr-icon-infinity-line",
+    ),
+    "dinum-ami": Partner(
+        "dinum-ami", "AMI", env.PARTNERS_DINUM_AMI_SECRET, "fr-icon-smartphone-line"
+    ),
+}
 
 
 async def provide_partner(request: Request[Partner, Any, Any]) -> Partner:
