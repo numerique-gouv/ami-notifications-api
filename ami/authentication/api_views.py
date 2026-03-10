e5f9afb7d94d4d043b5e34aec9e48dd919a619dc
Mathieu Agopian <mathieu@agopian.info>
2026-03-10 15:38:27 +0100

    Django migration: auth endpoints (#635)

---
+++ ami/authentication/api_views.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from ami.authentication.decorators import ami_login_required
+from ami.authentication.models import RevokedAuthToken
+
+
+@api_view(["GET"])
+@ami_login_required
+def check_auth(request):
+    return Response({"authenticated": True})
+
+
+@api_view(["POST"])
+@ami_login_required
+def logout(request):
+    if request.ami_payload.get("jti") is not None:
+        RevokedAuthToken.objects.create(jti=request.ami_payload["jti"])
+    response = Response({}, status=201)
+    response.delete_cookie(settings.AUTH_COOKIE_JWT_NAME)
+    return response
--- ami/urls.py
+++ ami/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from ami.api import urls as api_urls
 from ami.authentication import urls as authentication_urls
-from ami.utils import api_urls as util_api_urls
 
 urlpatterns = [
-    path("", include(util_api_urls)),
     path("", include(authentication_urls)),
+    path("", include(api_urls)),
     path("", TemplateView.as_view(template_name="index.html")),
 ]
