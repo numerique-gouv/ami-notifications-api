b775e2ba2aab12a0bcbabdd7d676bd2045665060
Mathieu Agopian <mathieu@agopian.info>
2026-03-10 10:18:08 +0100

    @zebuline's review

--- ami/utils/__init__.py
+++ ami/utils/__init__.py
@@ -2,7 +2,6 @@ import base64
 import csv
 import datetime
 import gzip
-import hashlib
 import json
 from uuid import uuid4
 
@@ -144,19 +143,3 @@ def generate_identity_tokens_in_file(
                     "identity_token": row["identity_token"],
                 }
             )
-
-
-def build_fc_hash(
-    *,
-    given_name: str,
-    family_name: str,
-    birthdate: str,
-    gender: str,
-    birthplace: str,
-    birthcountry: str,
-) -> str:
-    recipient_fc_hash = hashlib.sha256()
-    recipient_fc_hash.update(
-        f"{given_name}{family_name}{birthdate}{gender}{birthplace}{birthcountry}".encode("utf-8")
-    )
-    return recipient_fc_hash.hexdigest()
