f44c1adc4b8384938e3eff3d9d6f043e7f7c6056
Mathieu Agopian <mathieu@agopian.info>
2026-03-11 18:31:34 +0100

    Django migration: list notifications endpoint (#635)

--- ami/api/urls.py
+++ ami/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 
 from ami.authentication import api_urls as authentication_api_urls
+from ami.notification import api_urls as notification_api_urls
 from ami.utils import api_urls as util_api_urls
 
 urlpatterns = [
     path("", include(authentication_api_urls)),
     path("", include(util_api_urls)),
+    path("api/v1/", include(notification_api_urls)),
 ]
