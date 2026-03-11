2760a691b27c3fee4cc28c6d9f2fca2c8723625d
Mathieu Agopian <mathieu@agopian.info>
2026-03-11 22:09:13 +0100

    Django migration: migrate the notification-key endpoint (#635)

--- ami/notification/api_urls.py
+++ ami/notification/api_urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .api_views import list_notifications, read_notification
+from .api_views import get_notification_key, list_notifications, read_notification
 
 urlpatterns = [
     path("users/notifications", list_notifications),
     path("users/notification/<uuid:notification_id>/read", read_notification),
+]
+
+root_urlpatterns = [
+    path("notification-key", get_notification_key),
 ]
