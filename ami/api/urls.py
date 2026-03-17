eed78ef886ccbb3ecc399fd594cbd2956fcfa603
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-03-17 14:32:50 +0100

    Django migration: migrate partner url endpoint (#635)

--- ami/api/urls.py
+++ ami/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 
 from ami.authentication import api_urls as authentication_api_urls
 from ami.notification import api_urls as notification_api_urls
+from ami.partner import api_urls as partner_api_urls
 from ami.user import api_urls as user_api_urls
 from ami.utils import api_urls as util_api_urls
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path("", include(util_api_urls)),
     path("", include(notification_api_urls.root_urlpatterns)),
     path("api/v1/", include(notification_api_urls)),
+    path("api/v1/", include(partner_api_urls)),
     path("api/v1/", include(user_api_urls)),
 ]
