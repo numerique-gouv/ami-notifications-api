77834148c5150751629bb6e6e73cf535fd5ff307
Mathieu Agopian <mathieu@agopian.info>
2026-03-23 16:55:28 +0100

    Initialize the Firebase SDK (#712)

--- ami/notification/apps.py
+++ ami/notification/apps.py
@@ -1,5 +1,19 @@
+import os
+
+import firebase_admin
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class NotificationConfig(AppConfig):
     name = "ami.notification"
+
+    def ready(self) -> None:
+        if os.getenv("GOOGLE_APPLICATION_CREDENTIALS") is None:
+            # It's already set in Scalingo, locally it comes from .env.local, loaded in settings.CONFIG
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = settings.CONFIG.get(
+                "GOOGLE_APPLICATION_CREDENTIALS", ""
+            )
+        # This needs the "GOOGLE_APPLICATION_CREDENTIALS" env variable to be set to the secret json filename.
+        # See the CONTRIBUTING.md file.
+        firebase_admin.initialize_app()
