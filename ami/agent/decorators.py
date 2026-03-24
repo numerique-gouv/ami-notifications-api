81722ea1ef4fd6339370391a3f844472e4534eaf
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-03-24 14:31:07 +0100

    Add decorators to check agent role (#609)

--- ami/agent/decorators.py
+++ ami/agent/decorators.py
@@ -1,5 +1,8 @@
+from functools import wraps
+
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import redirect
 
 
 def agent_login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
@@ -20,3 +23,33 @@ def agent_login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME,
     if function:
         return actual_decorator(function)
     return actual_decorator
+
+
+def role_support_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.agent.has_role_support:
+            return redirect("agent-admin:access-denied")
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+def role_notifications_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.agent.has_role_notifications:
+            return redirect("agent-admin:access-denied")
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
+
+
+def role_admin_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.agent.has_role_admin:
+            return redirect("agent-admin:access-denied")
+        return view_func(request, *args, **kwargs)
+
+    return wrapper
