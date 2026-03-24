28f0c58156332a5f0baece099a524fd15aa58e76
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-03-24 16:50:49 +0100

    Display agents on management page (#609)

--- ami/agent_admin/views.py
+++ ami/agent_admin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from ami.agent.decorators import agent_login_required, role_admin_required, role_support_required
+from ami.agent.models import Agent
 
 
 @agent_login_required
@@ -26,4 +27,14 @@ def access_denied(request):
 @agent_login_required
 @role_admin_required
 def manage_access(request):
-    return render(request, "agent_admin/manage_access.html", {})
+    unauthorized_agents = Agent.objects.filter(role__isnull=True).order_by("-proconnect_last_login")
+    authorized_agents = Agent.objects.filter(role__isnull=False).order_by(
+        "user__last_name",
+        "user__first_name",
+    )
+    context = {
+        "unauthorized_agents": unauthorized_agents,
+        "authorized_agents": authorized_agents,
+        "roles": Agent.Role,
+    }
+    return render(request, "agent_admin/manage_access.html", context)
