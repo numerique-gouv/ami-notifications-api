e9a725aaa0edefbb4682e4f60d4bac8f1e85903a
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-03-24 16:50:49 +0100

    Display agents on management page (#609)

--- ami/agent_admin/tests/test_manage.py
+++ ami/agent_admin/tests/test_manage.py
@@ -1,13 +1,27 @@
 import pytest
+from django.utils.formats import date_format
+from pyquery import PyQuery
 
 from ami.agent.models import Agent
 from ami.agent_admin.tests.utils import assert_query_fails_without_agent_admin_auth
 
 
 @pytest.mark.django_db
-def test_manage_access(django_app, admin_agent: Agent) -> None:
+def test_manage_access(
+    django_app, agent: Agent, support_agent: Agent, notifications_agent: Agent, admin_agent: Agent
+) -> None:
     django_app.set_user(admin_agent.user)
-    django_app.get("/agent-admin/manage/access/", status=200)
+    response = django_app.get("/agent-admin/manage/access/")
+    assert [PyQuery(tr).text() for tr in response.pyquery("#with-role tr")] == [
+        "AGENT Admin\nAdmin",
+        "AGENT Notifications\nNotifications",
+        "AGENT Support\nSupport",
+    ]
+    last_login = date_format(agent.proconnect_last_login, "d/m/Y à H\\Hi")
+    assert [PyQuery(tr).text() for tr in response.pyquery("#without-role tr")] == [
+        "Agent concerné\nDate de connexion\nStatut",
+        f"AGENT Simple\n{last_login}",
+    ]
 
 
 @pytest.mark.django_db
