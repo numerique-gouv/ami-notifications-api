190855eab680ad2f9353f6d1a93304353e0bdd1f
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-03-27 13:31:39 +0100

    Create RolesRecordEntry on forms updates (#609)

--- ami/agent_admin/views.py
+++ ami/agent_admin/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.utils.timezone import now
 
 from ami.agent.decorators import agent_login_required, role_admin_required, role_support_required
 from ami.agent.models import Agent
 from ami.agent_admin.forms import AgentFormSet
+from ami.agent_admin.models import RolesRecordEntry
 
 
 @agent_login_required
@@ -45,6 +47,7 @@ def manage_access(request):
         is_unauthorized_agents_formset_valid = unauthorized_agents_formset.is_valid()
         is_authorized_agents_formset_valid = authorized_agents_formset.is_valid()
         if is_unauthorized_agents_formset_valid and is_authorized_agents_formset_valid:
+            update_roles_record(request, authorized_agents_formset, unauthorized_agents_formset)
             unauthorized_agents_formset.save()
             authorized_agents_formset.save()
             return redirect(reverse("agent-admin:manage-access"))
@@ -65,3 +68,28 @@ def manage_access(request):
     }
 
     return render(request, "agent_admin/manage_access.html", context)
+
+
+def update_roles_record(request, authorized_agents_formset, unauthorized_agents_formset):
+    admin_agent = Agent.objects.get(user=request.user)
+    for form in authorized_agents_formset:
+        if form.has_changed():
+            create_roles_record_entry(admin_agent, form)
+    for form in unauthorized_agents_formset:
+        if form.has_changed():
+            create_roles_record_entry(admin_agent, form)
+
+
+def create_roles_record_entry(admin_agent, form):
+    updated_agent = form.instance
+    new_role = form.cleaned_data["role"]
+    updated_user_from_db = Agent.objects.get(id=form.instance.id)
+    old_role = updated_user_from_db.role
+    update_date = now()
+    RolesRecordEntry.objects.create(
+        updated_agent=updated_agent,
+        admin_agent=admin_agent,
+        new_role=new_role,
+        old_role=old_role,
+        update_date=update_date,
+    )
