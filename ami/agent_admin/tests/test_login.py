dc5a148d9d7822fd8d34fe57ef697a9cd01aa8f1
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-03-31 11:20:57 +0200

    Add style on login page (#609)

--- ami/agent_admin/tests/test_login.py
+++ ami/agent_admin/tests/test_login.py
@@ -1,5 +1,7 @@
 import pytest
 
+from ami.agent.models import Agent
+
 
 @pytest.mark.django_db
 def test_login_pro_connect(
@@ -10,3 +12,13 @@ def test_login_pro_connect(
     response = response.click(href="/agent-admin/oidc/authenticate/")
     redirected_url = response.headers["location"]
     assert redirected_url.startswith(settings.OIDC_OP_AUTHORIZATION_ENDPOINT)
+
+
+@pytest.mark.django_db
+def test_login_pro_connect_with_auth(
+    django_app,
+    agent: Agent,
+) -> None:
+    django_app.set_user(agent.user)
+    response = django_app.get("/agent-admin/login/")
+    assert "/agent-admin/oidc/authenticate/" not in response
