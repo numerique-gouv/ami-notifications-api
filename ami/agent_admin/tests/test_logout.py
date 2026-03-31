6906d87b406ac18394c148aef36714909ff83c11
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-03-31 12:18:46 +0200

    Add logout form in header and remove logout page (#609)

--- ami/agent_admin/tests/test_logout.py
+++ ami/agent_admin/tests/test_logout.py
@@ -1,18 +1,19 @@
 import pytest
 
 from ami.agent.models import Agent
-from ami.agent_admin.tests.utils import assert_query_fails_without_agent_auth
 
 
 @pytest.mark.django_db
-def test_logout(settings, django_app, agent: Agent) -> None:
-    django_app.set_user(agent.user)
-    response = django_app.get("/agent-admin/logout/")
-    response = response.form.submit()
+def test_logout(settings, django_app, admin_agent: Agent) -> None:
+    django_app.set_user(admin_agent.user)
+    response = django_app.get("/agent-admin/")
+    assert "/agent-admin/oidc/logout/" in response
+    response = response.forms["logout-form"].submit()
     redirected_url = response.headers["location"]
     assert redirected_url.startswith(settings.OIDC_OP_LOGOUT_ENDPOINT)
 
 
 @pytest.mark.django_db
-def test_logout_without_agent_auth(django_app) -> None:
-    assert_query_fails_without_agent_auth(django_app, "/agent-admin/logout/")
+def test_logout_on_login_page(settings, django_app) -> None:
+    response = django_app.get("/agent-admin/login/")
+    assert "/agent-admin/oidc/logout/" not in response
