dad3d4d8d6937c820947f5be0316d6fe1d4e48ec
Mathieu Agopian <mathieu@agopian.info>
2026-04-02 15:51:11 +0200

    Scroll the address input to the top when there are addresses listed below it following some input

--- public/mobile-app/src/lib/utils.ts
+++ public/mobile-app/src/lib/utils.ts
@@ -13,16 +13,20 @@ export const formatDate = (data: string | Date) => {
   return date.toLocaleDateString('fr-FR', options);
 };
 
-export const scrollToInput = (event: FocusEvent & { currentTarget: HTMLElement }) => {
-  const input = event.currentTarget;
+export const scrollToNode = (node: HTMLElement) => {
   const nav = document.querySelector('nav');
   if (!nav) {
     return;
   }
-  const inputOffsetTop =
-    input.getBoundingClientRect().top -
+  const nodeOffsetTop =
+    node.getBoundingClientRect().top -
     nav.getBoundingClientRect().bottom +
     (document.scrollingElement?.scrollTop || 0) -
-    5; // Just a small margin to avoid cutting the top of the input
-  window.scrollTo({ top: inputOffsetTop, behavior: 'smooth' });
+    5; // Just a small margin to avoid cutting the top of the node
+  window.scrollTo({ top: nodeOffsetTop, behavior: 'smooth' });
+};
+
+export const scrollToInput = (event: FocusEvent & { currentTarget: HTMLElement }) => {
+  const input = event.currentTarget || event.target;
+  scrollToNode(input);
 };
