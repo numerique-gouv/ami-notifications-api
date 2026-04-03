f44826c09b6c2b74a22c1edb8edb4d73b733d204
Lauréline Guérin <zebuline@crocobox.org>
2026-04-03 16:40:26 +0200

    httpx: add caching to GET requests (#508)

--- ami/utils/httpx.py
+++ ami/utils/httpx.py
@@ -1,9 +1,50 @@
+import hashlib
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
-from httpx import URL, AsyncClient, Client  # noqa
+from django.core.cache import cache
+from django.utils.encoding import smart_bytes
+from httpx import URL, AsyncClient, Client, Response  # noqa
 
-httpxClient = Client(timeout=60)
+
+def get_cache_key(url: str):
+    return hashlib.md5(smart_bytes(url)).hexdigest()
+
+
+class AMIClient(Client):
+    def get(self, *args, **kwargs):
+        return self.request("GET", *args, **kwargs)
+
+    def request(
+        self,
+        method: str,
+        url: URL | str,
+        *,
+        cache_duration: int | None = None,
+        **kwargs,
+    ) -> Response:
+        request = self.build_request(
+            method,
+            url,
+            **{k: v for k, v in kwargs.items() if k not in ["auth", "follow_redirects"]},
+        )
+
+        cache_key = get_cache_key(str(request.url))
+        if method == "GET" and cache_duration:
+            cache_content = cache.get(cache_key)
+            if cache_content:
+                response = Response(status_code=200, content=cache_content)
+                return response
+
+        response = super().request(method, url, **kwargs)
+
+        if method == "GET" and cache_duration and (response.status_code // 100 == 2):
+            cache.set(cache_key, response.content, cache_duration)
+
+        return response
+
+
+httpxClient = AMIClient(timeout=60)
 
 
 @asynccontextmanager
---
+++ ami/utils/tests/test_httpx.py
@@ -0,0 +1,112 @@
+from django.core.cache import cache
+from pytest_httpx import HTTPXMock
+
+from ami.utils.httpx import URL, get_cache_key, httpxClient
+
+
+def test_httpx_request_without_cache(
+    app,
+    httpx_mock: HTTPXMock,
+):
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get("http://foo/bar")
+    assert response.json() == {"foo": "bar"}
+
+    # again, no cache here
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get("http://foo/bar")
+    assert response.json() == {"foo": "bar"}
+
+
+def test_httpx_request_with_cache(
+    app,
+    httpx_mock: HTTPXMock,
+):
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get("http://foo/bar", cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+    # again, value in cache
+    response = httpxClient.get("http://foo/bar", cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+    # new url
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get("http://foo/baz", cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+
+def test_httpx_request_with_cache_with_params(
+    app,
+    httpx_mock: HTTPXMock,
+):
+    httpx_mock.add_response(json={"foo": "bar1"})
+    response = httpxClient.get("http://foo/bar", params={"key": "value1"}, cache_duration=1)
+    assert response.json() == {"foo": "bar1"}
+
+    # other params
+    httpx_mock.add_response(json={"foo": "bar2"})
+    response = httpxClient.get("http://foo/bar", params={"key": "value2"}, cache_duration=1)
+    assert response.json() == {"foo": "bar2"}
+
+    # again, value in cache
+    response = httpxClient.get("http://foo/bar", params={"key": "value2"}, cache_duration=1)
+    assert response.json() == {"foo": "bar2"}
+
+
+def test_httpx_request_with_cache_expired(
+    app,
+    httpx_mock: HTTPXMock,
+):
+    # cache is expired
+    cache_key = get_cache_key("http://foo/bar")
+    cache.set(cache_key, "content", 0)
+
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get("http://foo/bar", cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+
+def test_httpx_request_dont_use_cache(
+    app,
+    httpx_mock: HTTPXMock,
+):
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get("http://foo/bar", cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+    # no cache_duration, don't get value in cache
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get("http://foo/bar")
+    assert response.json() == {"foo": "bar"}
+
+
+def test_httpx_request_with_URL(
+    app,
+    httpx_mock: HTTPXMock,
+):
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get(URL("http://foo/bar"), cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+    response = httpxClient.get(URL("http://foo/bar"), cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+
+def test_httpx_request_with_URL_and_params(
+    app,
+    httpx_mock: HTTPXMock,
+):
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get(URL("http://foo/bar"), cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+    httpx_mock.add_response(json={"foo": "bar"})
+    response = httpxClient.get(URL("http://foo/bar"), params={"key": "value"}, cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+    response = httpxClient.get(URL("http://foo/bar"), params={"key": "value"}, cache_duration=1)
+    assert response.json() == {"foo": "bar"}
+
+    response = httpxClient.get(URL("http://foo/bar?key=value"), cache_duration=1)
+    assert response.json() == {"foo": "bar"}
