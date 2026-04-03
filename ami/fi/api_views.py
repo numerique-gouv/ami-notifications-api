922ca6e982925533ef1ccc028377bb02f86fce8c
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-04-03 14:50:05 +0200

    FI: implement logout (#708)

--- ami/fi/api_views.py
+++ ami/fi/api_views.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import redirect
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -19,5 +22,11 @@ def userinfo(request: Request) -> Response:
 
 
 @api_view(["GET"])
-def logout(request: Request) -> Response:
-    return Response()
+def logout(request: Request) -> HttpResponseBadRequest | HttpResponseRedirect:
+    redirect_uri = request.GET.get("post_logout_redirect_uri")
+    if redirect_uri != f"{settings.PUBLIC_FC_BASE_URL}{settings.FC_LOGOUT_CALLBACK_ENDPOINT}":
+        return HttpResponseBadRequest()
+
+    redirect_uri = f"{redirect_uri}?state={request.GET.get('state')}"
+
+    return redirect(redirect_uri)
