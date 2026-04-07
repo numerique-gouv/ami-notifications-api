b153fd0c666a49d1d0aecec905237c1d75281da8
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-04-07 17:49:01 +0200

    FI: implement token (#708)

--- ami/fi/api_views.py
+++ ami/fi/api_views.py
@@ -1,10 +1,14 @@
 import logging
 import re
+from secrets import token_urlsafe
+from typing import cast
 
+import jwt
 from django.conf import settings
 from django.contrib.auth.hashers import make_password
 from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,6 +20,8 @@ from ami.fi.api_exceptions import (
     WrongFormatAuthHeader,
 )
 from ami.fi.models import FISession
+from ami.fi.serializers import TokenSerializer
+from ami.fi.utils import generate_id_token
 
 logger = logging.getLogger(__name__)
 
@@ -26,8 +32,42 @@ def authorize(request: Request) -> Response:
 
 
 @api_view(["POST"])
-def token(request: Request) -> Response:
-    return Response()
+def token(request: Request) -> JsonResponse:
+    serializer = TokenSerializer(data=request.data)
+    try:
+        serializer.is_valid(raise_exception=True)
+    except serializers.ValidationError as e:
+        logging.exception(e)
+        raise
+    data: dict = cast(dict, serializer.validated_data)
+
+    try:
+        fi_session = FISession.objects.get(code=data["code"])
+        if fi_session.is_expired:
+            logger.error("Session de connexion à AMI-FI expirée")
+            raise FISessionExpired
+    except FISession.DoesNotExist:
+        logger.error("Session de connexion à AMI-FI non trouvée")
+        raise FISessionNotFound
+
+    encoded_id_token = jwt.encode(
+        generate_id_token(fi_session),
+        data["client_secret"],
+        algorithm="HS256",
+    )
+
+    access_token = token_urlsafe(64)
+    fi_session.access_token = make_password(access_token, settings.FI_HASH_SALT)
+    fi_session.save()
+
+    return JsonResponse(
+        {
+            "access_token": access_token,
+            "expires_in": 60,
+            "id_token": encoded_id_token,
+            "token_type": "Bearer",
+        }
+    )
 
 
 @api_view(["GET"])
--- ami/fi/tests/test_token.py
+++ ami/fi/tests/test_token.py
@@ -1,5 +1,197 @@
+import datetime
+from unittest import mock
+
+import pytest
+from django.contrib.auth.hashers import make_password
+from freezegun import freeze_time
+
+from ami.fi.models import FISession
+
+
+@freeze_time("2026-04-07 17:21:00")
+@pytest.mark.django_db
 def test_token(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    user_data = {"sub": "fake-sub"}
+    nonce = "fake-nonce"
+    code = "fake-code"
+    fi_session = FISession.objects.create(user_data=user_data, nonce=nonce, code=code)
+    token_data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": settings.FC_AMI_REDIRECT_URL,
+        "client_id": settings.FI_CLIENT_ID,
+        "client_secret": settings.FI_CLIENT_SECRET,
+    }
+
+    encode_mock = mock.Mock(return_value="fake-encoded-id-token")
+    monkeypatch.setattr("jwt.encode", encode_mock)
+
+    monkeypatch.setattr("ami.fi.api_views.token_urlsafe", lambda a: "fake-access-token")
+    expected_access_token = make_password("fake-access-token", settings.FI_HASH_SALT)
+
+    response = app.post("/api/v1/fi/token/", token_data)
+    assert response.json == {
+        "access_token": "fake-access-token",
+        "expires_in": 60,
+        "id_token": "fake-encoded-id-token",
+        "token_type": "Bearer",
+    }
+    encode_mock.assert_called_once_with(
+        {
+            "aud": "33fe498cc172fe691778912a2967baa650b24f1ae0ebbe47ae552f37b2d25ead",
+            "exp": 1775582760,
+            "iat": 1775582460,
+            "iss": "https://localhost:8000/api/v1/fi/",
+            "sub": "fake-sub",
+            "nonce": "fake-nonce",
+            "acr": "eidas1",
+        },
+        settings.FI_CLIENT_SECRET,
+        algorithm="HS256",
+    )
+    fi_session.refresh_from_db()
+    assert fi_session.access_token == expected_access_token
+
+
+def test_token_invalid_data_code(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    code = ""
+    token_data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": settings.FC_AMI_REDIRECT_URL,
+        "client_id": settings.FI_CLIENT_ID,
+        "client_secret": settings.FI_CLIENT_SECRET,
+    }
+
+    response = app.post("/api/v1/fi/token/", token_data, status=400)
+    assert response.json == {"code": ["Ce champ ne peut être vide."]}
+
+
+def test_token_invalid_grant_type(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    code = "fake-code"
+    token_data = {
+        "code": code,
+        "grant_type": "invalid-grant-type",
+        "redirect_uri": settings.FC_AMI_REDIRECT_URL,
+        "client_id": settings.FI_CLIENT_ID,
+        "client_secret": settings.FI_CLIENT_SECRET,
+    }
+
+    response = app.post("/api/v1/fi/token/", token_data, status=400)
+    assert response.json == {
+        "grant_type": ["'grant_type' doit être 'authorization_code', trouvé 'invalid-grant-type'"]
+    }
+
+
+def test_token_invalid_redirect_uri(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    code = "fake-code"
+    token_data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": "invalid-redirect-uri",
+        "client_id": settings.FI_CLIENT_ID,
+        "client_secret": settings.FI_CLIENT_SECRET,
+    }
+
+    response = app.post("/api/v1/fi/token/", token_data, status=400)
+    assert response.json == {
+        "redirect_uri": [
+            f"'redirect_uri' doit être '{settings.FC_AMI_REDIRECT_URL}', trouvé 'invalid-redirect-uri'"
+        ]
+    }
+
+
+def test_token_invalid_client_id(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    code = "fake-code"
+    token_data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": settings.FC_AMI_REDIRECT_URL,
+        "client_id": "invalid-client-id",
+        "client_secret": settings.FI_CLIENT_SECRET,
+    }
+
+    response = app.post("/api/v1/fi/token/", token_data, status=400)
+    assert response.json == {"client_id": ["'client_id' invalide"]}
+
+
+def test_token_invalid_client_secret(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    code = "fake-code"
+    token_data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": settings.FC_AMI_REDIRECT_URL,
+        "client_id": settings.FI_CLIENT_ID,
+        "client_secret": "invalid-client-secret",
+    }
+
+    response = app.post("/api/v1/fi/token/", token_data, status=400)
+    assert response.json == {"client_secret": ["'client_secret' invalide"]}
+
+
+@pytest.mark.django_db
+def test_token_fi_session_expired(
+    settings,
     app,
 ) -> None:
-    response = app.post("/api/v1/fi/token/")
-    assert response.status_code == 200
+    user_data = {"sub": "fake-sub"}
+    nonce = "fake-nonce"
+    code = "fake-code"
+    fi_session = FISession.objects.create(user_data=user_data, nonce=nonce, code=code)
+    fi_session.created_at -= datetime.timedelta(seconds=settings.FI_SESSION_AGE)
+    fi_session.save()
+    token_data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": settings.FC_AMI_REDIRECT_URL,
+        "client_id": settings.FI_CLIENT_ID,
+        "client_secret": settings.FI_CLIENT_SECRET,
+    }
+
+    response = app.post("/api/v1/fi/token/", token_data, status=403)
+    assert response.json == {"detail": "Session de connexion à AMI-FI expirée"}
+
+
+@pytest.mark.django_db
+def test_token_fi_session_not_found(
+    settings,
+    app,
+) -> None:
+    user_data = {"sub": "fake-sub"}
+    nonce = "fake-nonce"
+    code = "fake-code"
+    FISession.objects.create(user_data=user_data, nonce=nonce, code="other-code")
+    token_data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": settings.FC_AMI_REDIRECT_URL,
+        "client_id": settings.FI_CLIENT_ID,
+        "client_secret": settings.FI_CLIENT_SECRET,
+    }
+
+    response = app.post("/api/v1/fi/token/", token_data, status=403)
+    assert response.json == {"detail": "Session de connexion à AMI-FI non trouvée"}
