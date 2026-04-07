a90d96d1b5ffee8eb9d0d38072b329fc575a46c4
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-04-07 14:24:59 +0200

    FI: implement userinfo (#708)

--- ami/fi/api_views.py
+++ ami/fi/api_views.py
@@ -1,9 +1,23 @@
+import logging
+import re
+
 from django.conf import settings
-from django.http import HttpResponseBadRequest, HttpResponseRedirect
+from django.contrib.auth.hashers import make_password
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+
+from ami.fi.api_exceptions import (
+    FISessionExpired,
+    FISessionNotFound,
+    MissingAuthHeader,
+    WrongFormatAuthHeader,
+)
+from ami.fi.models import FISession
+
+logger = logging.getLogger(__name__)
 
 
 @api_view(["GET"])
@@ -17,8 +31,29 @@ def token(request: Request) -> Response:
 
 
 @api_view(["GET"])
-def userinfo(request: Request) -> Response:
-    return Response()
+def userinfo(request: Request) -> JsonResponse:
+    auth_header = request.META.get("HTTP_AUTHORIZATION")
+    if not auth_header:
+        logger.error("Header d'authentification manquant")
+        raise MissingAuthHeader
+
+    pattern = re.compile(r"^Bearer\s([A-Z-a-z-0-9-_/-]+)$")
+    if not pattern.match(auth_header):
+        logger.error("Header d'authentification mal formé")
+        raise WrongFormatAuthHeader
+
+    auth_token = auth_header[7:]
+    auth_token_hash = make_password(auth_token, settings.FI_HASH_SALT)
+    try:
+        fi_session = FISession.objects.get(access_token=auth_token_hash)
+        if fi_session.is_expired:
+            logger.error("Session de connexion à AMI-FI expirée")
+            raise FISessionExpired
+    except FISession.DoesNotExist:
+        logger.error("Session de connexion à AMI-FI non trouvée")
+        raise FISessionNotFound
+
+    return JsonResponse(fi_session.user_data)
 
 
 @api_view(["GET"])
