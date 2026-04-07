41f749ac8533285e1863c8224a10a8fa22c49703
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-04-07 14:24:59 +0200

    FI: implement userinfo (#708)

--- ami/fi/tests/test_userinfo.py
+++ ami/fi/tests/test_userinfo.py
@@ -1,5 +1,68 @@
+import datetime
+
+import pytest
+from django.conf import settings
+from django.contrib.auth.hashers import make_password
+
+from ami.fi.models import FISession
+
+
+@pytest.mark.django_db
 def test_userinfo(
     app,
 ) -> None:
-    response = app.get("/api/v1/fi/userinfo/")
-    assert response.status_code == 200
+    auth_token = "fake-access-token"
+    auth_token_hash = make_password(auth_token, settings.FI_HASH_SALT)
+    user_data = {"fake-key": "fake-user-data"}
+    FISession.objects.create(user_data=user_data, access_token=auth_token_hash)
+    response = app.get("/api/v1/fi/userinfo/", headers={"AUTHORIZATION": f"Bearer {auth_token}"})
+    assert response.json == user_data
+
+
+@pytest.mark.django_db
+def test_userinfo_missing_auth_header(
+    app,
+) -> None:
+    user_data = {"fake-key": "fake-user-data"}
+    FISession.objects.create(user_data=user_data, access_token="fake-access-token")
+    response = app.get("/api/v1/fi/userinfo/", status=403)
+    assert response.json == {"detail": "Header d'authentification manquant"}
+
+
+@pytest.mark.django_db
+def test_userinfo_wrong_format_auth_header(
+    app,
+) -> None:
+    auth_token = "fake-access-token"
+    user_data = {"fake-key": "fake-user-data"}
+    FISession.objects.create(user_data=user_data, access_token="fake-access-token")
+    response = app.get("/api/v1/fi/userinfo/", headers={"AUTHORIZATION": auth_token}, status=403)
+    assert response.json == {"detail": "Header d'authentification mal formé"}
+
+
+@pytest.mark.django_db
+def test_userinfo_fi_session_expired(
+    app,
+) -> None:
+    auth_token = "fake-access-token"
+    auth_token_hash = make_password(auth_token, settings.FI_HASH_SALT)
+    user_data = {"fake-key": "fake-user-data"}
+    fi_session = FISession.objects.create(user_data=user_data, access_token=auth_token_hash)
+    fi_session.created_at -= datetime.timedelta(seconds=settings.FI_SESSION_AGE)
+    fi_session.save()
+    response = app.get(
+        "/api/v1/fi/userinfo/", headers={"AUTHORIZATION": f"Bearer {auth_token}"}, status=403
+    )
+    assert response.json == {"detail": "Session de connexion à AMI-FI expirée"}
+
+
+@pytest.mark.django_db
+def test_userinfo_fi_session_not_found(
+    app,
+) -> None:
+    user_data = {"fake-key": "fake-user-data"}
+    FISession.objects.create(user_data=user_data, access_token="fake-access-token")
+    response = app.get(
+        "/api/v1/fi/userinfo/", headers={"AUTHORIZATION": "Bearer azerty"}, status=403
+    )
+    assert response.json == {"detail": "Session de connexion à AMI-FI non trouvée"}
