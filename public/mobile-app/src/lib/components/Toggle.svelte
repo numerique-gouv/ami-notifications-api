e396d177306526bfaefbda306288e3399b68d877
Lauréline Guérin <zebuline@crocobox.org>
2026-04-14 14:40:49 +0200

    front: Toogle component (#508)

---
+++ public/mobile-app/src/lib/components/Toggle.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  interface Props {
+    id: string;
+    label: string;
+    isChecked: boolean;
+    onChangeAction: (id: string, checked: boolean) => void;
+  }
+  let { id, label, isChecked, onChangeAction }: Props = $props();
+</script>
+
+<div class="fr-toggle">
+  <input
+    type="checkbox"
+    class="fr-toggle__input"
+    id={id}
+    aria-describedby="toggle-messages toggle-hint"
+    checked={isChecked}
+    onchange={(e) => onChangeAction((e.target as HTMLInputElement).id, (e.target as HTMLInputElement).checked)}
+    data-testid="{id}"
+  >
+  <label class="fr-toggle__label" for={id}>{label}</label>
+</div>
+
+<style>
+  .fr-toggle {
+    padding: 1rem;
+    label.fr-toggle__label {
+      display: flex;
+      position: relative;
+      &:before {
+        display: flex;
+        position: absolute;
+        right: -2rem;
+        margin: 0;
+      }
+      &:after {
+        position: absolute;
+        left: auto;
+        right: -1rem;
+      }
+    }
+  }
+</style>
