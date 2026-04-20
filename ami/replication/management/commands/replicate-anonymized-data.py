b6676679b044ba4f77d37de5840d63715ad78df1
Quentin Loridant <quentin.loridant@protonmail.com>
2026-04-20 10:11:34 +0200

    Refactoring replicate method and add database interaction tests (#791)

--- ami/replication/management/commands/replicate-anonymized-data.py
+++ ami/replication/management/commands/replicate-anonymized-data.py
@@ -1,36 +1,16 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from django.db import utils
 
-from ami.replication.models import AnonymizedUser
-from ami.user.models import User
+from ami.replication.app import replicate_anonymized_users
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Replicate anonymised data to a new database"
+    help = "Replicate anonymised data to a new database (Data Ware House)"
 
     def handle(self, *args, **kwargs):
-        page_size = 1000
-        offset = 0
-        total = 0
-
         logger.info("Starting anonymized data replication")
-
-        while True:
-            users = User.objects.all()[offset : offset + page_size]
-            if not users:
-                break
-            for user in users:
-                try:
-                    AnonymizedUser.from_user(user, using="data_ware_house")
-                except utils.DatabaseError:
-                    logger.error("Replication error: Cannot access the datawarehouse")
-            count = len(users)
-            total += count
-            offset += page_size
-            logger.info(f"Replicated {count} users (total: {total})")
-
-        logger.info(f"Anonymized data replication complete. Total users replicated: {total}")
+        count = replicate_anonymized_users()
+        logger.info(f"Anonymized data replication complete. Total users replicated: {count}")
