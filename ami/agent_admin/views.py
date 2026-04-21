98c770a0309af62bb339a4fab6a581d3650ef067
Lauréline Guérin <zebuline@crocobox.org>
2026-04-21 16:46:42 +0200

    agent-admin: notification request as AMI partner (#773)

--- ami/agent_admin/views.py
+++ ami/agent_admin/views.py
@@ -8,7 +8,7 @@ from ami.agent.decorators import (
     role_support_required,
 )
 from ami.agent.models import Agent
-from ami.agent_admin.forms import AgentFormSet
+from ami.agent_admin.forms import AgentFormSet, NotificationForm
 from ami.agent_admin.models import AuditEntry
 
 
@@ -111,5 +111,30 @@ def manage_access(request):
 @agent_login_required
 @role_notifications_required
 def send_notification(request):
-    context = {}
+    result_data_dict = None
+    if request.method == "POST":
+        form = NotificationForm(data=request.POST)
+        if form.is_valid():
+            result_data_dict = form.submit()
+    else:
+        form = NotificationForm()
+    context = {
+        "form": form,
+        "form_result_data_dict": result_data_dict,
+        "btn_group": {
+            "items": [
+                {
+                    "label": "Annuler",
+                    "type": "button",
+                    "extra_classes": "fr-btn--secondary",
+                    "onclick": "window.location.href = window.location.href;",
+                },
+                {
+                    "label": "Enregistrer",
+                    "type": "submit",
+                },
+            ],
+            "extra_classes": "fr-btns-group--inline fr-btns-group--form-actions",
+        },
+    }
     return render(request, "agent_admin/send_notification.html", context)
