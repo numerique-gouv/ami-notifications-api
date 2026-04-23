a97e2922db79dcc32369ed1ad22bd6d524b84602
Lauréline Guérin <zebuline@crocobox.org>
2026-04-23 18:29:38 +0200

    amidsfr: autocomplete input (#773)

--- ami/amidsfr/widgets.py
+++ ami/amidsfr/widgets.py
@@ -1,4 +1,15 @@
-from django.forms import CheckboxInput
+from django.forms import CheckboxInput, TextInput
+
+
+class AutocompleteInput(TextInput):
+    class Media:
+        js = ["js/amidsfr-autocomplete.js"]
+        css = {"all": ["css/amidsfr-autocomplete.css"]}
+
+    def __init__(self, autocomplete_url=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.group_class = "fr-input-group amidsfr-autocomplete"
+        self.attrs["data-autocomplete-url"] = autocomplete_url or ""
 
 
 class ToggleInput(CheckboxInput):
