9d236e1f4d408e15231d6bf3d26ea4911b63dab0
Lauréline Guérin <zebuline@crocobox.org>
2026-04-28 11:15:55 +0200

    agent-admin: implement delete user view (#774)

--- ami/agent_admin/views/manage/user_views.py
+++ ami/agent_admin/views/manage/user_views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,7 +9,9 @@ from ami.agent.decorators import (
     role_admin_required,
 )
 from ami.agent_admin.forms import UserSearchForm
-from ami.user.models import User
+from ami.agent_admin.utils import audit
+from ami.notification.models import Notification, ScheduledNotification
+from ami.user.models import Registration, User
 
 
 @agent_login_required
@@ -35,6 +38,21 @@ def detail_user(request, user_id):
     context = {
         "form": form,
         "ami_user": user,
+        "btn_group": {
+            "items": [
+                {
+                    "label": "Fermer",
+                    "type": "button",
+                    "onclick": f"window.location.href = '{reverse('agent-admin:manage:search-user')}';",
+                },
+                {
+                    "label": "Supprimer les données en base",
+                    "type": "button",
+                    "extra_classes": "fr-btn--secondary",
+                    "onclick": "confirmModal('modal-delete-user');",
+                },
+            ],
+        },
     }
     return render(request, "agent_admin/manage/detail_user.html", context)
 
@@ -44,4 +62,14 @@ def detail_user(request, user_id):
 @require_http_methods(["POST"])
 @csrf_exempt
 def delete_user(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    with transaction.atomic():
+        audit("user:deleted", request.user.agent, {"user": user})
+
+        Registration.objects.filter(user=user).delete()
+        Notification.objects.filter(user=user).delete()
+        ScheduledNotification.objects.filter(user=user).delete()
+        user.delete()
+
     return redirect(reverse("agent-admin:manage:search-user"))
