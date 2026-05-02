4e57a3a81ac1a2d9f3feee887c2418933312666e
Lauréline Guérin <zebuline@crocobox.org>
2026-05-02 16:26:50 +0200

    front: use Modal component for Logout modal (#807)

---
+++ public/mobile-app/src/lib/components/modal/LogoutFooter.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { userStore } from '$lib/state/User.svelte';
+
+  let {
+    onClose = null,
+  }: {
+    onClose?: (() => void) | null;
+  } = $props();
+</script>
+
+<ul class="fr-btns-group action-buttons">
+  <li>
+    <button
+      class="fr-btn fr-btn--secondary cancel-button"
+      type="button"
+      onclick={onClose}
+      data-testid="logout-cancel-button"
+    >
+      Annuler
+    </button>
+  </li>
+  <li>
+    <button
+      class="fr-btn submit-button"
+      type="button"
+      onclick={userStore.logout}
+      data-testid="logout-submit-button"
+    >
+      Confirmer
+    </button>
+  </li>
+</ul>
+
+<style>
+  .action-buttons {
+    background-color: var(--background-default-grey);
+    display: flex;
+    gap: 1rem;
+    margin: 0;
+    li {
+      flex: 1;
+      button {
+        display: block;
+        width: 100%;
+        margin: 0;
+      }
+    }
+  }
+</style>
