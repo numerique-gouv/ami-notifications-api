9288233d6a5d43f4f3ecfdc30172dc0b49aa92d2
Quentin Loridant <quentin.loridant@protonmail.com>
2026-05-04 10:33:20 +0200

    Ajout des Registrations (#791)

--- ami/replication/models.py
+++ ami/replication/models.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 import uuid
 
 from django.db import models
+from django.utils import timezone
 
-from ami.user.models import User
+from ami.notification.models import Notification
+from ami.user.models import Registration, User
 
 """
 The anonymized classes are like the original ones, to the exception of :
@@ -32,6 +34,49 @@ class AnonymizedModel(models.Model):
         return instance
 
 
+class AnonymizedNotification(AnonymizedModel):
+    # New Field - name must match the given one in the method from_source
+    replication_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # Not specifiying user reference
+
+    id = models.UUIDField(editable=False)
+    content_title = models.CharField()
+    content_body = models.CharField()
+    content_icon = models.CharField(blank=True, null=True)
+
+    item_type = models.CharField(blank=True, null=True)
+    item_id = models.CharField(blank=True, null=True)
+    item_status_label = models.CharField(blank=True, null=True)
+    item_generic_status = models.CharField(blank=True, null=True)
+    item_canal = models.CharField(blank=True, null=True)
+    item_milestone_start_date = models.DateTimeField(blank=True, null=True)
+    item_milestone_end_date = models.DateTimeField(blank=True, null=True)
+    item_external_url = models.CharField(blank=True, null=True)
+
+    send_status = models.BooleanField(blank=True, null=True)
+    partner_id = models.CharField()
+    internal_url = models.CharField(
+        blank=True, null=True
+    )  # to link notification to a front url; used by scheduled notifications
+
+    read = models.BooleanField(default=False)
+    send_date = models.DateTimeField(default=timezone.now)
+    try_push = models.BooleanField(blank=True, null=True)
+
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta(AnonymizedModel.Meta):
+        db_table = "ami_notification_anonymized"
+
+    @classmethod
+    def from_notification(
+        cls, notification: Notification, using: str = "default"
+    ) -> AnonymizedNotification:
+        return cls.from_source(notification, using)
+
+
 class AnonymizedUser(AnonymizedModel):
     # New Field - name must match the given one in the method from_source
     replication_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
@@ -48,3 +93,22 @@ class AnonymizedUser(AnonymizedModel):
     @classmethod
     def from_user(cls, user: User, using: str = "default") -> AnonymizedUser:
         return cls.from_source(user, using)
+
+
+class AnonymizedRegistration(AnonymizedModel):
+    # New Field - name must match the given one in the method from_source
+    replication_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # Not specifying user reference nor subscription data
+    id = models.UUIDField(editable=False)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta(AnonymizedModel.Meta):
+        db_table = "ami_registration_anonymized"
+
+    @classmethod
+    def from_registration(
+        cls, registration: Registration, using: str = "default"
+    ) -> AnonymizedRegistration:
+        return cls.from_source(registration, using)
