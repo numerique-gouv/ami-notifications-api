cb39f4209d9ad3aac693a0691cf8df2d47807342
Lauréline Guérin <zebuline@crocobox.org>
2026-05-04 09:01:53 +0200

    FI: front page and api view to test login (#708)

---
+++ public/mobile-app/src/routes/fi/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { PUBLIC_API_URL } from '$env/static/public';
+
+  const AMIFILogin = async () => {
+    window.location.href = `${PUBLIC_API_URL}/login-ami-fi`;
+  };
+</script>
+
+<div>
+  <button class="fr-connect" type="button" id="fr-connect-button" onclick={AMIFILogin}>
+    Test AMI-FI
+  </button>
+</div>
