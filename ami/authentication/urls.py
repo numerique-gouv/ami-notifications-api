dd7b38d504f04ea7f11810e6b84f1c44e9d55752
Lauréline Guérin <zebuline@crocobox.org>
2026-05-04 09:01:53 +0200

    FI: front page and api view to test login (#708)

--- ami/authentication/urls.py
+++ ami/authentication/urls.py
@@ -4,5 +4,6 @@ from ami.authentication import views
 
 urlpatterns = [
     path("login-france-connect", views.login_france_connect),
+    path("login-ami-fi", views.login_ami_fi),
     path("login-callback", views.login_callback),
 ]
