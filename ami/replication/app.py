e00a8af93abcd6ad30142520310533cc8282b9ae
Quentin Loridant <quentin.loridant@protonmail.com>
2026-05-04 10:33:20 +0200

    Ajout des Registrations (#791)

--- ami/replication/app.py
+++ ami/replication/app.py
@@ -2,8 +2,9 @@ import logging
 
 from django.db import utils
 
-from ami.replication.models import AnonymizedUser
-from ami.user.models import User
+from ami.notification.models import Notification
+from ami.replication.models import AnonymizedNotification, AnonymizedRegistration, AnonymizedUser
+from ami.user.models import Registration, User
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +15,31 @@ def replicate_anonymized_users(chunk_size=1000):
         try:
             AnonymizedUser.from_user(user, using="data_ware_house")
         except utils.DatabaseError:
-            logger.error("Replication error: Cannot access the datawarehouse")
+            logger.error("Replication users error: Cannot access the datawarehouse")
         count += chunk_size
     logger.info(f"Replicated {count} users")
+    return count
+
+
+def replicate_anonymized_notifications(chunk_size=1000):
+    count = 0
+    for notification in Notification.objects.order_by("created_at").iterator(chunk_size=chunk_size):
+        try:
+            AnonymizedNotification.from_notification(notification, using="data_ware_house")
+        except utils.DatabaseError:
+            logger.error("Replication error: Cannot access the datawarehouse")
+        count += chunk_size
+    logger.info(f"Replicated {count} notifications")
+    return count
+
+
+def replicate_anonymized_registrations(chunk_size=1000):
+    count = 0
+    for registration in Registration.objects.order_by("created_at").iterator(chunk_size=chunk_size):
+        try:
+            AnonymizedRegistration.from_registration(registration, using="data_ware_house")
+        except utils.DatabaseError:
+            logger.error("Replication error: Cannot access the datawarehouse")
+        count += chunk_size
+    logger.info(f"Replicated {count} registrations")
     return count
