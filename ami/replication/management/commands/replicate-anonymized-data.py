47043c258187e2dc5ee99f7b2a217a2abdef6794
Quentin Loridant <quentin.loridant@protonmail.com>
2026-05-13 11:03:39 +0200

    Call all replication methods (#791)

--- ami/replication/management/commands/replicate-anonymized-data.py
+++ ami/replication/management/commands/replicate-anonymized-data.py
@@ -2,7 +2,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from ami.replication.app import replicate_anonymized_users
+from ami.replication.app import replicate_anonymized_data
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +11,6 @@ class Command(BaseCommand):
     help = "Replicate anonymised data to a new database (Data Ware House)"
 
     def handle(self, *args, **kwargs):
-        logger.info("Starting anonymized data replication")
-        count = replicate_anonymized_users()
-        logger.info(f"Anonymized data replication complete. Total users replicated: {count}")
+        logger.info("Starting anonymized data replication ... ")
+        replicate_anonymized_data()
+        logger.info("Anonymized data replication complete !")
