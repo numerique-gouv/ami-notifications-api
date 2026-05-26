d80f4e1df4bf17365043b0e230fa5dc35a118ae6
Quentin Loridant <quentin.loridant@protonmail.com>
2026-05-26 11:59:01 +0200

    Route replication database accesses to datawarehouse (#904)

--- ami/replication/tests/test_replication.py
+++ ami/replication/tests/test_replication.py
@@ -9,8 +9,9 @@ from ami.replication.app import (
 from ami.replication.models import AnonymizedNotification, AnonymizedRegistration, AnonymizedUser
 from ami.user.models import Registration, User
 
+pytestmark = pytest.mark.django_db(databases=["default", "data_ware_house"])
 
-@pytest.mark.django_db
+
 def test_anonymized_user_does_not_have_fc_hash(user: User) -> None:
     anonymized = AnonymizedUser.from_user(user)
     assert not hasattr(anonymized, "fc_hash")
@@ -19,28 +20,25 @@ def test_anonymized_user_does_not_have_fc_hash(user: User) -> None:
     assert hasattr(anonymized, "updated_at")
 
 
-@pytest.mark.django_db(databases=["default", "data_ware_house"])
 def test_replicate_anonymized_users(two_users: list[User]) -> None:
     replicate_anonymized_users()
 
-    assert AnonymizedUser.objects.using("data_ware_house").count() == 2
+    assert AnonymizedUser.objects.count() == 2
     for user in two_users:
-        anonymized = AnonymizedUser.objects.using("data_ware_house").get(id=user.id)
+        anonymized = AnonymizedUser.objects.get(id=user.id)
         assert anonymized.last_logged_in == user.last_logged_in
         assert anonymized.created_at == user.created_at
         assert anonymized.updated_at == user.updated_at
 
 
-@pytest.mark.django_db(databases=["default", "data_ware_house"])
 def test_replicate_anonymized_users_processes_by_batch(two_users: list[User]) -> None:
     replicate_anonymized_users(chunk_size=1)
 
-    assert AnonymizedUser.objects.using("data_ware_house").count() == 2
+    assert AnonymizedUser.objects.count() == 2
     for user in two_users:
-        assert AnonymizedUser.objects.using("data_ware_house").filter(id=user.id).exists()
+        assert AnonymizedUser.objects.filter(id=user.id).exists()
 
 
-@pytest.mark.django_db
 def test_anonymized_notification_does_not_have_user(notification: Notification) -> None:
     anonymized = AnonymizedNotification.from_notification(notification)
     assert not hasattr(anonymized, "user")
@@ -50,18 +48,16 @@ def test_anonymized_notification_does_not_have_user(notification: Notification) 
     assert hasattr(anonymized, "updated_at")
 
 
-@pytest.mark.django_db(databases=["default", "data_ware_house"])
 def test_replicate_anonymized_notifications(notification: Notification) -> None:
     replicate_anonymized_notifications()
 
-    anonymized = AnonymizedNotification.objects.using("data_ware_house").get(id=notification.id)
+    anonymized = AnonymizedNotification.objects.get(id=notification.id)
     assert anonymized.content_title == notification.content_title
     assert anonymized.content_body == notification.content_body
     assert anonymized.created_at == notification.created_at
     assert anonymized.updated_at == notification.updated_at
 
 
-@pytest.mark.django_db(databases=["default", "data_ware_house"])
 def test_replicate_anonymized_notifications_processes_by_batch(notification: Notification) -> None:
     Notification.objects.create(
         user_id=notification.user_id,
@@ -70,10 +66,9 @@ def test_replicate_anonymized_notifications_processes_by_batch(notification: Not
     )
     replicate_anonymized_notifications(chunk_size=1)
 
-    assert AnonymizedNotification.objects.using("data_ware_house").count() == 2
+    assert AnonymizedNotification.objects.count() == 2
 
 
-@pytest.mark.django_db
 def test_anonymized_registration_does_not_have_user_nor_subscription(
     webpush_registration: Registration,
 ) -> None:
@@ -84,18 +79,14 @@ def test_anonymized_registration_does_not_have_user_nor_subscription(
     assert hasattr(anonymized, "updated_at")
 
 
-@pytest.mark.django_db(databases=["default", "data_ware_house"])
 def test_replicate_anonymized_registrations(webpush_registration: Registration) -> None:
     replicate_anonymized_registrations()
 
-    anonymized = AnonymizedRegistration.objects.using("data_ware_house").get(
-        id=webpush_registration.id
-    )
+    anonymized = AnonymizedRegistration.objects.get(id=webpush_registration.id)
     assert anonymized.created_at == webpush_registration.created_at
     assert anonymized.updated_at == webpush_registration.updated_at
 
 
-@pytest.mark.django_db(databases=["default", "data_ware_house"])
 def test_replicate_anonymized_registrations_processes_by_batch(
     user: User,
     webpush_registration: Registration,
@@ -104,4 +95,4 @@ def test_replicate_anonymized_registrations_processes_by_batch(
     Registration.objects.create(user=user, subscription=mobileAppSubscription)
     replicate_anonymized_registrations(chunk_size=1)
 
-    assert AnonymizedRegistration.objects.using("data_ware_house").count() == 2
+    assert AnonymizedRegistration.objects.count() == 2
