cbbeb0fb7110e6ff1ee47296e84fa67bb3552472
Lauréline Guérin <zebuline@crocobox.org>
2026-05-27 17:25:34 +0200

    front: select zone by city selection (#789)

---
+++ public/mobile-app/src/lib/components/modal/NavWithBackButton.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  interface Props {
+    onClick: () => void;
+    title?: string;
+  }
+  let { onClick, title }: Props = $props();
+</script>
+
+<nav>
+  <div class="back-button-wrapper">
+    <div class="back-button">
+      <button
+        onclick={onClick}
+        title="Retour à la page précédente"
+        aria-label="Retour à la page précédente"
+        data-testid="back-button"
+      >
+        <span aria-hidden="true" class="fr-icon-arrow-left-line"></span>
+      </button>
+    </div>
+  </div>
+  {#if title}
+    <div class="title">
+      <h2>{title}</h2>
+    </div>
+  {/if}
+</nav>
+
+<style>
+  nav {
+    background-color: white;
+    padding: 0;
+    margin-bottom: 0.5rem;
+    position: sticky;
+    top: 0;
+    z-index: 1000;
+    .back-button-wrapper {
+      position: absolute;
+      .back-button {
+        margin-bottom: 0.5rem;
+        color: var(--text-active-blue-france);
+        button {
+          padding: 0;
+        }
+      }
+    }
+    .title {
+      padding-left: 2rem;
+      padding-top: 0rem;
+      h2 {
+        line-height: 2rem;
+        margin-bottom: 0;
+        font-size: 18px;
+      }
+    }
+  }
+</style>
--- public/mobile-app/src/lib/components/modal/ZonePreferences.svelte
+++ public/mobile-app/src/lib/components/modal/ZonePreferences.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
   import { mount, onMount, unmount } from 'svelte';
+  import { slide } from 'svelte/transition';
   import { goto } from '$app/navigation';
+  import { Address } from '$lib/address';
+  import {
+    callGeoAPI,
+    cityToBAN,
+    type ResponseFromGeoAPI,
+  } from '$lib/citiesFromGeoAPIAndBAN';
   import Toggle from '$lib/components/Toggle.svelte';
   import { Preferences, type ZoneInfo } from '$lib/state/preferences';
   import { userStore } from '$lib/state/User.svelte';
+  import NavWithBackButton from './NavWithBackButton.svelte';
   import ZonePreferencesFooter from './ZonePreferencesFooter.svelte';
 
   let backUrl: string = '/#/preferences';
   let zoneInfos: ZoneInfo[] = $state([]);
+  let timer: ReturnType<typeof setTimeout>;
+  let inputValue: string = $state('');
+  let filteredCities: ResponseFromGeoAPI[] = $state([]);
+  let cityApiHasError: boolean = $state(false);
+  let zonesVisible: boolean = $state(true);
 
   interface Props {
     footerTarget?: HTMLElement | null;
@@ -57,6 +70,78 @@
     };
   });
 
+  const hideZones = async () => {
+    zonesVisible = false;
+    await toggleModalElements?.(false);
+  };
+
+  const showZones = async () => {
+    zonesVisible = true;
+    await toggleModalElements?.(true);
+    mountFooter();
+  };
+
+  const cityInputHandler = (event: Event) => {
+    if (!event.target) {
+      return;
+    }
+    cityApiHasError = false;
+    const { value } = event.target as HTMLInputElement;
+    debounce(value);
+  };
+
+  const debounce = (value: string) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      inputValue = value;
+      filterCities();
+    }, 750);
+  };
+
+  const filterCities = async () => {
+    filteredCities = [];
+    if (inputValue) {
+      try {
+        const response = await callGeoAPI(inputValue);
+        cityApiHasError = response.errorCode === 'geo-api-unavailable';
+        if (cityApiHasError) {
+          return;
+        }
+        if (!response.results) {
+          return;
+        }
+        filteredCities = response.results;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const setInputVal = async (city: ResponseFromGeoAPI) => {
+    filteredCities = [];
+    inputValue = '';
+    try {
+      const response = await cityToBAN(city);
+      cityApiHasError = response.errorCode === 'ban-unavailable';
+      if (cityApiHasError) {
+        return;
+      }
+      if (!response.address) {
+        return;
+      }
+      if (!userStore.connected) {
+        return;
+      }
+      const preferences = userStore.connected.identity.preferences;
+      preferences.addAddress(response.address);
+      userStore.connected.setPreferences(preferences);
+      zoneInfos = userStore.connected.getZoneInfosFromPreferences();
+      showZones();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const saveSettings = async (id: string, checked: boolean) => {
     if (!userStore.connected) {
       return;
@@ -68,17 +153,147 @@
       preferences.removeZone(id);
     }
     userStore.connected.setPreferences(preferences);
+    zoneInfos = userStore.connected.getZoneInfosFromPreferences();
   };
 </script>
 
-<div class="preferences-content-container">
-  {#each zoneInfos as zoneInfo}
-    <Toggle
-      id={zoneInfo.zone}
-      label={zoneInfo.zone}
-      isChecked={zoneInfo.selected}
-      onChangeAction={saveSettings}
-      tags={zoneInfo.tags}
-    />
-  {/each}
+{#if !zonesVisible}
+  <NavWithBackButton title="Commune" onClick={showZones} />
+{/if}
+
+<div class="preferences-city-search-container">
+  {#if zonesVisible}
+    <p transition:slide>
+      Quelles zones scolaires et communes associées voulez-vous voir affichées dans
+      votre agenda&nbsp;?
+    </p>
+  {/if}
+  <form class="city-form">
+    <fieldset class="fr-fieldset">
+      <div class="fr-fieldset__element">
+        <div class="fr-input-group autocomplete">
+          <div class="fr-input-wrap fr-icon-search-line">
+            <input
+              class="fr-input"
+              id="city-input"
+              type="text"
+              bind:value={inputValue}
+              data-testid="city-input"
+              oninput={cityInputHandler}
+              onfocus={hideZones}
+            >
+          </div>
+          {#if !zonesVisible}
+            <div transition:slide>
+              {#if !inputValue}
+                <div class="city-input-empty">
+                  <div class="no-agenda--icon">
+                    <img
+                      class="city-input-empty--icon"
+                      src="/icons/city-search.svg"
+                      alt="Icône de recherche"
+                    >
+                  </div>
+                  <div class="city-input-empty--title">
+                    Recherchez une commune pour afficher une zone
+                  </div>
+                </div>
+              {/if}
+              {#if filteredCities.length > 0}
+                <ul id="autocomplete-items-list">
+                  {#each filteredCities as city, index}
+                    <li
+                      class="autocomplete-item"
+                      data-testid="autocomplete-item-{index}"
+                    >
+                      <button
+                        type="button"
+                        onclick={() => setInputVal(city)}
+                        data-testid="autocomplete-item-button-{index}"
+                      >
+                        <p>{city.nom} ({city.departement.code})</p>
+                      </button>
+                    </li>
+                  {/each}
+                </ul>
+              {/if}
+              {#if cityApiHasError}
+                <div class="fr-alert fr-alert--warning" data-testid="city-warning">
+                  <h3 class="fr-alert__title">
+                    Récupération de la commune indisponible
+                  </h3>
+                  <p>
+                    Nous rencontrons des difficultés à trouver votre commune dans notre
+                    répertoire. Merci de réessayer plus tard.
+                  </p>
+                </div>
+              {/if}
+            </div>
+          {/if}
+        </div>
+      </div>
+    </fieldset>
+  </form>
 </div>
+
+{#if zonesVisible}
+  <div class="preferences-content-container" transition:slide>
+    {#each zoneInfos as zoneInfo}
+      <Toggle
+        id={zoneInfo.zone}
+        label={zoneInfo.zone}
+        isChecked={zoneInfo.selected}
+        onChangeAction={saveSettings}
+        tags={zoneInfo.tags}
+      />
+    {/each}
+  </div>
+{/if}
+
+<style>
+  .preferences-city-search-container {
+    p {
+      margin-bottom: 0.5rem;
+    }
+    .city-input-empty {
+      padding: 1.5rem;
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      font-size: 16px;
+      line-height: 24px;
+      color: var(--grey-0-1000);
+      img {
+        height: 5rem;
+        width: 5rem;
+      }
+    }
+    ul#autocomplete-items-list {
+      position: relative;
+      margin: 0;
+      padding: 0;
+      top: 0;
+      border: 1px solid var(--grey-950-100);
+      background-color: var(--grey-975-75);
+      li.autocomplete-item {
+        list-style: none;
+        padding: 0;
+        background-color: var(--background-default-grey);
+        button {
+          padding: 0.75rem;
+          width: 100%;
+          text-align: left;
+          --hover-tint: var(--text-action-high-blue-france);
+          --active-tint: var(--text-action-high-blue-france);
+          p {
+            margin: 0;
+          }
+        }
+      }
+      li.autocomplete-item:hover {
+        background-color: var(--text-action-high-blue-france);
+        color: var(--text-inverted-blue-france);
+      }
+    }
+  }
+</style>
