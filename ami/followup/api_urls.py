800e210092f248d01f8a9b48ef8dfc0e8c72c2aa
Lauréline Guérin <zebuline@crocobox.org>
2026-06-05 16:31:54 +0200

    followup: new endpoint to archive an item (#776)

--- ami/followup/api_urls.py
+++ ami/followup/api_urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .api_views import get_follow_up_inventories
+from .api_views import archive_followup_item, get_follow_up_inventories
 
 root_urlpatterns = [
     path("users/follow-up/inventories", get_follow_up_inventories),
+    path(
+        "users/follow-up/item/<str:inventory>/<str:item_external_id>/archive", archive_followup_item
+    ),
 ]
