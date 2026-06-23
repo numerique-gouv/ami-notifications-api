48b58c78e731f2b45281f626fdf7ee847cd1a74b
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-06-23 11:32:11 +0200

    ami-fi: implement get and post methods for authorize view (#992)

--- ami/fi/tests/test_authorize.py
+++ ami/fi/tests/test_authorize.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 from typing import Any
 
 import pytest
@@ -9,7 +10,280 @@ from ami.tests.utils import url_contains_param
 
 
 @pytest.mark.django_db
-def test_authorize(
+def test_authorize_get(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+    userinfo: dict[str, Any],
+) -> None:
+    settings.PUBLIC_FC_PROXY_BASE_URL = ""
+
+    app.set_cookie(settings.USERINFO_COOKIE_JWT_NAME, "fake userinfo jwt token")
+
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    response = app.get("/api/v1/fi/authorize/", params=authorize_data)
+
+    fi_session = FISession.objects.get()
+    assert fi_session.user_data == {}
+    assert fi_session.state == "fake-state"
+    assert fi_session.nonce == "fake-nonce"
+    assert fi_session.code == ""
+    assert fi_session.access_token == ""
+
+    assert response.form["fi_session_id"].value == str(fi_session.id)
+    assert response.form["encoded_user_data"].value == "fake userinfo jwt token"
+
+
+def test_authorize_get_invalid_data_state(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        # without claims
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_data_nonce(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_data_response_type(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "invalid-response-type",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_data_client_id(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": "invalid-client-id",
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_data_redirect_uri(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": "invalid-redirect-uri",
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_data_scope(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_acr_values(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "invalid-acr-values",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_data_claims(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": "wrong value",
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_invalid_data_prompt(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
+
+
+def test_authorize_get_missing_cookie(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+    userinfo: dict[str, Any],
+) -> None:
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    app.get("/api/v1/fi/authorize/", params=authorize_data, status=403)
+
+
+@pytest.mark.django_db
+def test_authorize_post(
     settings,
     app,
     monkeypatch: pytest.MonkeyPatch,
@@ -44,6 +318,8 @@ def test_authorize(
     }
 
     response = app.get("/api/v1/fi/authorize/", params=authorize_data)
+
+    response = response.form.submit()
     assert response.status_code == 302
     fi_session = FISession.objects.get()
     assert fi_session.user_data == userinfo
@@ -66,7 +342,7 @@ def test_authorize(
 
 
 @pytest.mark.django_db
-def test_authorize_with_proxy(
+def test_authorize_post_with_proxy(
     settings,
     app,
     monkeypatch: pytest.MonkeyPatch,
@@ -101,6 +377,8 @@ def test_authorize_with_proxy(
     }
 
     response = app.get("/api/v1/fi/authorize/", params=authorize_data)
+
+    response = response.form.submit()
     assert response.status_code == 302
     fi_session = FISession.objects.get()
     assert fi_session.user_data == userinfo
@@ -120,220 +398,15 @@ def test_authorize_with_proxy(
     )
 
 
-def test_authorize_invalid_data_state(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "",
-        "nonce": "fake-nonce",
-        "response_type": "code",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "fake-scope",
-        "acr_values": "eidas1",
-        # without claims
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_data_nonce(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "",
-        "response_type": "code",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "fake-scope",
-        "acr_values": "eidas1",
-        "claims": json.dumps(
-            {
-                "id_token": "fake-id-token",
-            }
-        ),
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_data_response_type(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "fake-nonce",
-        "response_type": "invalid-response-type",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "fake-scope",
-        "acr_values": "eidas1",
-        "claims": json.dumps(
-            {
-                "id_token": "fake-id-token",
-            }
-        ),
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_data_client_id(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "fake-nonce",
-        "response_type": "code",
-        "client_id": "invalid-client-id",
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "fake-scope",
-        "acr_values": "eidas1",
-        "claims": json.dumps(
-            {
-                "id_token": "fake-id-token",
-            }
-        ),
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_data_redirect_uri(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "fake-nonce",
-        "response_type": "code",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": "invalid-redirect-uri",
-        "scope": "fake-scope",
-        "acr_values": "eidas1",
-        "claims": json.dumps(
-            {
-                "id_token": "fake-id-token",
-            }
-        ),
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_data_scope(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "fake-nonce",
-        "response_type": "code",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "",
-        "acr_values": "eidas1",
-        "claims": json.dumps(
-            {
-                "id_token": "fake-id-token",
-            }
-        ),
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_acr_values(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "fake-nonce",
-        "response_type": "code",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "fake-scope",
-        "acr_values": "invalid-acr-values",
-        "claims": json.dumps(
-            {
-                "id_token": "fake-id-token",
-            }
-        ),
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_data_claims(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "fake-nonce",
-        "response_type": "code",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "fake-scope",
-        "acr_values": "eidas1",
-        "claims": "wrong value",
-        "prompt": "fake-prompt",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_invalid_data_prompt(
-    settings,
-    app,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    authorize_data = {
-        "state": "fake-state",
-        "nonce": "fake-nonce",
-        "response_type": "code",
-        "client_id": settings.FI_CLIENT_ID,
-        "redirect_uri": settings.FI_REDIRECT_URI,
-        "scope": "fake-scope",
-        "acr_values": "eidas1",
-        "claims": json.dumps(
-            {
-                "id_token": "fake-id-token",
-            }
-        ),
-        "prompt": "",
-    }
-
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=400)
-
-
-def test_authorize_missing_cookie(
+@pytest.mark.django_db
+def test_authorize_post_missing_fi_session_id(
     settings,
     app,
     monkeypatch: pytest.MonkeyPatch,
     userinfo: dict[str, Any],
 ) -> None:
+    app.set_cookie(settings.USERINFO_COOKIE_JWT_NAME, "fake userinfo jwt token")
+
     authorize_data = {
         "state": "fake-state",
         "nonce": "fake-nonce",
@@ -350,4 +423,100 @@ def test_authorize_missing_cookie(
         "prompt": "fake-prompt",
     }
 
-    app.get("/api/v1/fi/authorize/", params=authorize_data, status=403)
+    response = app.get("/api/v1/fi/authorize/", params=authorize_data)
+
+    response.form["fi_session_id"] = ""
+    response = response.form.submit(status=400)
+
+
+@pytest.mark.django_db
+def test_authorize_post_unknown_fi_session_id(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+    userinfo: dict[str, Any],
+) -> None:
+    app.set_cookie(settings.USERINFO_COOKIE_JWT_NAME, "fake userinfo jwt token")
+
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    response = app.get("/api/v1/fi/authorize/", params=authorize_data)
+
+    response.form["fi_session_id"] = str(uuid.uuid4())
+    response = response.form.submit(status=400)
+
+
+@pytest.mark.django_db
+def test_authorize_post_invalid_fi_session_id(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+    userinfo: dict[str, Any],
+) -> None:
+    app.set_cookie(settings.USERINFO_COOKIE_JWT_NAME, "fake userinfo jwt token")
+
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    response = app.get("/api/v1/fi/authorize/", params=authorize_data)
+
+    response.form["fi_session_id"] = "not-a-uuid"
+    response = response.form.submit(status=400)
+
+
+@pytest.mark.django_db
+def test_authorize_post_missing_fi_encoded_user_data(
+    settings,
+    app,
+    monkeypatch: pytest.MonkeyPatch,
+    userinfo: dict[str, Any],
+) -> None:
+    app.set_cookie(settings.USERINFO_COOKIE_JWT_NAME, "fake userinfo jwt token")
+
+    authorize_data = {
+        "state": "fake-state",
+        "nonce": "fake-nonce",
+        "response_type": "code",
+        "client_id": settings.FI_CLIENT_ID,
+        "redirect_uri": settings.FI_REDIRECT_URI,
+        "scope": "fake-scope",
+        "acr_values": "eidas1",
+        "claims": json.dumps(
+            {
+                "id_token": "fake-id-token",
+            }
+        ),
+        "prompt": "fake-prompt",
+    }
+
+    response = app.get("/api/v1/fi/authorize/", params=authorize_data)
+
+    response.form["encoded_user_data"] = ""
+    response = response.form.submit(status=400)
