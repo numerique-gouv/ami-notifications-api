8af53f19b7b0deb5f6ef0ac022a351090bf0b916
Lauréline Provost <zebuline@crocobox.org>
2026-06-24 09:38:43 +0200

    agent-admin: don't display success message if api call has errors (#940)

--- ami/replication/migrations/0009_apiv2_fields.py
+++ ami/replication/migrations/0009_apiv2_fields.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("replication", "0007_anonymizedregistration_subscription"),
+        ("replication", "0008_user"),
     ]
 
     operations = [
