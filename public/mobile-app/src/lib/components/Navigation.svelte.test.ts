86f994e30915e7bbe075a91dc072114b95298fb6
Lauréline Provost <zebuline@crocobox.org>
2026-07-02 11:50:00 +0200

    front: add "Services" entry in menu (#943)

--- public/mobile-app/src/lib/components/Navigation.svelte.test.ts
+++ public/mobile-app/src/lib/components/Navigation.svelte.test.ts
@@ -1,6 +1,7 @@
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { render } from '@testing-library/svelte';
+import * as envModule from '$env/static/public';
 import Navigation from './Navigation.svelte';
 
 describe('/Navigation.svelte', () => {
@@ -18,23 +19,27 @@ describe('/Navigation.svelte', () => {
 
   test('should highlight nothing', async () => {
     // Given
+    vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SERVICES_ENABLED = 'true';
     const { container } = render(Navigation);
 
     // Then
     expect(container).toHaveTextContent('Accueil');
     expect(container).toHaveTextContent('Agenda');
+    expect(container).toHaveTextContent('Services');
     expect(container).toHaveTextContent('Suivi');
     const highlight = container.querySelector('.highlight');
     expect(highlight).toBeNull();
   });
 
-  test('should highlight nothing - no followup', async () => {
+  test('should highlight nothing - no services', async () => {
     // Given
+    vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SERVICES_ENABLED = 'false';
     const { container } = render(Navigation);
 
     // Then
     expect(container).toHaveTextContent('Accueil');
     expect(container).toHaveTextContent('Agenda');
+    expect(container).not.toHaveTextContent('Services');
     expect(container).toHaveTextContent('Suivi');
     const highlight = container.querySelector('.highlight');
     expect(highlight).toBeNull();
@@ -42,6 +47,7 @@ describe('/Navigation.svelte', () => {
 
   test('should highlight home item', async () => {
     // Given
+    vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SERVICES_ENABLED = 'true';
     const { container } = render(Navigation, { currentItem: 'home' });
 
     // Then
@@ -51,6 +57,7 @@ describe('/Navigation.svelte', () => {
 
   test('should highlight agenda item', async () => {
     // Given
+    vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SERVICES_ENABLED = 'true';
     const { container } = render(Navigation, { currentItem: 'agenda' });
 
     // Then
@@ -58,8 +65,19 @@ describe('/Navigation.svelte', () => {
     expect(highlight).toHaveTextContent('Agenda');
   });
 
+  test('should highlight services item', async () => {
+    // Given
+    vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SERVICES_ENABLED = 'true';
+    const { container } = render(Navigation, { currentItem: 'services' });
+
+    // Then
+    const highlight = container.querySelector('.highlight');
+    expect(highlight).toHaveTextContent('Services');
+  });
+
   test('should highlight followup item', async () => {
     // Given
+    vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SERVICES_ENABLED = 'true';
     const { container } = render(Navigation, { currentItem: 'followup' });
 
     // Then
