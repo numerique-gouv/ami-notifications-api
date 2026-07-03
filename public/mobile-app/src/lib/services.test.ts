09440657fe89f1483555f690ad1400f816a18ab6
Lauréline Provost <zebuline@crocobox.org>
2026-07-03 09:32:08 +0200

    front: Services class populated with services retrieved from back (#943)

---
+++ public/mobile-app/src/lib/services.test.ts
@@ -0,0 +1,213 @@
+import { describe, expect, test, vi } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import * as apiServicesMethods from '$lib/api-services';
+import { buildServices, Services, ServicesItem } from '$lib/services';
+
+describe('/services.ts', () => {
+  describe('ServicesItem', () => {
+    describe('id', () => {
+      test('should return an id from partner_id and item_type', async () => {
+        // Given
+        const item = new ServicesItem(
+          'partner',
+          'type',
+          'title',
+          'short description',
+          'description',
+          'external-url',
+          false
+        );
+
+        // When
+        const id = item.id;
+
+        // Then
+        expect(id).equal('partner:type');
+      });
+    });
+    describe('formatDescription', () => {
+      test('should replace {date} by parameter', async () => {
+        // Given
+        const item1 = new ServicesItem(
+          'partner',
+          'type',
+          'title',
+          'short description',
+          'description',
+          'external-url',
+          false
+        );
+        const item2 = new ServicesItem(
+          'partner',
+          'type',
+          'title',
+          'short description',
+          'description {date}',
+          'external-url',
+          false
+        );
+
+        // When
+        const description1 = item1.formatDescription('date1');
+        const description2 = item2.formatDescription('date2');
+
+        // Then
+        expect(description1).equal('description');
+        expect(description2).equal('description date2');
+      });
+    });
+  });
+  describe('Services', () => {
+    test('should order items', async () => {
+      // Given
+      const servicesItem1 = {
+        partner_id: 'psl',
+        item_type: 'OperationTranquilliteVacances',
+        title: 'Opération Tranquillité Vacances',
+        short_description: 'Sécurisez votre logement',
+        description: 'Vous partez en vacances ? **Securisez votre logement.**',
+        url: 'http://external-url',
+        with_silent_login: true,
+      };
+      const servicesItem2 = {
+        partner_id: 'dinum-dn',
+        item_type: 'JeDéménage',
+        title: 'Je déménage',
+        short_description: 'Indiquez votre nouvelle adresse',
+        description: 'Vous déménagez ? **Indiquez votre nouvelle adresse.**',
+        url: 'http://external-url',
+        with_silent_login: false,
+      };
+
+      // When
+      const services = new Services({
+        internal: [servicesItem1, servicesItem2],
+      });
+
+      // Then
+      expect(services.items.length).equal(2);
+      expect(
+        services.items[0].equals(
+          new ServicesItem(
+            'dinum-dn',
+            'JeDéménage',
+            'Je déménage',
+            'Indiquez votre nouvelle adresse',
+            'Vous déménagez ? **Indiquez votre nouvelle adresse.**',
+            'http://external-url',
+            false
+          )
+        )
+      ).toBe(true);
+      expect(
+        services.items[1].equals(
+          new ServicesItem(
+            'psl',
+            'OperationTranquilliteVacances',
+            'Opération Tranquillité Vacances',
+            'Sécurisez votre logement',
+            'Vous partez en vacances ? **Securisez votre logement.**',
+            'http://external-url',
+            true
+          )
+        )
+      ).toBe(true);
+    });
+    describe('find', () => {
+      test('should return only one matching service', async () => {
+        // Given
+        const servicesItem1 = {
+          partner_id: 'psl',
+          item_type: 'OperationTranquilliteVacances',
+          title: 'Opération Tranquillité Vacances',
+          short_description: 'Sécurisez votre logement',
+          description: 'Vous partez en vacances ? **Securisez votre logement.**',
+          url: 'http://external-url',
+          with_silent_login: true,
+        };
+        const servicesItem2 = {
+          partner_id: 'dinum-dn',
+          item_type: 'JeDéménage',
+          title: 'Je déménage',
+          short_description: 'Indiquez votre nouvelle adresse',
+          description: 'Vous déménagez ? **Indiquez votre nouvelle adresse.**',
+          url: 'http://external-url',
+          with_silent_login: false,
+        };
+        const servicesItem3 = {
+          partner_id: 'dinum-dn',
+          item_type: 'JeDéménage',
+          title: 'Je déménage',
+          short_description: 'Indiquez votre nouvelle adresse',
+          description: 'Vous déménagez ? **Indiquez votre nouvelle adresse.**',
+          url: 'http://external-url',
+          with_silent_login: false,
+        };
+        const services = new Services({
+          internal: [servicesItem1, servicesItem2, servicesItem3],
+        });
+
+        // When
+        const find1 = services.find('psl', 'OperationTranquilliteVacances');
+        const find2 = services.find('dinum-dn', 'JeDéménage');
+        const find3 = services.find('unknown', 'unknown');
+
+        // Then
+        expect(find1).not.toBeNull();
+        expect(
+          find1?.equals(
+            new ServicesItem(
+              'psl',
+              'OperationTranquilliteVacances',
+              'Opération Tranquillité Vacances',
+              'Sécurisez votre logement',
+              'Vous partez en vacances ? **Securisez votre logement.**',
+              'http://external-url',
+              true
+            )
+          )
+        ).toBe(true);
+        expect(find2).toBeNull();
+        expect(find3).toBeNull();
+      });
+    });
+  });
+  describe('buildServices', () => {
+    test('should retrieve sources and init services with them', async () => {
+      // Given
+      const servicesItem = {
+        partner_id: 'psl',
+        item_type: 'OperationTranquilliteVacances',
+        title: 'Opération Tranquillité Vacances',
+        short_description: 'Sécurisez votre logement',
+        description: 'Vous partez en vacances ? **Securisez votre logement.**',
+        url: 'http://external-url',
+        with_silent_login: true,
+      };
+      const spy = vi.spyOn(apiServicesMethods, 'retrieveServices').mockResolvedValue({
+        internal: [servicesItem],
+      });
+
+      // When
+      const services = await buildServices();
+
+      // Then
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(services).toBeInstanceOf(Services);
+      expect(services.items.length).equal(1);
+      expect(
+        services.items[0].equals(
+          new ServicesItem(
+            'psl',
+            'OperationTranquilliteVacances',
+            'Opération Tranquillité Vacances',
+            'Sécurisez votre logement',
+            'Vous partez en vacances ? **Securisez votre logement.**',
+            'http://external-url',
+            true
+          )
+        )
+      ).toBe(true);
+    });
+  });
+});
---
+++ public/mobile-app/src/lib/services.ts
@@ -0,0 +1,125 @@
+import type { APIServices, APIServicesItem } from '$lib/api-services';
+import { retrieveServices } from '$lib/api-services';
+
+export class ServicesItem {
+  constructor(
+    private _partner_id: string,
+    private _item_type: string,
+
+    private _title: string,
+    private _short_description: string,
+    private _description: string,
+    private _link: string,
+
+    private _with_silent_login: boolean
+  ) {}
+
+  equals(other: ServicesItem): boolean {
+    if (!(other instanceof ServicesItem)) {
+      return false;
+    }
+    return JSON.stringify(this) === JSON.stringify(other);
+  }
+
+  get id(): string {
+    return `${this.partner_id}:${this.item_type}`;
+  }
+
+  get partner_id(): string {
+    return this._partner_id;
+  }
+
+  get item_type(): string {
+    return this._item_type;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get short_description(): string {
+    return this._short_description;
+  }
+
+  get link(): string | null {
+    return this._link;
+  }
+
+  get with_silent_login(): boolean {
+    return this._with_silent_login;
+  }
+
+  get url(): string {
+    return `/#/services/service/${this.partner_id}/${this.item_type}`;
+  }
+
+  formatDescription(date: string): string {
+    return this.description.replace('{date}', date);
+  }
+}
+
+export class Services {
+  private _items: ServicesItem[] = [];
+
+  constructor(apiServices: APIServices | null = null) {
+    const servicesItems: ServicesItem[] = [];
+
+    const items: APIServicesItem[] = apiServices?.internal || [];
+
+    // build items
+    items.forEach((item) => {
+      const servicesItem = this.createServicesItem(item);
+      servicesItems.push(servicesItem);
+    });
+
+    // sort items by title
+    servicesItems.sort((a, b) => a.title.localeCompare(b.title, 'fr'));
+
+    // organize items in _items or
+    servicesItems.forEach((servicesItem) => {
+      this._items.push(servicesItem);
+    });
+  }
+
+  private createServicesItem(item: APIServicesItem): ServicesItem {
+    return new ServicesItem(
+      item.partner_id,
+      item.item_type,
+      item.title,
+      item.short_description,
+      item.description,
+      item.url,
+      item.with_silent_login
+    );
+  }
+
+  get items(): ServicesItem[] {
+    return this._items;
+  }
+
+  find(partner_id: string, item_type: string): ServicesItem | null {
+    const found = this.items.filter(
+      (item) => item.partner_id === partner_id && item.item_type === item_type
+    );
+    if (found.length === 0) {
+      console.log(`Can not find ServicesItem ${partner_id}, ${item_type}: no result`);
+      return null;
+    }
+    if (found.length > 1) {
+      console.log(
+        `Can not find ServicesItem ${partner_id}, ${item_type}: many results`
+      );
+      return null;
+    }
+    return found[0];
+  }
+}
+
+export const buildServices = async (): Promise<Services> => {
+  const apiServices: APIServices = await retrieveServices();
+  return new Services(apiServices);
+};
