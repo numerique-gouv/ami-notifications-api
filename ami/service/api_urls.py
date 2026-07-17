8142f6941875feab80c2db705f71bdd717804509
Lauréline Provost <zebuline@crocobox.org>
2026-07-17 10:59:48 +0200

    api: endpoint to get service parameters (#943)

--- ami/service/api_urls.py
+++ ami/service/api_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .api_views import get_services
+from .api_views import get_services, get_services_item_parameters
 
 root_urlpatterns = [
     path("users/data/services", get_services),
+    path("users/data/services/item/parameters", get_services_item_parameters),
 ]
--- ami/service/serializers.py
+++ ami/service/serializers.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+from rest_framework import serializers
 from rest_framework_dataclasses.serializers import DataclassSerializer
+
+from ami.utils import generate_identity_token
 
 from .schemas import Services
 
@@ -6,3 +10,57 @@ from .schemas import Services
 class ServicesSerializer(DataclassSerializer):
     class Meta:
         dataclass = Services
+
+
+class OTVJWTTokenSerializer(serializers.Serializer):
+    preferred_username = serializers.CharField(allow_blank=True, required=False)
+    email = serializers.CharField(allow_blank=True, required=False)
+    address_city = serializers.CharField(allow_blank=True, required=False)
+    address_postcode = serializers.CharField(allow_blank=True, required=False)
+    address_name = serializers.CharField(allow_blank=True, required=False)
+
+    @classmethod
+    def get_parameter_value(cls, *, validated_data, user):
+        otv_private_key = settings.PARTNERS_PSL_OTV_JWT_CERT_PFX_B64
+        psl_otv_public_key = settings.PARTNERS_PSL_OTV_JWE_PUBLIC_KEY
+        if otv_private_key and psl_otv_public_key:
+            return generate_identity_token(
+                validated_data["preferred_username"],
+                validated_data["email"],
+                validated_data["address_city"],
+                validated_data["address_postcode"],
+                validated_data["address_name"],
+                user.fc_hash,
+            )
+        return ""
+
+
+PARAMETER_SERIALIZERS = {
+    "otv_jwt_token": OTVJWTTokenSerializer,
+}
+
+
+class ParameterSerializer(serializers.Serializer):
+    parameter = serializers.ChoiceField(choices=list(PARAMETER_SERIALIZERS.keys()))
+    values = serializers.DictField()
+
+    def validate(self, attrs):
+        data_serializer = PARAMETER_SERIALIZERS[attrs["parameter"]](data=attrs["values"])
+        data_serializer.is_valid(raise_exception=True)
+
+        attrs["values"] = data_serializer.validated_data
+        return attrs
+
+
+class ServicesItemsParametersSerializer(serializers.Serializer):
+    parameters = ParameterSerializer(many=True, required=False)
+
+
+def get_parameters_values(*, validated_data, user):
+    values = {}
+    for value in validated_data.get("parameters") or []:
+        data_serializer_class = PARAMETER_SERIALIZERS[value["parameter"]]
+        values[value["parameter"]] = data_serializer_class.get_parameter_value(
+            validated_data=value["values"], user=user
+        )
+    return values
