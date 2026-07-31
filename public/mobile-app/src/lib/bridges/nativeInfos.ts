c76e00bb2b7e78def56830e78d774665f6284780
Lauréline Provost <zebuline@crocobox.org>
2026-07-31 16:08:35 +0200

    front: get promoted url aliases from mobile app (#1027)

--- public/mobile-app/src/lib/bridges/nativeInfos.ts
+++ public/mobile-app/src/lib/bridges/nativeInfos.ts
@@ -24,3 +24,8 @@ export const getVersion = () => {
   const infos = getNativeInfos();
   return infos?.version || '';
 };
+
+export const getPromotedUrlAliases = () => {
+  const infos = getNativeInfos();
+  return JSON.parse(infos?.promoted_url_aliases || '[]');
+};
