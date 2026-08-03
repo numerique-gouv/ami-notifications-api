3e1b481348c6549d03b21459af48f0939c304c0c
Frédéric Péters <fpeters@0d.be>
2026-08-03 09:46:45 +0200

    api: add rate limiting to access key endpoint (#1096)

--- ami/access_key/api_views.py
+++ ami/access_key/api_views.py
@@ -1,12 +1,15 @@
+import asyncio
+
 from django.conf import settings
 from django.http import Http404, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from django_ratelimit.core import is_ratelimited
 
 
 @require_POST
 @csrf_exempt
-def access_key(request):
+async def access_key(request):
     if not settings.WEB_APP_ACCESS_KEYS:
         raise Http404
     try:
@@ -14,4 +17,13 @@ def access_key(request):
             return JsonResponse({}, status=200)
     except KeyError:
         pass
+    if settings.WEB_APP_ACCESS_KEY_RATE_LIMIT:
+        if is_ratelimited(
+            request,
+            group="access-key",
+            key="ip",
+            rate=settings.WEB_APP_ACCESS_KEY_RATE_LIMIT,
+            increment=True,
+        ):
+            await asyncio.sleep(float(settings.WEB_APP_ACCESS_KEY_RATE_LIMIT_DELAY or 1))
     return JsonResponse({"err": "access denied"}, status=401)
