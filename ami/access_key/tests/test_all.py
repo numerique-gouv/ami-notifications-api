23202354cc918ecab0092d9692e0efc470337797
Frédéric Péters <fpeters@0d.be>
2026-08-03 09:46:45 +0200

    api: add rate limiting to access key endpoint (#1096)

--- ami/access_key/tests/test_all.py
+++ ami/access_key/tests/test_all.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 
@@ -11,3 +13,15 @@ def test_access_key(app, settings):
     app.post("/api/v1/access-key", params={"key": "123"}, status=200)
     app.post("/api/v1/access-key", params={"key": "456"}, status=200)
     app.post("/api/v1/access-key", params={"key": "789"}, status=401)
+
+
+@pytest.mark.django_db
+def test_access_key_rate_limit(app, settings):
+    settings.WEB_APP_ACCESS_KEYS = {"123", "456"}
+    settings.WEB_APP_ACCESS_KEY_RATE_LIMIT = "1/s"
+    settings.WEB_APP_ACCESS_KEY_RATE_LIMIT_DELAY = 1
+    t0 = time.time()
+    app.post("/api/v1/access-key", params={"key": "789"}, status=401)
+    assert time.time() - t0 < 0.5
+    app.post("/api/v1/access-key", params={"key": "789"}, status=401)
+    assert time.time() - t0 > 1
