11a6dc24facde7e2cf0632b1002a064a032c5686
Frédéric Péters <fpeters@0d.be>
2026-08-04 15:03:11 +0200

    mobile: publish .well-known/apple-app-site-association file for iOS (#1113)

--- ami/urls.py
+++ ami/urls.py
@@ -4,10 +4,12 @@ from django.views.generic import TemplateView
 from ami.agent_admin import urls as agent_admin_urls
 from ami.api import urls as api_urls
 from ami.authentication import urls as authentication_urls
+from ami.well_known import urls as well_known_urls
 
 urlpatterns = [
     path("", include(authentication_urls)),
     path("", include(api_urls)),
     path("", TemplateView.as_view(template_name="index.html")),
     path("agent-admin/", include(agent_admin_urls, namespace="agent-admin")),
+    path(".well-known/", include(well_known_urls)),
 ]
