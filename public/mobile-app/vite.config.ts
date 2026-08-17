6f466c57e5f0543766a6d346a8a809ad1fe3cad7
Frédéric Péters <fpeters@0d.be>
2026-08-17 15:29:40 +0200

    front: proxify well-known urls (#1088)

--- public/mobile-app/vite.config.ts
+++ public/mobile-app/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       '/schema': { target: 'http://localhost:8000', xfwd: true },
       '/sector_identifier_url ': { target: 'http://localhost:8000', xfwd: true },
       '/silent-login-ami-fi': { target: 'http://localhost:8000', xfwd: true },
+      '/.well-known': { target: 'http://localhost:8000', xfwd: true },
     },
   },
   test: {
