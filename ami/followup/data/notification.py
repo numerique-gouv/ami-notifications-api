c17e9c7ffc1e52f36563c9f9eea73b9c97d72cc9
Lauréline Provost <zebuline@crocobox.org>
2026-08-17 14:54:50 +0200

    followup: organize items by parent and return sub_items (#825)

--- ami/followup/data/notification.py
+++ ami/followup/data/notification.py
@@ -1,6 +1,11 @@
 import collections
 
-from ami.followup.schemas import FollowupItem, FollowupSource, FollowupSourceStatus
+from ami.followup.schemas import (
+    FollowupItem,
+    FollowupSource,
+    FollowupSourceStatus,
+    NotificationsFollowup,
+)
 from ami.notification.models import Notification
 from ami.partner.models import partners
 from ami.service.models import Service
@@ -17,24 +22,34 @@ def get_notifications_data(*, current_user: User) -> list[FollowupItem]:
         partner_id__in=[p.id for p in partners.values() if p.followup_from_notifications],
     ).order_by("event_date", "created_at")
 
-    notifications_by_item_id: collections.defaultdict[str, list[Notification]] = (
-        collections.defaultdict(list)
-    )
-    for notification in notifications:
-        external_id = f"{notification.partner_id}:{notification.item_type}:{notification.item_id}"
-        notifications_by_item_id[external_id].append(notification)
-
     services_by_id: collections.defaultdict[str, Service] = collections.defaultdict()
     for service in Service.objects.all():
         external_id = f"{service.partner_id}:{service.item_type}"
         services_by_id[external_id] = service
 
+    notifications_followup = NotificationsFollowup()
+    notifications_followup.services_by_id = services_by_id
+    for notification in notifications:
+        external_id = f"{notification.partner_id}:{notification.item_type}:{notification.item_id}"
+        if (
+            notification.item_parent_partner_id
+            and notification.item_parent_type
+            and notification.item_parent_id
+        ):
+            external_parent_id = f"{notification.item_parent_partner_id}:{notification.item_parent_type}:{notification.item_parent_id}"
+            notifications_followup.add_sub_notification(
+                external_parent_id, external_id, notification
+            )
+        else:
+            notifications_followup.add_notification(external_id, notification)
+
     items: list[FollowupItem] = []
 
-    for notifications in notifications_by_item_id.values():
-        item = FollowupItem.from_notifications(notifications, services_by_id)
+    for notifications_item in notifications_followup.items.values():
+        item = notifications_item.build_followup_item()
         if item is None:
             continue
+        assert isinstance(item, FollowupItem)  # should not happen
         items.append(item)
 
     return sorted(items, key=lambda a: (a.updated_at, a.created_at), reverse=True)
