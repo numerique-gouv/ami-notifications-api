ff8d075f4504796c9db099d6a2d7d34f09f09499
Frédéric Péters <fpeters@0d.be>
2026-08-18 18:33:14 +0200

    mobile: publish .well-known/assetlinks.json file for Android (#1088)

--- ami/well_known/tests/test_all.py
+++ ami/well_known/tests/test_all.py
@@ -5,3 +5,16 @@ def test_apple_app_site_association(app, settings):
     settings.IOS_APP_IDS = ["fake.app.id"]
     response = app.get("/.well-known/apple-app-site-association")
     assert response.json["webcredentials"]["apps"] == settings.IOS_APP_IDS
+
+
+def test_assetlinks(app, settings):
+    settings.ANDROID_PACKAGE_NAME = None
+    app.get("/.well-known/assetlinks.json", status=404)
+
+    settings.ANDROID_PACKAGE_NAME = "fake.app"
+    settings.ANDROID_CERT_FINGERPRINTS = ["12:34"]
+    response = app.get("/.well-known/assetlinks.json")
+    assert response.json[0]["target"]["package_name"] == settings.ANDROID_PACKAGE_NAME
+    assert (
+        response.json[0]["target"]["sha256_cert_fingerprints"] == settings.ANDROID_CERT_FINGERPRINTS
+    )
