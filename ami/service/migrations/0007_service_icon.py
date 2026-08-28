ef43d1c6f460b9a97d601008222a937c39b88cd0
Lauréline Provost <zebuline@crocobox.org>
2026-08-28 18:08:41 +0200

    services: fix missing migration (#1205)

--- ami/service/migrations/0007_service_icon.py
+++ ami/service/migrations/0007_service_icon.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="service",
             name="icon",
-            field=models.CharField(default=""),
+            field=models.CharField(blank=True),
             preserve_default=False,
         ),
     ]
