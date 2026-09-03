ef40c96f5c8ac1032c310fc3fd00142b62e12b4b
Frédéric Péters <fpeters@0d.be>
2026-09-03 17:11:13 +0200

    front: add types and tests for sidemenu component (#1180)

---
+++ public/mobile-app/src/lib/components/SideMenu.svelte.test.ts
@@ -0,0 +1,69 @@
+import { describe, expect, test } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import { render, screen } from '@testing-library/svelte';
+import SideMenu from './SideMenu.svelte';
+
+describe('/SideMenu.svelte', () => {
+  test('should display basic items', async () => {
+    // Given
+    const entries = [
+      {
+        url: '/#/checklist',
+        title: 'Pendant la grossesse',
+        id: 'during-pregnancy',
+      },
+      {
+        url: '/#/checklist',
+        title: 'Après la naissance',
+        id: 'after-birth',
+      },
+    ];
+
+    // When
+    render(SideMenu, { sideMenus: entries });
+
+    // Then
+    expect(screen.queryByTestId('service-steps-during-pregnancy')).not.toBeNull();
+    expect(screen.queryByTestId('service-steps-after-birth')).not.toBeNull();
+  });
+
+  test('should display icon on item', async () => {
+    // Given
+    const entries = [
+      {
+        url: '/#/checklist',
+        title: 'Pendant la grossesse',
+        id: 'during-pregnancy',
+        iconClass: 'fr-icon-user-line',
+      },
+    ];
+
+    // When
+    render(SideMenu, { sideMenus: entries });
+
+    // Then
+    expect(screen.queryByTestId('service-steps-during-pregnancy')).toHaveClass(
+      'fr-icon-user-line'
+    );
+  });
+
+  test('should display tag on item', async () => {
+    // Given
+    const entries = [
+      {
+        url: '/#/checklist',
+        title: 'Pendant la grossesse',
+        id: 'during-pregnancy',
+        tag: '2/8',
+      },
+    ];
+
+    // When
+    render(SideMenu, { sideMenus: entries });
+
+    // Then
+    expect(screen.queryByTestId('service-steps-during-pregnancy')).toHaveTextContent(
+      '2/8'
+    );
+  });
+});
