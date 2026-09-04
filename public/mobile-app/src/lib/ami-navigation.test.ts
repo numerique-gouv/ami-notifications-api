c8f3d5d1f73c48d06b6b1eaa59d728f5cd1ca813
Lauréline Provost <zebuline@crocobox.org>
2026-09-04 12:03:38 +0200

    front: new AMIBack function (#1200)

--- public/mobile-app/src/lib/ami-navigation.test.ts
+++ public/mobile-app/src/lib/ami-navigation.test.ts
@@ -1,7 +1,8 @@
 import { beforeEach, describe, expect, test, vi } from 'vitest';
 import * as navigationMethods from '$app/navigation';
 import * as envModule from '$env/static/public';
-import { AMIGoto } from '$lib/ami-navigation';
+import * as AMINavigationMethods from '$lib/ami-navigation';
+import { AMIBack, AMIGoto } from '$lib/ami-navigation';
 
 describe('/ami-navigation', () => {
   beforeEach(async () => {
@@ -13,142 +14,177 @@ describe('/ami-navigation', () => {
       });
     });
     vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'true';
-    vi.resetAllMocks();
   });
 
-  describe('without silent-login', () => {
-    test('should redirect to url - internal url', async () => {
-      // Given
-      const url = '/';
-      const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
+  describe('AMIGoto', () => {
+    describe('without silent-login', () => {
+      test('should redirect to url - internal url', async () => {
+        // Given
+        const url = '/';
+        const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
 
-      // When
-      AMIGoto(url);
+        // When
+        AMIGoto(url);
 
-      // Then
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(spy).toHaveBeenCalledWith('/');
-    });
-    test('should redirect to url - external url without protocol', async () => {
-      // Given
-      const url = '//';
-      const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
-
-      // When
-      AMIGoto(url);
-
-      // Then
-      expect(spy).not.toHaveBeenCalled();
-    });
-    test('should redirect to url - external url', async () => {
-      // Given
-      const url = 'http://external-url';
-      vi.stubGlobal('location', {
-        href: 'fake-link',
-        hash: '',
-        origin: 'http://localhost',
+        // Then
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith('/');
       });
+      test('should redirect to url - external url without protocol', async () => {
+        // Given
+        const url = '//';
+        const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
 
-      // When
-      AMIGoto(url);
+        // When
+        AMIGoto(url);
 
-      // Then
-      expect(window.location.href).toBe('http://external-url/');
-    });
-    test('should redirect to url - not external url', async () => {
-      // Given
-      const url = 'javascript:alert("foobar")';
-      vi.stubGlobal('location', {
-        href: 'fake-link',
-        hash: '',
-        origin: 'http://localhost',
+        // Then
+        expect(spy).not.toHaveBeenCalled();
       });
+      test('should redirect to url - external url', async () => {
+        // Given
+        const url = 'http://external-url';
+        vi.stubGlobal('location', {
+          href: 'fake-link',
+          hash: '',
+          origin: 'http://localhost',
+        });
 
-      // When
-      AMIGoto(url);
+        // When
+        AMIGoto(url);
 
-      // Then
-      expect(window.location.href).toBe('fake-link');
+        // Then
+        expect(window.location.href).toBe('http://external-url/');
+      });
+      test('should redirect to url - not external url', async () => {
+        // Given
+        const url = 'javascript:alert("foobar")';
+        vi.stubGlobal('location', {
+          href: 'fake-link',
+          hash: '',
+          origin: 'http://localhost',
+        });
+
+        // When
+        AMIGoto(url);
+
+        // Then
+        expect(window.location.href).toBe('fake-link');
+      });
+    });
+
+    describe('with silent-login', () => {
+      test('should redirect to silent login page with url and hash in param', async () => {
+        // Given
+        const url = 'http://external-url';
+        vi.stubGlobal('location', {
+          href: 'fake-link',
+          hash: '#/page',
+          origin: 'http://localhost',
+        });
+
+        // When
+        AMIGoto(url, true);
+
+        // Then
+        expect(window.location.href).toBe(
+          '/silent-login-ami-fi?redirect_url=http%3A%2F%2Fexternal-url&from_hash=/page'
+        );
+      });
+    });
+
+    describe('with silent-login - flag disabled', () => {
+      test('should redirect to url - internal url', async () => {
+        // Given
+        vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
+        const url = '/';
+        const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
+
+        // When
+        AMIGoto(url, true);
+
+        // Then
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith('/');
+      });
+      test('should redirect to url - internal url without protocol', async () => {
+        // Given
+        vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
+        const url = '//';
+        const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
+
+        // When
+        AMIGoto(url, true);
+
+        // Then
+        expect(spy).not.toHaveBeenCalled();
+      });
+      test('should redirect to url - external url', async () => {
+        // Given
+        vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
+        const url = 'http://external-url';
+        vi.stubGlobal('location', {
+          href: 'fake-link',
+          hash: '',
+          origin: 'http://localhost',
+        });
+
+        // When
+        AMIGoto(url, true);
+
+        // Then
+        expect(window.location.href).toBe('http://external-url/');
+      });
+      test('should redirect to url - not external url', async () => {
+        // Given
+        vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
+        const url = 'javascript:alert("foobar")';
+        vi.stubGlobal('location', {
+          href: 'fake-link',
+          hash: '',
+          origin: 'http://localhost',
+        });
+
+        // When
+        AMIGoto(url, true);
+
+        // Then
+        expect(window.location.href).toBe('fake-link');
+      });
     });
   });
 
-  describe('with silent-login', () => {
-    test('should redirect to silent login page with url and hash in param', async () => {
+  describe('AMIBack', () => {
+    test('Should call history.back if history', () => {
       // Given
-      const url = 'http://external-url';
-      vi.stubGlobal('location', {
-        href: 'fake-link',
-        hash: '#/page',
-        origin: 'http://localhost',
-      });
+      const spyBack = vi.spyOn(window.history, 'back').mockImplementation(() => {});
+      const spyAMIGoto = vi
+        .spyOn(AMINavigationMethods, 'AMIGoto')
+        .mockImplementation(() => {});
+      Object.defineProperty(window.history, 'length', { value: 2, configurable: true });
 
       // When
-      AMIGoto(url, true);
+      AMIBack('/#/page');
 
       // Then
-      expect(window.location.href).toBe(
-        '/silent-login-ami-fi?redirect_url=http%3A%2F%2Fexternal-url&from_hash=/page'
-      );
+      expect(spyBack).toHaveBeenCalled();
+      expect(spyAMIGoto).not.toHaveBeenCalled();
     });
-  });
 
-  describe('with silent-login - flag disabled', () => {
-    test('should redirect to url - internal url', async () => {
+    test('Should call AMIGoto if no history', () => {
       // Given
-      vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
-      const url = '/';
-      const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
+      const spyBack = vi.spyOn(window.history, 'back').mockImplementation(() => {});
+      const spyAMIGoto = vi
+        .spyOn(AMINavigationMethods, 'AMIGoto')
+        .mockImplementation(() => {});
+      Object.defineProperty(window.history, 'length', { value: 1, configurable: true });
 
       // When
-      AMIGoto(url, true);
+      AMIBack('/#/page');
 
       // Then
-      expect(spy).toHaveBeenCalledTimes(1);
-      expect(spy).toHaveBeenCalledWith('/');
-    });
-    test('should redirect to url - internal url without protocol', async () => {
-      // Given
-      vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
-      const url = '//';
-      const spy = vi.spyOn(navigationMethods, 'goto').mockResolvedValue();
-
-      // When
-      AMIGoto(url, true);
-
-      // Then
-      expect(spy).not.toHaveBeenCalled();
-    });
-    test('should redirect to url - external url', async () => {
-      // Given
-      vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
-      const url = 'http://external-url';
-      vi.stubGlobal('location', {
-        href: 'fake-link',
-        hash: '',
-        origin: 'http://localhost',
-      });
-
-      // When
-      AMIGoto(url, true);
-
-      // Then
-      expect(window.location.href).toBe('http://external-url/');
-    });
-    test('should redirect to url - not external url', async () => {
-      // Given
-      vi.mocked(envModule).PUBLIC_FEATURE_FLAG_SILENT_FC_ENABLED = 'false';
-      const url = 'javascript:alert("foobar")';
-      vi.stubGlobal('location', {
-        href: 'fake-link',
-        hash: '',
-        origin: 'http://localhost',
-      });
-
-      // When
-      AMIGoto(url, true);
-
-      // Then
-      expect(window.location.href).toBe('fake-link');
+      expect(spyBack).not.toHaveBeenCalled();
+      expect(spyAMIGoto).toHaveBeenCalledWith('/#/page');
     });
   });
 });
