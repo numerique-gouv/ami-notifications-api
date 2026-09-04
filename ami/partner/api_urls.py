38d3d4ff9a962748f64354372df8b7485f23e21b
Clotilde DESQUILBET <clotilde.desquilbet@dila.gouv.fr>
2026-09-04 16:10:30 +0200

    partner: add endpoint to get partners (#1250)

--- ami/partner/api_urls.py
+++ ami/partner/api_urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .api_views import (
     generate_partner_url,
     get_partner_public_key,
+    get_partners,
 )
 
 urlpatterns = [
     path("partner/otv/url", generate_partner_url),
     path("partner/otv/public_key", get_partner_public_key),
+    path("users/data/partners", get_partners),
 ]
