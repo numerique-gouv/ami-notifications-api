737946572e51c15f481c2a1c1c89dacf37ee9b0e
Lauréline Provost <zebuline@crocobox.org>
2026-09-07 10:38:18 +0200

    front: new page help center (#1080)

--- public/mobile-app/src/lib/components/Icon.svelte
+++ public/mobile-app/src/lib/components/Icon.svelte
@@ -5,13 +5,11 @@
   let { className }: Props = $props();
 </script>
 
-<span class="icon {className}"></span>
+<span aria-hidden="true" class="icon {className}"></span>
 
 <style>
   .icon {
     display: inline-block;
-    width: 1.25rem;
-    height: 1.25rem;
     flex-shrink: 0;
 
     &:before {
