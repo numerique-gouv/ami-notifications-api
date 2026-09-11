d242bd5da0b640e9e2d5114eb3e61711e4a49420
Lauréline Provost <zebuline@crocobox.org>
2026-09-11 22:10:06 +0200

    front: add custom icons for notifications (#1302)

--- public/mobile-app/src/lib/dsfr-icon.ts
+++ public/mobile-app/src/lib/dsfr-icon.ts
@@ -1,7 +1,12 @@
 import dsfrIconList from '@gouvfr/dsfr/.config/icon.json';
 
 export const getDSFRIcon = (icon: string, defaultIcon: string): string => {
-  const customIcons = ['infinity-line'];
+  const customIcons = [
+    'infinity-line',
+    'folder-check-line',
+    'folder-open-line',
+    'indeterminate-circle-line',
+  ];
   const iconList = [...dsfrIconList.map((i) => i.name), ...customIcons];
 
   return iconList.includes(icon?.replace('fr-icon-', '')) ? icon : defaultIcon;
