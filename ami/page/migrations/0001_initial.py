94c8a92fbc26a39a7f671c64d786d9a3d0362137
Frédéric Péters <fpeters@0d.be>
2026-09-14 17:19:44 +0200

    agent_admin: add support for managing pages with sections (#1067)

---
+++ ami/page/migrations/0001_initial.py
@@ -0,0 +1,58 @@
+import uuid
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Page",
+            fields=[
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("slug", models.SlugField(unique=True)),
+                ("title", models.CharField()),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name="Section",
+            fields=[
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                    ),
+                ),
+                ("slug", models.SlugField(unique=True)),
+                ("title", models.CharField()),
+                ("order", models.PositiveSmallIntegerField()),
+                ("text", models.TextField(blank=True)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "page",
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="page.page"),
+                ),
+            ],
+            options={
+                "ordering": ["order", "title"],
+            },
+        ),
+    ]
