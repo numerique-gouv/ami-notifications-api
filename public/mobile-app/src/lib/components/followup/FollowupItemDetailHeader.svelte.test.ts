7ef61048d7dfa378fe981a36adfc744a1adfa3f2
Lauréline Provost <zebuline@crocobox.org>
2026-09-15 18:22:02 +0200

    front: followup, adapt item detail when it has milestone (#268)

--- public/mobile-app/src/lib/components/followup/FollowupItemDetailHeader.svelte.test.ts
+++ public/mobile-app/src/lib/components/followup/FollowupItemDetailHeader.svelte.test.ts
@@ -131,6 +131,161 @@ describe('/FollowupItemDetailHeader.svelte', () => {
       expect(screen.queryByTestId('item-reference')).toBeNull();
     });
   });
+  test('Should not display a period as item has no milestone', async () => {
+    // Given
+    const item = new FollowupItem(
+      'partner',
+      'type',
+      'id1',
+      'ref1',
+      'notifications',
+      null,
+      null,
+      [],
+      'title',
+      'subheading',
+      'description',
+      'icon',
+      new Date('2026-01-03T08:05:42Z'),
+      'new',
+      'New',
+      false,
+      'link1',
+      []
+    );
+    vi.spyOn(item, 'hasMilestone').mockReturnValue(false);
+
+    // When
+    render(FollowupItemDetailHeader, { props: { item: item } });
+
+    // Then
+    expect(screen.queryByTestId('item-period')).toBeNull();
+  });
+  test('Should display a period', async () => {
+    // Given
+    const item = new FollowupItem(
+      'partner',
+      'type',
+      'id1',
+      'ref1',
+      'notifications',
+      null,
+      null,
+      [],
+      'title',
+      'subheading',
+      'description',
+      'icon',
+      new Date('2026-01-03T08:05:42Z'),
+      'new',
+      'New',
+      false,
+      'link1',
+      []
+    );
+    vi.spyOn(item, 'hasMilestone').mockReturnValue(true);
+    vi.spyOn(item, 'period', 'get').mockReturnValue('A Period');
+
+    // When
+    render(FollowupItemDetailHeader, { props: { item: item } });
+
+    // Then
+    const period = screen.getByTestId('item-period');
+    expect(period).toHaveTextContent('A Period');
+  });
+  test('Should not display a duration as item has no milestone', async () => {
+    // Given
+    const item = new FollowupItem(
+      'partner',
+      'type',
+      'id1',
+      'ref1',
+      'notifications',
+      null,
+      null,
+      [],
+      'title',
+      'subheading',
+      'description',
+      'icon',
+      new Date('2026-01-03T08:05:42Z'),
+      'new',
+      'New',
+      false,
+      'link1',
+      []
+    );
+    vi.spyOn(item, 'hasMilestone').mockReturnValue(false);
+
+    // When
+    render(FollowupItemDetailHeader, { props: { item: item } });
+
+    // Then
+    expect(screen.queryByTestId('item-duration')).toBeNull();
+  });
+  test('Should not display a duration as item has no duration', async () => {
+    // Given
+    const item = new FollowupItem(
+      'partner',
+      'type',
+      'id1',
+      'ref1',
+      'notifications',
+      null,
+      null,
+      [],
+      'title',
+      'subheading',
+      'description',
+      'icon',
+      new Date('2026-01-03T08:05:42Z'),
+      'new',
+      'New',
+      false,
+      'link1',
+      []
+    );
+    vi.spyOn(item, 'hasMilestone').mockReturnValue(true);
+    vi.spyOn(item, 'duration', 'get').mockReturnValue(undefined);
+
+    // When
+    render(FollowupItemDetailHeader, { props: { item: item } });
+
+    // Then
+    expect(screen.queryByTestId('item-duration')).toBeNull();
+  });
+  test('Should display a period', async () => {
+    // Given
+    const item = new FollowupItem(
+      'partner',
+      'type',
+      'id1',
+      'ref1',
+      'notifications',
+      null,
+      null,
+      [],
+      'title',
+      'subheading',
+      'description',
+      'icon',
+      new Date('2026-01-03T08:05:42Z'),
+      'new',
+      'New',
+      false,
+      'link1',
+      []
+    );
+    vi.spyOn(item, 'hasMilestone').mockReturnValue(true);
+    vi.spyOn(item, 'duration', 'get').mockReturnValue('A Duration');
+
+    // When
+    render(FollowupItemDetailHeader, { props: { item: item } });
+
+    // Then
+    const duration = screen.getByTestId('item-duration');
+    expect(duration).toHaveTextContent('A Duration');
+  });
   test('Should display "Accéder à ma démarche" button', async () => {
     // Given
     const item = new FollowupItem(
@@ -154,6 +309,7 @@ describe('/FollowupItemDetailHeader.svelte', () => {
       []
     );
     const spy = vi.spyOn(AMINavigationMethods, 'AMIGoto').mockResolvedValue();
+    vi.spyOn(item, 'hasMilestone').mockReturnValue(false);
 
     // When
     render(FollowupItemDetailHeader, { props: { item: item } });
@@ -164,6 +320,46 @@ describe('/FollowupItemDetailHeader.svelte', () => {
     await fireEvent.click(button);
 
     // Then
+    expect(button).toHaveTextContent('Accéder à ma démarche');
+    await waitFor(() => {
+      expect(spy).toHaveBeenCalledWith('link1');
+    });
+  });
+  test('Should display "Accéder au détail du rendez-vous" button', async () => {
+    // Given
+    const item = new FollowupItem(
+      'partner',
+      'type',
+      'id1',
+      'ref1',
+      'notifications',
+      null,
+      null,
+      [],
+      'title',
+      'subheading',
+      'description',
+      'icon',
+      new Date('2026-01-03T08:05:42Z'),
+      'new',
+      'New',
+      false,
+      'link1',
+      []
+    );
+    const spy = vi.spyOn(AMINavigationMethods, 'AMIGoto').mockResolvedValue();
+    vi.spyOn(item, 'hasMilestone').mockReturnValue(true);
+
+    // When
+    render(FollowupItemDetailHeader, { props: { item: item } });
+    await waitFor(async () => {
+      expect(screen.queryByTestId('external-item-button')).not.toBeNull();
+    });
+    const button = screen.getByTestId('external-item-button');
+    await fireEvent.click(button);
+
+    // Then
+    expect(button).toHaveTextContent('Accéder au détail du rendez-vous');
     await waitFor(() => {
       expect(spy).toHaveBeenCalledWith('link1');
     });
