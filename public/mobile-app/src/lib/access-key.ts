37d56f3c516d3b5617aaa5ecee2015a427ea1179
Frédéric Péters <fpeters@0d.be>
2026-09-18 14:04:51 +0200

    front: move access key check to init() hook (#1395)

---
+++ public/mobile-app/src/lib/access-key.ts
@@ -0,0 +1,32 @@
+import { PUBLIC_PROMPT_FOR_ACCESS_KEY } from '$env/static/public';
+
+export const checkAccessKey = async () => {
+  if (!window.NativeBridge && PUBLIC_PROMPT_FOR_ACCESS_KEY) {
+    try {
+      let access_key: string = '';
+      const access_key_cookie = await window.cookieStore.get('access_key');
+      if (!access_key_cookie) {
+        const day = 24 * 60 * 60 * 1000;
+        access_key = prompt(PUBLIC_PROMPT_FOR_ACCESS_KEY) || '';
+        await window.cookieStore.set({
+          name: 'access_key',
+          value: access_key,
+          expires: Date.now() + 7 * day,
+        });
+      } else {
+        access_key = access_key_cookie.value || '';
+      }
+      const response = await fetch('/api/v1/access-key', {
+        method: 'POST',
+        body: new URLSearchParams({ key: access_key }),
+      });
+      if (response.status !== 200) {
+        await cookieStore.delete('access_key');
+        (window as Window).location = '/';
+      }
+    } catch {
+      await cookieStore.delete('access_key');
+      (window as Window).location = '/';
+    }
+  }
+};
